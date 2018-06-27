0e01d230968baa40f3af7dda62a4f718d23db25d
Moritz Huebner <email@moritz-huebner.de>
2018-06-27 10:33:25 +1000

    Use the newly integrated starting_time functionality in tupak

--- scripts/test.py
+++ scripts/test.py
@@ -16,7 +16,7 @@ s21 = S2[0]
 s22 = S2[1]
 s23 = S2[2]
 LMax = 3
-luminosity_distance = 2000.
+luminosity_distance = 500.
 inc = np.pi / 2
 pol = 0
 ra = 1.375
@@ -24,18 +24,21 @@ dec = -1.2108
 psi = 2.659
 geocent_time = 1126259642.413
 
-start_time = -0.5
+starting_time = -0.5
 end_time = 0.01 # 0.029
+
+time_duration = end_time - starting_time
+sampling_frequency = 2000
 
 outdir = 'outdir'
 label = 'test'
 
 tupak.core.utils.setup_logger(outdir=outdir, label=label)
-time_array = np.linspace(start_time, end_time, 1000)
-time_duration = time_array[-1] - time_array[0]
-sampling_frequency = tupak.utils.get_sampling_frequency(time_array)
-frequency_array = tupak.utils.create_frequency_series(sampling_frequency=sampling_frequency,
-                                                      duration=time_duration)
+# time_array = np.linspace(starting_time, end_time, 1000)
+# time_duration = time_array[-1] - time_array[0]
+# sampling_frequency = tupak.utils.get_sampling_frequency(time_array)
+# frequency_array = tupak.utils.create_frequency_series(sampling_frequency=sampling_frequency,
+#                                                       duration=time_duration)
 
 np.random.seed(88170235)
 
@@ -45,32 +48,34 @@ injection_parameters = dict(total_mass=total_mass, mass_ratio=mass_ratio, s11=s1
 
 waveform_generator = tupak.WaveformGenerator(time_duration=time_duration,
                                              sampling_frequency=sampling_frequency,
+                                             starting_time=starting_time,
                                              time_domain_source_model=waveforms.
                                              time_domain_nr_sur_waveform_without_memory,
                                              parameters=injection_parameters,
                                              waveform_arguments=dict(LMax=LMax))
-waveform_generator.time_array = time_array.copy()
-waveform_generator.frequency_array = frequency_array
+# waveform_generator.time_array = time_array.copy()
+# waveform_generator.frequency_array = frequency_array
 hf_signal = waveform_generator.frequency_domain_strain()
 test_strain_signal = waveform_generator.time_domain_strain()
-plt.plot(time_array, test_strain_signal['plus'])
+plt.plot(waveform_generator.time_array, test_strain_signal['plus'])
 plt.show()
 IFOs = [tupak.gw.detector.get_interferometer_with_fake_noise_and_injection(
     name, injection_polarizations=hf_signal, injection_parameters=injection_parameters, time_duration=time_duration,
-    sampling_frequency=sampling_frequency, start_time=start_time, outdir=outdir) for name in ['H1', 'L1']]
+    sampling_frequency=sampling_frequency, start_time=starting_time, outdir=outdir) for name in ['H1', 'L1']]
 
 priors = dict()
 for key in ['total_mass', 'mass_ratio', 's11', 's12', 's13', 's21', 's22', 's23', 'luminosity_distance',
             'inc', 'pol', 'ra', 'dec', 'geocent_time', 'psi']:
     priors[key] = injection_parameters[key]
 priors['total_mass'] = tupak.prior.Uniform(minimum=50, maximum=70)
-priors['luminosity_distance'] = tupak.prior.Uniform(minimum=1800, maximum=2100)
+priors['luminosity_distance'] = tupak.prior.Uniform(minimum=400, maximum=600)
+priors['inc'] = tupak.prior.Uniform(minimum=0, maximum=np.pi)
 
 likelihood = tupak.GravitationalWaveTransient(interferometers=IFOs, waveform_generator=waveform_generator,
                                               time_marginalization=False, phase_marginalization=False,
                                               distance_marginalization=False, prior=priors)
 
-result = tupak.run_sampler(likelihood=likelihood, priors=priors, sampler='dynesty', npoints=100,
+result = tupak.run_sampler(likelihood=likelihood, priors=priors, sampler='dynesty', npoints=300,
                            injection_parameters=injection_parameters, outdir=outdir, label=label)
 
 result.plot_corner(lionize=True)
