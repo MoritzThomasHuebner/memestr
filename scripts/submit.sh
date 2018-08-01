ac3e31ca3f08d8b57e7d3bb830e9d41502c8b169
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 16:49:09 +1000

    corrected the file name reference

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 bash ../install.sh
 sbatch basic_job.sh ./non_memory_injection_recovery.py non_memory_res_2.txt
-sbatch memory_slurm.sh ./memory_injection_recovery.py memory_res_2.txt
+sbatch basic_job.sh ./memory_injection_recovery.py memory_res_2.txt
