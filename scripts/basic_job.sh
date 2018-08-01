b0d89092cb54ca28c0d5646361f998d27366dcfe
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 14:03:40 +1000

    Did some more workflow streamlining

--- scripts/basic_job.sh
+++ scripts/basic_job.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+PYTHON_FILE=$1
+JOB_NAME=${PYTHON_FILE}
+OUTPUT=$2
 #
-#SBATCH --job-name=test
-#SBATCH --output=res.txt
+#SBATCH --job-name=${JOB_NAME}
+#SBATCH --output=${OUTPUT}
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=500
 
-PYTHON_FILE=$1
-srun python $PYTHON_FILE
+srun python ${PYTHON_FILE}
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-git pull
-python ../setup.py install --user
-sbatch non_memory_slurm.sh
-sbatch memory_slurm.sh
+bash ../install.sh
+sbatch basic_job.sh ./non_memory_injection_recovery.py non_memory_res.txt
+sbatch memory_slurm.sh ./memory_injection_recovery.py memory_res.txt
