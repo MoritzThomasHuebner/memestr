a2c03492aba4634f889501b3cfaa2ec69f25b52d
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 13:56:18 +1000

    Fixed a typo

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 setup(name='memestr',
       version='0.0.1',
-      packages=['memestr', 'memestr.core'],
+      packages=['memestr', 'memestr.core', 'memestr.wrappers'],
       package_dir={'memestr': 'memestr'},
       )
