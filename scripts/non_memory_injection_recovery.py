ba437c1916687886d79c74f45c64c8cf0a78ab96
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 13:47:14 +1000

    Rewrote call to function

--- scripts/non_memory_injection_recovery.py
+++ scripts/non_memory_injection_recovery.py
@@ -1,4 +1,5 @@
 from __future__ import division
 import memestr
 
-memestr.wrappers.wrappers.run_basic_injection(memestr.core.waveforms.time_domain_nr_sur_waveform_without_memory, 'outdir_non_memory')
+memestr.wrappers.wrappers.run_basic_injection(
+    memestr.core.waveforms.time_domain_nr_sur_waveform_without_memory, 'outdir_non_memory')
