d97e5d142b55fe5db509c8e0d1c8383beef770dd
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 18:23:59 +1000

    renamed file

--- scripts/memory_injection.sh
+++ scripts/memory_injection.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=test
-#SBATCH --output=test_res.txt
+#SBATCH --job-name=mem_inj
+#SBATCH --output=mem_inj.txt
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
