f1e742ab0a357b1fc31fbdd1b39952e73d3841b0
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 18:17:15 +1000

    added more basic script

---
+++ scripts/super_basic_job.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=test
+#SBATCH --output=test_res.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=2000
+
+srun python basic_job.sh
