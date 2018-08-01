1e16e16d9acc1d61c99c7e40f80a894f42d1f0ee
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 12:19:46 +1000

    Reorganised project structure

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-from __future__ import division
-import memestr.core.waveforms as waveforms
-from memestr.wrappers.wrappers import run_basic_injection
-
-run_basic_injection(waveforms.time_domain_nr_sur_waveform_without_memory, 'outdir_non_memory')
-run_basic_injection(waveforms.time_domain_nr_sur_waveform_with_memory, 'outdir_memory')
