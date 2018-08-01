dd7ce9dffc3ec71169557bda2df1ba53faf419f3
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-01 12:28:47 +1000

    fixed the script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sbatch basic_job ./memory_injection_recovery.py
-sbatch basic_job ./non_memory_injection_recovery.py
+sbatch basic_job.sh ./memory_injection_recovery.py
+sbatch basic_job.sh ./non_memory_injection_recovery.py
