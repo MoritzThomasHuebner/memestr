f6b01cd3d58a95656dbea74fb4ba3f926babdffa
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-03 10:23:04 +1000

    Changed the submit script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 bash ../install.sh
-sbatch basic_job.sh ./non_memory_injection_recovery.py non_memory_res_2.txt
-sbatch basic_job.sh ./memory_injection_recovery.py memory_res_2.txt
+sbatch non_memory_injection.sh ./non_memory_injection_recovery.py non_memory_res_2.txt
+sbatch memory_injection.sh ./memory_injection_recovery.py memory_res_2.txt
