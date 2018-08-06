d157f2c17cb6f1a7335c7677815b7cbfd03f18a6
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-06 19:52:29 +1000

    Changed some parameters

--- scripts/phenom_waveforms.py
+++ scripts/phenom_waveforms.py
@@ -4,7 +4,7 @@ import tupak
 import numpy as np
 
 mass_ratio = 1.5
-total_mass = 60
+total_mass = 70
 luminosity_distance = 400
 s11 = 0
 s12 = 0
@@ -17,19 +17,19 @@ pol = 0.6
 injection_parameters = dict(total_mass=total_mass, mass_ratio=mass_ratio, s11=s11, s12=s12, s13=s13, s21=s21,
                             s22=s22, s23=s23, luminosity_distance=luminosity_distance, inc=inc, pol=pol)
 
-duration = 4
-sampling_frequency = 4096
+duration = 1.932
+sampling_frequency = 2000
 start_time = 0
 
 waveform_generator = tupak.gw.WaveformGenerator(duration=duration,
                                                 sampling_frequency=sampling_frequency,
                                                 start_time=start_time,
-                                                time_domain_source_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
+                                                time_domain_source_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
                                                 parameters=injection_parameters)
 time_domain_data = waveform_generator.time_domain_strain()
 frequency_domain_data = waveform_generator.frequency_domain_strain()
 fig = plt.figure(figsize=(12, 4))
 for key in time_domain_data.keys():
-    plt.plot(time_domain_data[key])
+    plt.plot(waveform_generator.time_array, time_domain_data[key])
 plt.show()
 plt.close()
