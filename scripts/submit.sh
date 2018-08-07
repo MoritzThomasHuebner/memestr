453b981ac7b6806995013a0b63c95711afe9410f
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-07 14:00:29 +1000

    Removed obsolete argument

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 bash ../install.sh
-sbatch non_memory_injection.sh ./non_memory_injection_recovery.py
-sbatch memory_injection.sh ./memory_injection_recovery.py
+sbatch non_memory_injection.sh
+sbatch memory_injection.sh
