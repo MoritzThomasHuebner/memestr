e6007468a92b2c0396e05f645c3b506c4f8faf8f
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-08 13:02:58 +1000

    Updated parameters for next run. Allocated significantly more memory

--- scripts/non_memory_injection.sh
+++ scripts/non_memory_injection.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #
 #SBATCH --job-name=non_mem_inj
-#SBATCH --output=non_mem_inj_4.txt
+#SBATCH --output=non_mem_inj_5.txt
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=72:00:00
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=16G
 
 srun python non_memory_injection_recovery.py
