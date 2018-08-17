708d761a06126bcb2133b098c0da036ac9f01730
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 16:36:59 +1000

    Fixed an error

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 bash ../install.sh
-sbatch IMR_mem_inj_mem_rec.sh
-sbatch IMR_mem_inj_non_mem_rec.sh
-sbatch NRSur_mem_inj_mem_rec.sh
-sbatch NRSur_mem_inj_non_mem_rec.sh
+bash IMR_mem_inj_mem_rec.sh
+bash IMR_mem_inj_non_mem_rec.sh
+bash NRSur_mem_inj_mem_rec.sh
+bash NRSur_mem_inj_non_mem_rec.sh
