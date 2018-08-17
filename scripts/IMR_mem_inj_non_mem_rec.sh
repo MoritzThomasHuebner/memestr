1c1f6fb80334290b7e1c7922e9c3c082fea0bf2e
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 15:34:20 +1000

    More simplifications

--- scripts/IMR_mem_inj_non_mem_rec.sh
+++ scripts/IMR_mem_inj_non_mem_rec.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-JOB_NAME="--job_name=$1"
-OUTPUT="--output=$1.log"
+LABEL={$0::-3}
+
+JOB_NAME="--job_name=$LABEL"
+OUTPUT="--output=$LABEL.log"
 TIME="--time=72:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=16G"
@@ -10,7 +12,7 @@ EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
 
 sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<'EOF'
 JOB=run_basic_job.py
-NAMING_SCHEME=IMR_mem_inj_non_mem_rec
+NAMING_SCHEME=$LABEL
 SCRIPT=run_basic_injection_imr_phenom
 INJECTION_MODEL=time_domain_IMRPhenomD_waveform_with_memory
 RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_without_memory
