39f343191022c20fa4860dc28a8ac8994392146d
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 15:26:53 +1000

    Further simplified, can now set the naming scheme

--- scripts/NRSur_mem_inj_mem_rec.sh
+++ scripts/NRSur_mem_inj_mem_rec.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-#
-#SBATCH --job-name=NRSur_mem_inj_mem_rec
-#SBATCH --output=NRSur_mem_inj_mem_rec.log
-#
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=72:00:00
-#SBATCH --mem-per-cpu=16G
-#SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
 
+JOB_NAME="--job_name=$1"
+OUTPUT="--output=$1.log"
+TIME="--time=72:00:00"
+NTASKS="--ntasks=1"
+MEM_PER_CPU="--mem-per-cpu=16G"
+CPUS_PER_TASK="--cpus-per-task=1"
+EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
+
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<'EOF'
 JOB=run_basic_job.py
 NAMING_SCHEME=NRSur_mem_inj_mem_rec
 SCRIPT=run_basic_injection
 INJECTION_MODEL=time_domain_nr_sur_waveform_with_memory
 RECOVERY_MODEL=time_domain_nr_sur_waveform_with_memory
 srun python ${JOB} ${NAMING_SCHEME} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL}
+EOF
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 bash ../install.sh
-sbatch IMR_mem_inj_mem_rec.sh
-sbatch IMR_mem_inj_non_mem_rec.sh
-sbatch NRSur_mem_inj_mem_rec.sh
-sbatch NRSur_mem_inj_non_mem_rec.sh
+sbatch IMR_mem_inj_mem_rec.sh IMR_mem_inj_mem_rec
+sbatch IMR_mem_inj_non_mem_rec.sh IMR_mem_inj_non_mem_rec
+sbatch NRSur_mem_inj_mem_rec.sh NRSur_mem_inj_mem_rec
+sbatch NRSur_mem_inj_non_mem_rec.sh NRSur_mem_inj_non_mem_rec
