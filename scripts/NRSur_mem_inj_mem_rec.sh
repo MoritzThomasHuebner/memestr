d142f92e27035a75b72a8a1069888306ef8ca548
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 18:27:02 +1000

    Final fix

--- scripts/NRSur_mem_inj_mem_rec.sh
+++ scripts/NRSur_mem_inj_mem_rec.sh
@@ -2,12 +2,12 @@
 
 source default_slurm_setup.sh ${0}
 
-sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<'EOF'
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<EOF
 #!/usr/bin/env bash
 JOB=run_basic_job.py
-NAMING_SCHEME=$LABEL
+NAMING_SCHEME=${LABEL}
 SCRIPT=run_basic_injection
 INJECTION_MODEL=time_domain_nr_sur_waveform_with_memory
 RECOVERY_MODEL=time_domain_nr_sur_waveform_with_memory
-srun python ${JOB} ${NAMING_SCHEME} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL}
+srun python \${JOB} \${NAMING_SCHEME} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
 EOF
