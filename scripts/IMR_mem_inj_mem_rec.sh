7d49cadc86702f18a4b490d85a5e6de993bb710b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 18:22:53 +1000

    Test

--- scripts/IMR_mem_inj_mem_rec.sh
+++ scripts/IMR_mem_inj_mem_rec.sh
@@ -2,12 +2,12 @@
 
 source default_slurm_setup.sh ${0}
 
-sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<'EOF'
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<EOF
 #!/usr/bin/env bash
 JOB=run_basic_job.py
-NAMING_SCHEME=$LABEL
+NAMING_SCHEME=${LABEL}
 SCRIPT=run_basic_injection_imr_phenom
 INJECTION_MODEL=time_domain_IMRPhenomD_waveform_with_memory
 RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_with_memory
-srun python ${JOB} ${NAMING_SCHEME} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL}
+srun python \${JOB} \${NAMING_SCHEME} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
 EOF
