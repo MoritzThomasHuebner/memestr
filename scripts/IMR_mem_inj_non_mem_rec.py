4486454abea4986ad16ebae091adc25c3ad57b10
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 12:42:09 +1000

    Automated moving log file

--- scripts/IMR_mem_inj_non_mem_rec.py
+++ scripts/IMR_mem_inj_non_mem_rec.py
@@ -1,5 +1,7 @@
 from __future__ import division
 import memestr
+import os
+import shutil
 
 from . import submitter
 
@@ -9,3 +11,8 @@ memestr.wrappers.wrappers.run_basic_injection_imr_phenom(
     injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
     recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
     outdir=outdir)
+
+log_file = 'IMR_mem_inj_non_mem_rec.log'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.rename(dir_path + log_file, dir_path + outdir + "/" + log_file)
+shutil.move(dir_path + log_file, dir_path + outdir + "/" + log_file)
