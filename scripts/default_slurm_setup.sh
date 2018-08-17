c698574806a9b88ff6085c519651147d41ad90a1
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-17 18:14:07 +1000

    Test

--- scripts/default_slurm_setup.sh
+++ scripts/default_slurm_setup.sh
@@ -4,7 +4,7 @@ LABEL="${1::-3}"
 PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_name(name=\"$LABEL\"))"
 OUTDIR=`python -c "${PYTHON_COMMAND}"`
 JOB_NAME="--job-name=$LABEL"
-OUTPUT="--output=$LABEL.log"
+OUTPUT="--output=$OUTDIR.log"
 TIME="--time=72:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=16G"
--- scripts/run_basic_job.py
+++ scripts/run_basic_job.py
@@ -2,7 +2,7 @@ from __future__ import division
 import sys
 from memestr.submit import submitter
 from memestr import models, scripts
-print('test')
+print(sys.argv)
 submitter.run_job(naming_scheme=sys.argv[1],
                   script=scripts[sys.argv[2]],
                   injection_model=models[sys.argv[3]],
