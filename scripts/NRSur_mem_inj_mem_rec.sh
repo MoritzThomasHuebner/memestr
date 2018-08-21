806878944899972f2ac3dfc6f8ea4ecab61b4f47
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-21 18:34:01 +1000

    Hopefully fix outdir bug

--- scripts/NRSur_mem_inj_mem_rec.sh
+++ scripts/NRSur_mem_inj_mem_rec.sh
@@ -5,9 +5,8 @@ source default_slurm_setup.sh ${0}
 sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<EOF
 #!/usr/bin/env bash
 JOB=run_basic_job.py
-NAMING_SCHEME=${LABEL}
 SCRIPT=run_basic_injection_nrsur
 INJECTION_MODEL=time_domain_nr_sur_waveform_with_memory
 RECOVERY_MODEL=time_domain_nr_sur_waveform_with_memory
-srun python \${JOB} \${NAMING_SCHEME} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
+srun python \${JOB} ${OUTDIR} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
 EOF
