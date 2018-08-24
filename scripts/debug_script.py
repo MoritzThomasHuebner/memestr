c7987de40a6659fd4661c065896f2d08494e7670
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-24 11:44:01 +1000

    Changed debug script

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -1,5 +1,5 @@
 import memestr
 
-memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
-                                                         recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
+memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
+                                                         recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
                                                          outdir='test')
