72c61827351e9aa1c3e7eaa483c0183cadcdf8d3
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-24 16:19:06 +1000

    Added alternative python command

--- scripts/default_slurm_setup.sh
+++ scripts/default_slurm_setup.sh
@@ -2,6 +2,7 @@
 
 LABEL="${1::-3}"
 PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_name(name=\"$LABEL\"))"
+ALTERNATIVE_COMMAND="import os; outdir = ''; for i in range(0, 999): outdir = str(i).zfill(3) + "_" + \"$LABEL\"; if not os.path.exists(outdir): break; print(outdir)"
 OUTDIR=`python -c "${PYTHON_COMMAND}"`
 JOB_NAME="--job-name=$LABEL"
 OUTPUT="--output=$OUTDIR.log"
