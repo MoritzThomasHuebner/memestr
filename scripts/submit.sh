4251ed53dbe1591464b97feae47c07347718948f
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-24 11:03:27 +1000

    Fixed an error

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -4,6 +4,6 @@ bash IMR_mem_inj_mem_rec.sh
 bash IMR_mem_inj_non_mem_rec.sh
 bash IMR_non_mem_inj_non_mem_rec.sh
 bash IMR_non_mem_inj_mem_rec.sh
-bash IMR_pure_mem_inj_pure_mem_mem_rec.sh
+bash IMR_pure_mem_inj_pure_mem_rec.sh
 #bash NRSur_mem_inj_mem_rec.sh
 #bash NRSur_mem_inj_non_mem_rec.sh
