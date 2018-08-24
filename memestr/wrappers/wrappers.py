0a76bf82e827316e0bdefafc4891141dbce58e1b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-24 15:24:02 +1000

    Some more simplifications to the settings

--- memestr/wrappers/wrappers.py
+++ memestr/wrappers/wrappers.py
@@ -3,6 +3,31 @@ import numpy as np
 import tupak
 
 
+class AllSettings(object):
+
+    def __init__(self, injection_parameters, recovery_priors, waveform_arguments, waveform_data, sampler_settings,
+                 detector_settings, other_settings):
+        self.injection_parameters = injection_parameters
+        self.recovery_priors = recovery_priors
+        self.waveform_arguments = waveform_arguments
+        self.waveform_data = waveform_data
+        self.sampler_settings = sampler_settings
+        self.detector_settings = detector_settings
+        self.other_settings = other_settings
+
+    @classmethod
+    def from_defaults_with_some_specified_kwargs(cls, **kwargs):
+        result = cls(injection_parameters=InjectionParameters.init_with_updated_kwargs(**kwargs),
+                     recovery_priors=RecoveryPriors.init_with_updated_kwargs(**kwargs),
+                     waveform_arguments=WaveformArguments.init_with_updated_kwargs(**kwargs),
+                     waveform_data=WaveformData.init_with_updated_kwargs(**kwargs),
+                     sampler_settings=SamplerSettings.init_with_updated_kwargs(**kwargs),
+                     detector_settings=DetectorSettings.init_with_updated_kwargs(**kwargs),
+                     other_settings=OtherSettings.init_with_updated_kwargs(**kwargs)
+                     )
+        return result
+
+
 class RunParameters(object):
 
     def __init__(self):
@@ -46,38 +71,44 @@ class InjectionParameters(RunParameters):
 class RecoveryPriors(RunParameters):
 
     def __init__(self,
-                 prior_total_mass=tupak.core.prior.Uniform(minimum=50, maximum=70, latex_label="$M_{tot}$"),
-                 prior_mass_ratio=tupak.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$"),
-                 prior_luminosity_distance=tupak.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
-                                                                                maximum=5e3, latex_label="$L_D$"),
-                 prior_inc=tupak.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$"),
-                 prior_phase=tupak.core.prior.Uniform(name='phase', minimum=0, maximum=2 * np.pi, latex_label="$\phi$"),
-                 prior_ra=tupak.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$"),
-                 prior_dec=tupak.core.prior.Cosine(name='dec', latex_label="$DEC$"),
-                 prior_psi=tupak.core.prior.Uniform(name='psi', minimum=0, maximum=2 * np.pi, latex_label="$\psi$"),
-                 prior_geocent_time=tupak.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time'),
-                 prior_s11=tupak.core.prior.DeltaFunction(name='s11', peak=0, latex_label='s11'),
-                 prior_s12=tupak.core.prior.DeltaFunction(name='s12', peak=0, latex_label='s12'),
-                 prior_s21=tupak.core.prior.DeltaFunction(name='s21', peak=0, latex_label='s21'),
-                 prior_s22=tupak.core.prior.DeltaFunction(name='s22', peak=0, latex_label='s22'),
-                 prior_s13=tupak.core.prior.DeltaFunction(name='s13', peak=0, latex_label='s13'),
-                 prior_s23=tupak.core.prior.DeltaFunction(name='s23', peak=0, latex_label='s23')):
+                 total_mass=tupak.core.prior.Uniform(minimum=50, maximum=70, latex_label="$M_{tot}$"),
+                 mass_ratio=tupak.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$"),
+                 luminosity_distance=tupak.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
+                                                                          maximum=5e3, latex_label="$L_D$"),
+                 inc=tupak.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$"),
+                 phase=tupak.core.prior.Uniform(name='phase', minimum=0, maximum=2 * np.pi, latex_label="$\phi$"),
+                 ra=tupak.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$"),
+                 dec=tupak.core.prior.Cosine(name='dec', latex_label="$DEC$"),
+                 psi=tupak.core.prior.Uniform(name='psi', minimum=0, maximum=2 * np.pi, latex_label="$\psi$"),
+                 geocent_time=tupak.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time'),
+                 s11=tupak.core.prior.Uniform(name='s11', minimum=0, maximum=0.8, latex_label='s11'),
+                 s12=tupak.core.prior.Uniform(name='s12', minimum=0, maximum=0.8, latex_label='s12'),
+                 s21=tupak.core.prior.Uniform(name='s21', minimum=0, maximum=0.8, latex_label='s21'),
+                 s22=tupak.core.prior.Uniform(name='s22', minimum=0, maximum=0.8, latex_label='s22'),
+                 s13=tupak.core.prior.Uniform(name='s13', minimum=0, maximum=0.8, latex_label='s13'),
+                 s23=tupak.core.prior.Uniform(name='s23', minimum=0, maximum=0.8, latex_label='s23')):
         super(RecoveryPriors, self).__init__()
-        self.prior_total_mass = prior_total_mass
-        self.prior_mass_ratio = prior_mass_ratio
-        self.prior_luminosity_distance = prior_luminosity_distance
-        self.prior_inc = prior_inc
-        self.prior_phase = prior_phase
-        self.prior_ra = prior_ra
-        self.prior_dec = prior_dec
-        self.prior_psi = prior_psi
-        self.prior_geocent_time = prior_geocent_time
-        self.prior_s11 = prior_s11
-        self.prior_s12 = prior_s12
-        self.prior_s21 = prior_s21
-        self.prior_s22 = prior_s22
-        self.prior_s13 = prior_s13
-        self.prior_s23 = prior_s23
+        self.prior_total_mass = total_mass
+        self.prior_mass_ratio = mass_ratio
+        self.prior_luminosity_distance = luminosity_distance
+        self.prior_inc = inc
+        self.prior_phase = phase
+        self.prior_ra = ra
+        self.prior_dec = dec
+        self.prior_psi = psi
+        self.prior_geocent_time = geocent_time
+        self.prior_s11 = s11
+        self.prior_s12 = s12
+        self.prior_s21 = s21
+        self.prior_s22 = s22
+        self.prior_s13 = s13
+        self.prior_s23 = s23
+
+    def proper_dict(self):
+        result = dict()
+        for key in self.__dict__:
+            result[key[6:]] = self.__dict__[key]
+        return result
 
 
 class WaveformArguments(RunParameters):
@@ -124,47 +155,41 @@ class OtherSettings(RunParameters):
 
 
 def run_basic_injection(injection_model, recovery_model, outdir, **kwargs):
-    injection_parameters = InjectionParameters.init_with_updated_kwargs(**kwargs)
-    recovery_priors = RecoveryPriors.init_with_updated_kwargs(**kwargs)
-    waveform_arguments = WaveformArguments().init_with_updated_kwargs(**kwargs)
-    waveform_data = WaveformData().init_with_updated_kwargs(**kwargs)
-    sampler_kwargs = SamplerSettings().init_with_updated_kwargs(**kwargs)
-    detector_kwargs = DetectorSettings().init_with_updated_kwargs(**kwargs)
-    other_kwargs = OtherSettings().init_with_updated_kwargs(**kwargs)
+    settings = AllSettings.from_defaults_with_some_specified_kwargs(**kwargs)
 
-    for key in waveform_arguments.__dict__:
-        injection_parameters.__dict__[key] = waveform_arguments.__dict__[key]
+    for key in settings.waveform_arguments.__dict__:
+        settings.injection_parameters.__dict__[key] = settings.waveform_arguments.__dict__[key]
 
-    tupak.core.utils.setup_logger(outdir=outdir, label=sampler_kwargs.label)
-    if not other_kwargs.new_seed:
+    tupak.core.utils.setup_logger(outdir=outdir, label=settings.sampler_settings.label)
+    if not settings.other_settings.new_seed:
         np.random.seed(88170235)
 
     waveform_generator = tupak.gw.WaveformGenerator(time_domain_source_model=injection_model,
-                                                    parameters=injection_parameters.__dict__,
-                                                    waveform_arguments=waveform_arguments.__dict__,
-                                                    **waveform_data.__dict__)
+                                                    parameters=settings.injection_parameters.__dict__,
+                                                    waveform_arguments=settings.waveform_arguments.__dict__,
+                                                    **settings.waveform_data.__dict__)
 
     hf_signal = waveform_generator.frequency_domain_strain()
     ifos = [tupak.gw.detector.get_interferometer_with_fake_noise_and_injection(
         name,
         injection_polarizations=hf_signal,
-        injection_parameters=injection_parameters.__dict__,
+        injection_parameters=settings.injection_parameters.__dict__,
         outdir=outdir,
-        zero_noise=detector_kwargs.zero_noise,
-        **waveform_data.__dict__) for name in detector_kwargs.detectors]
+        zero_noise=settings.detector_settings.zero_noise,
+        **settings.waveform_data.__dict__) for name in settings.detector_settings.detectors]
 
     waveform_generator.time_domain_source_model = recovery_model
 
     likelihood = tupak.gw.likelihood.GravitationalWaveTransient(interferometers=ifos,
                                                                 waveform_generator=waveform_generator,
-                                                                prior=recovery_priors.__dict__)
+                                                                prior=settings.recovery_priors.proper_dict())
 
     result = tupak.core.sampler.run_sampler(likelihood=likelihood,
-                                            priors=recovery_priors.__dict__,
-                                            injection_parameters=injection_parameters.__dict__,
+                                            priors=settings.recovery_priors.proper_dict(),
+                                            injection_parameters=settings.injection_parameters.__dict__,
                                             outdir=outdir,
-                                            **sampler_kwargs.__dict__)
-    result.plot_corner(lionize=other_kwargs.lionize)
+                                            **settings.sampler_settings.__dict__)
+    result.plot_corner(lionize=settings.other_settings.lionize)
     print(result)
 
 
@@ -196,20 +221,21 @@ def run_basic_injection_imr_phenom(injection_model, recovery_model, outdir, **kw
     for key in injection_parameters.__dict__:
         priors['prior_' + key] = injection_parameters.__dict__[key]
     priors['prior_total_mass'] = tupak.core.prior.Uniform(minimum=50, maximum=70, latex_label="$M_{tot}$")
-    priors['prior_mass_ratio'] = tupak.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$")
-    priors['prior_luminosity_distance'] = tupak.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
-                                                                               maximum=5e3, latex_label="$L_D$")
-    priors['prior_inc'] = tupak.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$")
-    priors['prior_phase'] = tupak.core.prior.Uniform(name='phase', minimum=0, maximum=2 * np.pi, latex_label="$\phi$")
-    priors['prior_ra'] = tupak.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
-    priors['prior_dec'] = tupak.core.prior.Cosine(name='dec', latex_label="$DEC$")
-    priors['prior_psi'] = tupak.core.prior.Uniform(name='psi', minimum=0, maximum=2 * np.pi, latex_label="$\psi$")
-    priors['prior_geocent_time'] = tupak.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
+    # priors['prior_mass_ratio'] = tupak.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$")
+    # priors['prior_luminosity_distance'] = tupak.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
+    #                                                                            maximum=5e3, latex_label="$L_D$")
+    # priors['prior_inc'] = tupak.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$")
+    # priors['prior_phase'] = tupak.core.prior.Uniform(name='phase', minimum=0, maximum=2 * np.pi, latex_label="$\phi$")
+    # priors['prior_ra'] = tupak.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
+    # priors['prior_dec'] = tupak.core.prior.Cosine(name='dec', latex_label="$DEC$")
+    # priors['prior_psi'] = tupak.core.prior.Uniform(name='psi', minimum=0, maximum=2 * np.pi, latex_label="$\psi$")
+    # priors['prior_geocent_time'] = tupak.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
 
     imr_phenom_kwargs = dict(
         label='IMRPhenomD',
         new_seed=True,
         zero_noise=True,
+        npoints=500
     )
     imr_phenom_kwargs.update(priors)
     imr_phenom_kwargs.update(kwargs)
