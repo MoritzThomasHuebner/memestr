1161b70c69ec092141c14f6efe6c2c3df7d4c001
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-24 10:58:43 +1000

    Now hands the local directory to the submitter

--- scripts/run_basic_job.py
+++ scripts/run_basic_job.py
@@ -2,8 +2,10 @@ from __future__ import division
 import sys
 from memestr.submit import submitter
 from memestr import models, scripts
-print(sys.argv)
+import os
+
 submitter.run_job(outdir=sys.argv[1],
                   script=scripts[sys.argv[2]],
+                  dir_path=os.path.dirname(os.path.realpath(__file__)),
                   injection_model=models[sys.argv[3]],
                   recovery_model=models[sys.argv[4]])
