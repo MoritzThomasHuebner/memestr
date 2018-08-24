ee0b1a1f88b5049b0e5f61cb36ff3ebc45f6bc85
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-24 10:35:51 +1000

    Added three additional injection/recovery scripts

---
+++ scripts/IMR_non_mem_inj_non_mem_rec.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source default_slurm_setup.sh ${0}
+
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<EOF
+#!/usr/bin/env bash
+JOB=run_basic_job.py
+SCRIPT=run_basic_injection_imr_phenom
+INJECTION_MODEL=time_domain_IMRPhenomD_waveform_without_memory
+RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_without_memory
+srun python \${JOB} ${OUTDIR} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
+EOF
---
+++ scripts/IMR_pure_mem_inj_pure_mem_rec.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source default_slurm_setup.sh ${0}
+
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL}<<EOF
+#!/usr/bin/env bash
+JOB=run_basic_job.py
+SCRIPT=run_basic_injection_imr_phenom
+INJECTION_MODEL=time_domain_IMRPhenomD_memory_waveform
+RECOVERY_MODEL=time_domain_IMRPhenomD_memory_waveform
+srun python \${JOB} ${OUTDIR} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
+EOF
