c059787231d6e829dd779a5f16db95099dcb4a29
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-29 14:50:46 +1000

    Set up for a local run

--- scripts/debug_script.sh
+++ scripts/debug_script.sh
@@ -5,4 +5,4 @@ SCRIPT=run_basic_job
 INJECTION_MODEL=time_domain_nr_sur_waveform_with_memory
 RECOVERY_MODEL=time_domain_nr_sur_waveform_with_memory
 DISTANCE=200
-python ${JOB} ${OUTDIR} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL} luminosity_distance=50 l_max=4
+python ${JOB} ${OUTDIR} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL} luminosity_distance=50 l_max=4 mass_ratio=1.2414 total_mass=65
