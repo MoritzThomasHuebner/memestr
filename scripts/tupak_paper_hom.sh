42e5ce5c89622e1dae3901c8df561cb7a9ebdd93
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-29 13:44:31 +1000

    Set up for next debug run

--- scripts/tupak_paper_hom.sh
+++ scripts/tupak_paper_hom.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 bash ../install.sh
-bash NRSur_non_mem_inj_non_mem_rec.sh luminosity_distance=50 l_max=4
-bash NRSur_non_mem_inj_non_mem_rec.sh luminosity_distance=50 l_max=4
+bash NRSur_non_mem_inj_non_mem_rec.sh luminosity_distance=50 l_max=2 mass_ratio=1.2414
+bash NRSur_non_mem_inj_non_mem_rec.sh luminosity_distance=50 l_max=4 mass_ratio=1.2414
