5fc18968fc9d2c0d1bc092eca75ca20bb4a1c392
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-29 14:50:34 +1000

    Set up for a local run

--- scripts/debug_script.sh
+++ scripts/debug_script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 JOB=run_basic_job.py
-OUTDIR="test"
-SCRIPT=run_basic_injection_imr_phenom
-INJECTION_MODEL=time_domain_IMRPhenomD_waveform_with_memory
-RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_with_memory
+OUTDIR="NRSur_HOM"
+SCRIPT=run_basic_job
+INJECTION_MODEL=time_domain_nr_sur_waveform_with_memory
+RECOVERY_MODEL=time_domain_nr_sur_waveform_with_memory
 DISTANCE=200
-python ${JOB} ${OUTDIR} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL} ${DISTANCE}
+python ${JOB} ${OUTDIR} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL} luminosity_distance=50 l_max=4
