4249600a761af1a5b10e84aaba07c1faffe37c08
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-29 13:59:36 +1000

    Set up for next debug run

--- scripts/tupak_paper_hom.sh
+++ scripts/tupak_paper_hom.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 bash ../install.sh
 bash NRSur_non_mem_inj_non_mem_rec.sh luminosity_distance=50 l_max=2 mass_ratio=1.2414 total_mass=65
+sleep 10
 bash NRSur_non_mem_inj_non_mem_rec.sh luminosity_distance=50 l_max=4 mass_ratio=1.2414 total_mass=65
