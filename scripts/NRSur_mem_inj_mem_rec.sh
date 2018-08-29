45a2dcde5e8e725dfd8d8d715a0f6405fcb184a3
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-29 13:43:00 +1000

    Enabled the use of kwargs

--- scripts/NRSur_mem_inj_mem_rec.sh
+++ scripts/NRSur_mem_inj_mem_rec.sh
@@ -8,5 +8,5 @@ JOB=run_basic_job.py
 SCRIPT=run_basic_injection_nrsur
 INJECTION_MODEL=time_domain_nr_sur_waveform_with_memory
 RECOVERY_MODEL=time_domain_nr_sur_waveform_with_memory
-srun python \${JOB} ${OUTDIR} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}
+srun python \${JOB} ${OUTDIR} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL} $@
 EOF
