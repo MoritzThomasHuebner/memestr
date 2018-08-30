4cbe581d2e0415d737eafc037ed71949e9f4fecd
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 21:45:34 +1000

    Improved analysis script

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -1,18 +1,25 @@
 import tupak
 import os
+run_id = '002'
+runs = [run_id+'_IMR_mem_inj_mem_rec',
+        run_id+'_IMR_mem_inj_non_mem_rec',
+        run_id+'_IMR_non_mem_inj_mem_rec',
+        run_id+'_IMR_non_mem_inj_non_mem_rec',
+        run_id+'_IMR_pure_mem_inj_pure_mem_rec']
 
-with open('distance_evidence.dat', 'a') as outfile:
-    outfile.write("#Luminosity distance\tlog_bayes_factor\tlog_bayes_evidence\tlog_bayes_evidence_err")
-run = "001_IMR_mem_inj_non_mem_rec"
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + run
-for subdir, dirs, files in os.walk(dir_path):
-    for dir in dirs:
-        dir_path = subdir + "/" + dir + "/" + "IMRPhenomD_result.h5"
-        print(dir_path)
-        result = tupak.core.result.read_in_result(filename=dir_path)
+for run in runs:
+    with open(run + '_distance_evidence.dat', 'a') as outfile:
+        outfile.write("#Luminosity distance\tlog_bayes_factor\tlog_bayes_evidence\tlog_bayes_evidence_err")
 
-        with open(run + '_distance_evidence.dat', 'a') as outfile:
-            outfile.write(str(dir) + '\t' +
-                          str(result.log_bayes_factor) + '\t' +
-                          str(result.log_evidence) + '\t' +
-                          str(result.log_evidence_err) + '\n')
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + run
+    for subdir, dirs, _ in os.walk(dir_path):
+        for dir in dirs:
+            dir_path = subdir + "/" + dir + "/" + "IMRPhenomD_result.h5"
+            print(dir_path)
+            result = tupak.core.result.read_in_result(filename=dir_path)
+
+            with open(run + '_distance_evidence.dat', 'a') as outfile:
+                outfile.write(str(dir) + '\t' +
+                              str(result.log_evidence) + '\t' +
+                              str(result.log_bayes_factor) + '\t' +
+                              str(result.log_evidence_err) + '\n')
