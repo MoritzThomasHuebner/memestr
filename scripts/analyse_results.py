5b3eee3eedf1382cd1d9dccc4593b98b44be2f8b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 17:26:02 +1000

    Wrote analysis script for evidence

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -2,18 +2,15 @@ import tupak
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + "001_IMR_mem_inj_mem_rec"
-print(dir_path)
 for subdir, dirs, files in os.walk(dir_path):
-    print(subdir)
-    print(dirs)
-    print(files)
-    dir_path = subdir + "/" + "IMRPhenomD_result.h5"
-    print(dir_path)
-    result = tupak.core.result.read_in_result(dir_path)
+    for dir in dirs:
+        dir_path = subdir + "/" + dir + "/" + "IMRPhenomD_result.h5"
+        print(dir_path)
+        result = tupak.core.result.read_in_result(dir_path)
 
-    with open('distance_evidence.dat', 'a') as outfile:
-        outfile.write(str(subdir) + '\t' +
-                      str(result.bayes_factor) + '\t' +
-                      str(result.bayes_factor_err) + '\t' +
-                      str(result.log_evidence) + '\t' +
-                      str(result.log_evidence_err) + '\n')
+        with open('distance_evidence.dat', 'a') as outfile:
+            outfile.write(str(subdir) + '\t' +
+                          str(result.bayes_factor) + '\t' +
+                          str(result.bayes_factor_err) + '\t' +
+                          str(result.log_evidence) + '\t' +
+                          str(result.log_evidence_err) + '\n')
