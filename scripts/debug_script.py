cf649822cb6a3f8bb0ee3fa5e22c004d3adfa67d
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 11:52:30 +1000

    Set up for distances debug run

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -1,5 +1,15 @@
-import memestr
+from memestr.submit import submitter
+from memestr import models, scripts
 
-memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
-                                                         recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
-                                                         outdir='test')
+# memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
+#                                                          recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
+#                                                          outdir='test')
+submitter.run_job(outdir='NRSur_Base_Mode',
+                  script=scripts['run_basic_injection_nrsur'],
+                  injection_model=models['time_domain_nr_sur_waveform_without_memory'],
+                  recovery_model=models['time_domain_nr_sur_waveform_without_memory'],
+                  luminosity_distance=50,
+                  l_max=2,
+                  mass_ratio=1.2414,
+                  total_mass=65,
+                  npoints=500)
---
+++ scripts/distances_slurm_setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+LABEL="${1::-3}"
+PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_name(name=\"$LABEL\"))"
+OUTDIR=`python -c "${PYTHON_COMMAND}"`
+JOB_NAME="--job-name=$LABEL"
+OUTPUT="--output=$OUTDIR.log"
+TIME="--time=00:40:00"
+NTASKS="--ntasks=1"
+MEM_PER_CPU="--mem-per-cpu=16G"
+CPUS_PER_TASK="--cpus-per-task=1"
+ARRAY="--array=10:500:100"
+EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
