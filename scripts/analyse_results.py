423b3901e9b59766243f370beefae92b5de0ac4d
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 17:24:04 +1000

    Wrote analysis script for evidence

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -2,9 +2,12 @@ import tupak
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + "001_IMR_mem_inj_mem_rec"
-
+print(dir_path)
 for subdir, dirs, files in os.walk(dir_path):
-    dir_path = dir_path + "/" + subdir + "/" + "IMRPhenomD_result.h5"
+    print(subdir)
+    print(dirs)
+    print(files)
+    dir_path = subdir + "/" + "IMRPhenomD_result.h5"
     print(dir_path)
     result = tupak.core.result.read_in_result(dir_path)
 
