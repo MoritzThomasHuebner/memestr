53ed2e89a719b6b184f291e2f176f5955e8b1c48
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 17:37:44 +1000

    Wrote analysis script for evidence

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -1,14 +1,17 @@
 import tupak
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + "001_IMR_mem_inj_mem_rec"
+with open('distance_evidence.dat', 'a') as outfile:
+    outfile.write("#Luminosity distance\tlog_bayes_factor\tlog_bayes_evidence\tlog_bayes_evidence_err")
+run = "001_IMR_mem_inj_non_mem_rec"
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + run
 for subdir, dirs, files in os.walk(dir_path):
     for dir in dirs:
         dir_path = subdir + "/" + dir + "/" + "IMRPhenomD_result.h5"
         print(dir_path)
         result = tupak.core.result.read_in_result(filename=dir_path)
 
-        with open('distance_evidence.dat', 'a') as outfile:
+        with open(run + '_distance_evidence.dat', 'a') as outfile:
             outfile.write(str(dir) + '\t' +
                           str(result.log_bayes_factor) + '\t' +
                           str(result.log_evidence) + '\t' +
