359a9b94680eda68fcac1b857b25f551bd316886
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 17:29:24 +1000

    Wrote analysis script for evidence

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -6,7 +6,7 @@ for subdir, dirs, files in os.walk(dir_path):
     for dir in dirs:
         dir_path = subdir + "/" + dir + "/" + "IMRPhenomD_result.h5"
         print(dir_path)
-        result = tupak.core.result.read_in_result(dir_path)
+        result = tupak.core.result.read_in_result(filename=dir_path)
 
         with open('distance_evidence.dat', 'a') as outfile:
             outfile.write(str(subdir) + '\t' +
