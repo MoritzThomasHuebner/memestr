80aff92d0e2da5074d909164c2a422504ef1b171
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-30 17:30:01 +1000

    Wrote analysis script for evidence

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -10,7 +10,7 @@ for subdir, dirs, files in os.walk(dir_path):
 
         with open('distance_evidence.dat', 'a') as outfile:
             outfile.write(str(subdir) + '\t' +
-                          str(result.bayes_factor) + '\t' +
-                          str(result.bayes_factor_err) + '\t' +
+                          str(result.log_bayes_factor) + '\t' +
+                          str(result.log_bayes_factor_err) + '\t' +
                           str(result.log_evidence) + '\t' +
                           str(result.log_evidence_err) + '\n')
