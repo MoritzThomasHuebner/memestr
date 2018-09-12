01d05c8f7399e913222e58180e5d6dee84158b04
MoritzThomasHuebner <email@moritz-huebner.de>
2018-09-12 14:20:22 +1000

    ID 007: Set up production run with time marginalization

--- memestr/wrappers/wrappers.py
+++ memestr/wrappers/wrappers.py
@@ -101,22 +101,22 @@ def run_basic_injection_imr_phenom(injection_model, recovery_model, outdir, **kw
     for key in injection_parameters.__dict__:
         priors['prior_' + key] = injection_parameters.__dict__[key]
     priors['prior_total_mass'] = tupak.core.prior.Uniform(minimum=50, maximum=70, latex_label="$M_{tot}$")
-    # priors['prior_mass_ratio'] = tupak.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$")
-    # priors['prior_luminosity_distance'] = tupak.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
-    #                                                                            maximum=5e3, latex_label="$L_D$")
-    # priors['prior_inc'] = tupak.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$")
-    # priors['prior_phase'] = tupak.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi, latex_label="$\phi$")
-    # priors['prior_ra'] = tupak.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
-    # priors['prior_dec'] = tupak.core.prior.Cosine(name='dec', latex_label="$DEC$")
-    # priors['prior_psi'] = tupak.core.prior.Uniform(name='psi', minimum=0, maximum=np.pi, latex_label="$\psi$")
-    # priors['prior_geocent_time'] = tupak.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
+    priors['prior_mass_ratio'] = tupak.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$")
+    priors['prior_luminosity_distance'] = tupak.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
+                                                                               maximum=5e3, latex_label="$L_D$")
+    priors['prior_inc'] = tupak.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$")
+    priors['prior_phase'] = tupak.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi, latex_label="$\phi$")
+    priors['prior_ra'] = tupak.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
+    priors['prior_dec'] = tupak.core.prior.Cosine(name='dec', latex_label="$DEC$")
+    priors['prior_psi'] = tupak.core.prior.Uniform(name='psi', minimum=0, maximum=np.pi, latex_label="$\psi$")
+    priors['prior_geocent_time'] = tupak.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
 
     imr_phenom_kwargs = dict(
         label='IMRPhenomD',
         new_seed=False,
         zero_noise=True,
-        npoints=500,
-        time_marginaliztaion=False
+        npoints=5000,
+        time_marginaliztaion=True
     )
     imr_phenom_kwargs.update(priors)
     imr_phenom_kwargs.update(kwargs)
