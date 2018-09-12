1c13455660ac03e4fb24163560614d51dd96fd8f
MoritzThomasHuebner <email@moritz-huebner.de>
2018-09-12 18:14:06 +1000

    Changed the analysis script

--- scripts/analyse_results.py
+++ scripts/analyse_results.py
@@ -1,6 +1,6 @@
 import tupak
 import os
-run_id = '004'
+run_id = '006'
 runs = [run_id+'_IMR_mem_inj_mem_rec',
         run_id+'_IMR_mem_inj_non_mem_rec',
         run_id+'_IMR_non_mem_inj_mem_rec',
@@ -11,15 +11,15 @@ for run in runs:
     with open(run + '_distance_evidence.dat', 'w') as outfile:
         outfile.write("#Luminosity distance\tlog_bayes_factor\tlog_bayes_evidence\tlog_bayes_evidence_err\n")
         dir_path = os.path.dirname(os.path.realpath(__file__)) + "/" + run
-        for subdir, dirs, _ in os.walk(dir_path):
-            for dir in dirs:
-                dir_path = subdir + "/" + dir + "/" + "IMRPhenomD_result.h5"
-                print(dir_path)
-                try:
-                    result = tupak.core.result.read_in_result(filename=dir_path)
-                except ValueError:
-                    break
-                outfile.write(str(dir) + '\t' +
-                              str(result.log_evidence) + '\t' +
-                              str(result.log_bayes_factor) + '\t' +
-                              str(result.log_evidence_err) + '\n')
+        for subdirs, _, _ in os.walk(dir_path):
+            for subdir in subdirs:
+                files = os.listdir(subdirs)
+                for f in files:
+                    if 'result.h5' in f:
+                        dir_path = subdir + "/" + f
+                        print(dir_path)
+                        result = tupak.core.result.read_in_result(filename=dir_path)
+                        outfile.write(str(dir) + '\t' +
+                                      str(result.log_evidence) + '\t' +
+                                      str(result.log_bayes_factor) + '\t' +
+                                      str(result.log_evidence_err) + '\n')
