9cfc5fa1313daee9308b40eb0442ce37b2eb9189
MoritzThomasHuebner <email@moritz-huebner.de>
2018-09-26 22:14:22 +1000

    Set up for next debug run ID 024

--- scripts/get_injection_bash_strings.py
+++ scripts/get_injection_bash_strings.py
@@ -1,5 +1,7 @@
 import memestr
 import sys
 
+
 id = sys.argv[1]
-print(memestr.submit.submitter.get_injection_bash_strings(id=id))
+sys.stdout.write(memestr.submit.submitter.get_injection_bash_strings(id=id))
+sys.stdout.flush()
