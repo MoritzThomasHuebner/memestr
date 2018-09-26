ecee21bd08d6770d77873d9c54f7f0cbb293463b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-09-26 12:34:41 +1000

    ID 018: Set up for production injection/recovery runs with random parameters

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,6 +3,6 @@ bash IMR_mem_inj_mem_rec.sh           time_marginalization=True npoints=2000 zer
 bash IMR_mem_inj_non_mem_rec.sh       time_marginalization=True npoints=2000 zero_noise=False random_injection_parameters=True label=IMR_mem_inj_non_mem_rec
 bash IMR_non_mem_inj_non_mem_rec.sh   time_marginalization=True npoints=2000 zero_noise=False random_injection_parameters=True label=IMR_non_mem_inj_non_mem_rec
 bash IMR_non_mem_inj_mem_rec.sh       time_marginalization=True npoints=2000 zero_noise=False random_injection_parameters=True label=IMR_non_mem_inj_mem_rec
-bash IMR_pure_mem_inj_pure_mem_rec.sh time_marginalization=True npoints=2000 zero_noise=False random_injection_parameters=True label=IMR_pure_mem_inj_pure_mem_rec
+#bash IMR_pure_mem_inj_pure_mem_rec.sh time_marginalization=True npoints=2000 zero_noise=False random_injection_parameters=True label=IMR_pure_mem_inj_pure_mem_rec
 #bash NRSur_mem_inj_mem_rec.sh
 #bash NRSur_mem_inj_non_mem_rec.sh
