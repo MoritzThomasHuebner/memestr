cefe06ee77a4a85ecd458eb68a4828f024ab5b6c
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-01 16:27:44 +1000

    Set up for next debug run ID 024

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 bash IMR_mem_inj_mem_rec.sh           time_marginalization=True npoints=5000 zero_noise=False label=IMR_mem_inj_mem_rec
-#bash IMR_mem_inj_non_mem_rec.sh       time_marginalization=True npoints=5000 zero_noise=False label=IMR_mem_inj_non_mem_rec
-#bash IMR_non_mem_inj_non_mem_rec.sh   time_marginalization=True npoints=5000 zero_noise=False label=IMR_non_mem_inj_non_mem_rec
-#bash IMR_non_mem_inj_mem_rec.sh       time_marginalization=True npoints=5000 zero_noise=False label=IMR_non_mem_inj_mem_rec
+bash IMR_mem_inj_non_mem_rec.sh       time_marginalization=True npoints=5000 zero_noise=False label=IMR_mem_inj_non_mem_rec
+bash IMR_non_mem_inj_non_mem_rec.sh   time_marginalization=True npoints=5000 zero_noise=False label=IMR_non_mem_inj_non_mem_rec
+bash IMR_non_mem_inj_mem_rec.sh       time_marginalization=True npoints=5000 zero_noise=False label=IMR_non_mem_inj_mem_rec
 #bash IMR_pure_mem_inj_pure_mem_rec.sh time_marginalization=True npoints=5000 zero_noise=False random_injection_parameters=True label=IMR_pure_mem_inj_pure_mem_rec
 #bash NRSur_mem_inj_mem_rec.sh
 #bash NRSur_mem_inj_non_mem_rec.sh
