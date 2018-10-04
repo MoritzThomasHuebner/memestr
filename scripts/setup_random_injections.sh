53bf0ae9d04847294ee3c67bc018f638d90a169b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-04 12:46:30 +1000

    Set up resume job

--- scripts/setup_random_injections.sh
+++ scripts/setup_random_injections.sh
@@ -2,10 +2,10 @@
 
 LABEL="${1::-3}"
 PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_name(name=\"$LABEL\"))"
-OUTDIR=`python -c "${PYTHON_COMMAND}"`
+OUTDIR="026_$LABEL"#`python -c "${PYTHON_COMMAND}"`
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
-TIME="--time=24:00:00"
+TIME="--time=48:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=16G"
 CPUS_PER_TASK="--cpus-per-task=1"
