2b6e4e4172a2d85e28abae8695480692a30d927a
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-05 10:12:08 +1000

    ID 027 Set up for next production run only with masses and distance

--- memestr/wrappers/wrappers.py
+++ memestr/wrappers/wrappers.py
@@ -109,16 +109,16 @@ def run_basic_injection_imr_phenom(injection_model, recovery_model, outdir, **kw
     injection_parameters = InjectionParameters.init_with_updated_kwargs(**kwargs)
     for key in injection_parameters.__dict__:
         priors['prior_' + key] = injection_parameters.__dict__[key]
-    priors['prior_total_mass'] = bilby.core.prior.Uniform(minimum=40, maximum=200, latex_label="$M_{tot}$")
+    priors['prior_total_mass'] = bilby.core.prior.Uniform(minimum=30, maximum=80, latex_label="$M_{tot}$")
     priors['prior_mass_ratio'] = bilby.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$")
     priors['prior_luminosity_distance'] = bilby.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
                                                                                maximum=1000, latex_label="$L_D$")
-    priors['prior_inc'] = bilby.core.prior.Sine(latex_label="$\iota$")
-    priors['prior_phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi, latex_label="$\phi$")
-    priors['prior_ra'] = bilby.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
-    priors['prior_dec'] = bilby.core.prior.Cosine(name='dec', latex_label="$DEC$")
-    priors['prior_psi'] = bilby.core.prior.Uniform(name='psi', minimum=0, maximum=np.pi, latex_label="$\psi$")
-    priors['prior_geocent_time'] = bilby.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
+    # priors['prior_inc'] = bilby.core.prior.Sine(latex_label="$\iota$")
+    # priors['prior_phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi, latex_label="$\phi$")
+    # priors['prior_ra'] = bilby.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
+    # priors['prior_dec'] = bilby.core.prior.Cosine(name='dec', latex_label="$DEC$")
+    # priors['prior_psi'] = bilby.core.prior.Uniform(name='psi', minimum=0, maximum=np.pi, latex_label="$\psi$")
+    # priors['prior_geocent_time'] = bilby.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
 
     imr_phenom_kwargs = dict(
         label='IMRPhenomD',
--- scripts/IMR_pure_mem_inj_pure_mem_rec.sh
+++ scripts/IMR_pure_mem_inj_pure_mem_rec.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#source distances_slurm_setup.sh ${0}
+source setup_distances_run.sh ${0}
 #source default_slurm_setup.sh ${0}
-source setup_random_injections.sh ${0}
+#source setup_random_injections.sh ${0}
 
 sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL} ${ARRAY}<<EOF
 #!/usr/bin/env bash
@@ -11,9 +11,10 @@ SCRIPT=run_basic_injection_imr_phenom
 INJECTION_MODEL=time_domain_IMRPhenomD_memory_waveform
 RECOVERY_MODEL=time_domain_IMRPhenomD_memory_waveform
 FILENAME="./parameter_sets/\${SLURM_ARRAY_TASK_ID}"
-while IFS= read -r var
-do
-  PARAMS="\$var"
-done < "\$FILENAME"
-srun python \${JOB} ${OUTDIR}/\${SLURM_ARRAY_TASK_ID} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL} \${PARAMS} $@
+#while IFS= read -r var
+#do
+#  PARAMS="\$var"
+#done < "\$FILENAME"
+PARAMS=""
+srun python \${JOB} ${OUTDIR}/\${SLURM_ARRAY_TASK_ID} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL} \${PARAMS} luminosity_distance=\${SLURM_ARRAY_TASK_ID} $@
 EOF
