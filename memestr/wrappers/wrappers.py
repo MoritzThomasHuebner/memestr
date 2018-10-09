2ab2a6dfaaee4d6f896f778d9b7f70cf3af5b60b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-09 11:45:13 +1100

    Set up for run with masses, distance, inclination, sky positioin ID 032

--- memestr/wrappers/wrappers.py
+++ memestr/wrappers/wrappers.py
@@ -115,8 +115,8 @@ def run_basic_injection_imr_phenom(injection_model, recovery_model, outdir, **kw
                                                                                maximum=1000, latex_label="$L_D$")
     priors['prior_inc'] = bilby.core.prior.Sine(latex_label="$\iota$")
     # priors['prior_phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi, latex_label="$\phi$")
-    # priors['prior_ra'] = bilby.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
-    # priors['prior_dec'] = bilby.core.prior.Cosine(name='dec', latex_label="$DEC$")
+    priors['prior_ra'] = bilby.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
+    priors['prior_dec'] = bilby.core.prior.Cosine(name='dec', latex_label="$DEC$")
     # priors['prior_psi'] = bilby.core.prior.Uniform(name='psi', minimum=0, maximum=np.pi, latex_label="$\psi$")
     # priors['prior_geocent_time'] = bilby.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
 
