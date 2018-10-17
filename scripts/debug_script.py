6f7f806e9af7f82cc3bf92fb02127f24ad026c5b
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-17 14:18:30 +1100

    Restricted priors to +/- pi/4 around the injected values for phase and psi ID 038

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Debug Waveform plots
-# submitter.create_injection_parameter_set(256, wrappers.sample_injection_parameters)
+submitter.create_injection_parameter_set(256, wrappers.sample_injection_parameters)
 # times = np.linspace(0, 4, 32000)
 # for i in range(16):
 #     params = submitter.get_injection_parameter_set(i)
@@ -63,7 +63,7 @@ import numpy as np
 #                   geocent_time=1126259642.413,
 #                   ra=1.375,
 #                   dec=-1.2108)
-settings = memestr.submit.parameters.AllSettings()
+# settings = memestr.submit.parameters.AllSettings()
 # if settings.injection_parameters.random_injection_parameters:
 #     settings.injection_parameters.__dict__.update(memestr.wrappers.wrappers.sample_injection_parameters())
 #     pd = settings.recovery_priors.proper_dict()
@@ -72,36 +72,36 @@ settings = memestr.submit.parameters.AllSettings()
 #             settings.recovery_priors.__dict__['prior_' + key] = \
 #                 bilby.core.prior.DeltaFunction(peak=settings.injection_parameters.__dict__[key])
 
-models = [memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
-          memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
-          memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory]
-
-def plot_waveform(td_model):
-    waveform_generator = bilby.gw.WaveformGenerator(time_domain_source_model=td_model,
-                                                    parameters=settings.injection_parameters.__dict__,
-                                                    waveform_arguments=settings.waveform_arguments.__dict__,
-                                                    **settings.waveform_data.__dict__)
-
-    plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['plus']))
-    plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['cross']))
-    plt.xlim(20, 1000)
-    plt.ylim(1e-27, 4e-23)
-    plt.loglog()
-    plt.show()
-
-    hf_signal = waveform_generator.frequency_domain_strain()
-    ifo = bilby.gw.detector.get_interferometer_with_fake_noise_and_injection(
-        'H1',
-        injection_polarizations=hf_signal,
-        injection_parameters=settings.injection_parameters.__dict__,
-        outdir='test',
-        zero_noise=False,
-        **settings.waveform_data.__dict__)
-    plt.plot(ifo.strain_data.frequency_array, np.abs(ifo.strain_data.frequency_domain_strain))
-    plt.loglog()
-    plt.show()
-
-
-
-for model in models:
-    plot_waveform(model)
+# models = [memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
+#           memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
+#           memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory]
+#
+# def plot_waveform(td_model):
+#     waveform_generator = bilby.gw.WaveformGenerator(time_domain_source_model=td_model,
+#                                                     parameters=settings.injection_parameters.__dict__,
+#                                                     waveform_arguments=settings.waveform_arguments.__dict__,
+#                                                     **settings.waveform_data.__dict__)
+#
+#     plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['plus']))
+#     plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['cross']))
+#     plt.xlim(20, 1000)
+#     plt.ylim(1e-27, 4e-23)
+#     plt.loglog()
+#     plt.show()
+#
+#     hf_signal = waveform_generator.frequency_domain_strain()
+#     ifo = bilby.gw.detector.get_interferometer_with_fake_noise_and_injection(
+#         'H1',
+#         injection_polarizations=hf_signal,
+#         injection_parameters=settings.injection_parameters.__dict__,
+#         outdir='test',
+#         zero_noise=False,
+#         **settings.waveform_data.__dict__)
+#     plt.plot(ifo.strain_data.frequency_array, np.abs(ifo.strain_data.frequency_domain_strain))
+#     plt.loglog()
+#     plt.show()
+#
+#
+#
+# for model in models:
+#     plot_waveform(model)
