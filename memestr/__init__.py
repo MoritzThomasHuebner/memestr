0a85ac885e8578e93a6ad62d30f060b34c45b0d5
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-30 15:20:26 +1100

    Added NRSur base mode waveforms

--- memestr/__init__.py
+++ memestr/__init__.py
@@ -8,8 +8,11 @@ models = dict(
     time_domain_IMRPhenomD_waveform_without_memory=core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
     time_domain_nr_sur_memory_waveform=core.waveforms.time_domain_nr_sur_memory_waveform,
     time_domain_nr_sur_waveform_with_memory=core.waveforms.time_domain_nr_sur_waveform_with_memory,
-    time_domain_nr_sur_waveform_without_memory=core.waveforms.time_domain_nr_sur_waveform_without_memory
+    time_domain_nr_sur_waveform_without_memory=core.waveforms.time_domain_nr_sur_waveform_without_memory,
+    time_domain_nr_sur_waveform_with_memory_base_modes=core.waveforms.time_domain_nr_sur_waveform_with_memory_base_modes,
+    time_domain_nr_sur_waveform_without_memory_base_modes=core.waveforms.time_domain_nr_sur_waveform_without_memory_base_modes
 )
+
 scripts = dict(
     run_basic_injection_imr_phenom=wrappers.wrappers.run_basic_injection_imr_phenom,
     run_basic_injection_nrsur=wrappers.wrappers.run_basic_injection_nrsur,
--- memestr/core/waveforms.py
+++ memestr/core/waveforms.py
@@ -5,6 +5,46 @@ import copy
 gamma_lmlm = gwmemory.angles.load_gamma()
 
 
+def time_domain_nr_sur_waveform_without_memory_base_modes(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+                                                          luminosity_distance,
+                                                          inc, phase, **kwargs):
+    temp_times = copy.copy(times)
+    memory_generator = gwmemory.waveforms.Surrogate(q=mass_ratio,
+                                                    name='',
+                                                    MTot=total_mass,
+                                                    S1=np.array([s11, s12, s13]),
+                                                    S2=np.array([s21, s22, s23]),
+                                                    modes=[(2, 2), (2, -2)],
+                                                    LMax=2,
+                                                    times=temp_times,
+                                                    distance=luminosity_distance
+                                                    )
+    h_oscillatory, _ = memory_generator.time_domain_oscillatory(times=temp_times, inc=inc, phase=phase)
+    return h_oscillatory
+
+
+def time_domain_nr_sur_waveform_with_memory_base_modes(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+                                                          luminosity_distance,
+                                                          inc, phase, **kwargs):
+    temp_times = copy.copy(times)
+    memory_generator = gwmemory.waveforms.Surrogate(q=mass_ratio,
+                                                    name='',
+                                                    MTot=total_mass,
+                                                    S1=np.array([s11, s12, s13]),
+                                                    S2=np.array([s21, s22, s23]),
+                                                    modes=[(2, 2), (2, -2)],
+                                                    LMax=2,
+                                                    times=temp_times,
+                                                    distance=luminosity_distance
+                                                    )
+    h_oscillatory, _ = memory_generator.time_domain_oscillatory(times=temp_times, inc=inc, phase=phase)
+    h_memory, _ = memory_generator.time_domain_memory(inc=inc, phase=phase, gamma_lmlm=gamma_lmlm)
+    res = dict()
+    for mode in h_memory:
+        res[mode] = h_memory[mode] + h_oscillatory[mode]
+    return res
+
+
 def time_domain_nr_sur_waveform_without_memory(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
                                                luminosity_distance,
                                                inc, phase, **kwargs):
