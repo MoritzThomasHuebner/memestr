2dc5328676fcc4829c7d9df6a046fe446dc74eb2
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-30 15:20:51 +1100

    Manually set outdir to 004_... and added much closer distances

--- scripts/setup_distances_run.sh
+++ scripts/setup_distances_run.sh
@@ -2,12 +2,12 @@
 
 LABEL="${1::-3}"
 PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_name(name=\"$LABEL\"))"
-OUTDIR=`python -c "${PYTHON_COMMAND}"`
+OUTDIR="003_$LABEL"#`python -c "${PYTHON_COMMAND}"`
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
 TIME="--time=120:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=16G"
 CPUS_PER_TASK="--cpus-per-task=1"
-ARRAY="--array=210,230,250,270,290,310,340,370,400,450,500,550,600,650,700,750,800,850,900,950"
+ARRAY="--array=50,70,90,110,130,150,170,190,210,230,250,270,290,310,340,370,400,450,500,550,600,650,700,750,800,850,900,950"
 EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
