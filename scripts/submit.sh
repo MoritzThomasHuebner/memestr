09a930e1c8831c5983ba997196c72981444efc32
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-30 15:21:35 +1100

    ID 003: New runs to see if degeneracy can be broken in base and higher order modes

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,7 +3,9 @@
 #bash IMR_mem_inj_non_mem_rec.sh       npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_mem_inj_non_mem_rec
 #bash IMR_non_mem_inj_non_mem_rec.sh   npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_non_mem_inj_non_mem_rec
 #bash IMR_non_mem_inj_mem_rec.sh       npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_non_mem_inj_mem_rec
-bash NRSur_mem_inj_mem_rec.sh          npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_mem_rec
-bash NRSur_non_mem_inj_mem_rec.sh      npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_mem_rec
-bash NRSur_mem_inj_non_mem_rec.sh      npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_non_mem_rec
-bash NRSur_non_mem_inj_non_mem_rec.sh  npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
+bash NRSur_mem_inj_mem_rec.sh                     npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_mem_rec
+bash NRSur_mem_inj_mem_rec_base_modes.sh          npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=2 sampler=dynesty label=NRSur_mem_inj_mem_rec
+bash NRSur_non_mem_inj_non_mem_rec.sh             npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
+bash NRSur_non_mem_inj_non_mem_rec_base_modes.sh  npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=2 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
+#bash NRSur_non_mem_inj_mem_rec.sh      npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_mem_rec
+#bash NRSur_mem_inj_non_mem_rec.sh      npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_non_mem_rec
