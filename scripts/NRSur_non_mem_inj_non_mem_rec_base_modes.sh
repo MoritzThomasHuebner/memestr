57deb02700c7c66c9b20456976ddb0f575d5ee52
MoritzThomasHuebner <email@moritz-huebner.de>
2018-10-30 15:21:50 +1100

    New scripts for base mode waveforms

---
+++ scripts/NRSur_non_mem_inj_non_mem_rec_base_modes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#source setup_default.sh ${0}
+source setup_distances_run.sh ${0}
+
+
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL} ${ARRAY}<<EOF
+#!/usr/bin/env bash
+JOB=run_basic_job.py
+SCRIPT=run_basic_injection_nrsur
+INJECTION_MODEL=time_domain_nr_sur_waveform_without_memory_base_modes
+RECOVERY_MODEL=time_domain_nr_sur_waveform_without_memory_base_modes
+FILENAME="./parameter_sets/\${SLURM_ARRAY_TASK_ID}"
+#while IFS= read -r var
+#do
+#  PARAMS="\$var"
+#done < "\$FILENAME"
+PARAMS=""
+srun python \${JOB} ${OUTDIR}/\${SLURM_ARRAY_TASK_ID} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL}  \${PARAMS} luminosity_distance=\${SLURM_ARRAY_TASK_ID} $@
+EOF
