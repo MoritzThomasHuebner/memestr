829ffc299a46b1f0df90ebe6efc14402a63f46b8
Moritz <email@moritz-huebner.de>
2018-11-05 13:03:09 +1100

    Set up new run with time marginalization ID 004

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-#bash IMR_mem_inj_mem_rec.sh           npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_mem_inj_mem_rec
-#bash IMR_mem_inj_non_mem_rec.sh       npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_mem_inj_non_mem_rec
-#bash IMR_non_mem_inj_non_mem_rec.sh   npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_non_mem_inj_non_mem_rec
-#bash IMR_non_mem_inj_mem_rec.sh       npoints=5000 zero_noise=False inc=0.5 sampler=nestle label=IMR_non_mem_inj_mem_rec
-bash NRSur_mem_inj_mem_rec.sh                     npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_mem_rec
-bash NRSur_mem_inj_mem_rec_base_modes.sh          npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=2 sampler=dynesty label=NRSur_mem_inj_mem_rec
-bash NRSur_non_mem_inj_non_mem_rec.sh             npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
-bash NRSur_non_mem_inj_non_mem_rec_base_modes.sh  npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=2 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
+bash IMR_mem_inj_mem_rec.sh                 npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_mem_inj_mem_rec
+bash IMR_mem_inj_non_mem_rec.sh             npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_mem_inj_non_mem_rec
+bash IMR_non_mem_inj_non_mem_rec.sh         npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_non_mem_inj_non_mem_rec
+bash IMR_non_mem_inj_mem_rec.sh             npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_non_mem_inj_mem_rec
+bash IMR_pure_mem_inj_pure_mem_rec.sh       npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_pure_mem_inj_pure_mem_rec
+#bash NRSur_mem_inj_mem_rec.sh                     npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_mem_rec
+#bash NRSur_mem_inj_mem_rec_base_modes.sh          npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=2 sampler=dynesty label=NRSur_mem_inj_mem_rec
+#bash NRSur_non_mem_inj_non_mem_rec.sh             npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
+#bash NRSur_non_mem_inj_non_mem_rec_base_modes.sh  npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=2 sampler=dynesty label=NRSur_non_mem_inj_non_mem_rec
 #bash NRSur_non_mem_inj_mem_rec.sh      npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_non_mem_inj_mem_rec
 #bash NRSur_mem_inj_non_mem_rec.sh      npoints=1000 psi=2.356 phase=0.785 zero_noise=False l_max=4 sampler=dynesty label=NRSur_mem_inj_non_mem_rec
