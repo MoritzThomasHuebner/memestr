3c37465ff25d427898d2a86b3f5a66e0c40f1ce9
Moritz <email@moritz-huebner.de>
2018-11-08 16:08:32 +1100

    Set up for run ID 013: phi/psi/t_c, 2500 npoints, 80 walks, zero noise

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-bash IMR_mem_inj_mem_rec.sh                 npoints=5000 walks=200 dlogz=0.001 zero_noise=True sampler=dynesty label=IMR_mem_inj_mem_rec
-bash IMR_mem_inj_non_mem_rec.sh             npoints=5000 walks=200 dlogz=0.001 zero_noise=True sampler=dynesty label=IMR_mem_inj_non_mem_rec
+bash IMR_mem_inj_mem_rec.sh                 npoints=2500 walks=80 zero_noise=True sampler=dynesty label=IMR_mem_inj_mem_rec
+bash IMR_mem_inj_non_mem_rec.sh             npoints=2500 walks=80 zero_noise=True sampler=dynesty label=IMR_mem_inj_non_mem_rec
 #bash IMR_non_mem_inj_non_mem_rec.sh         npoints=5000 walks=200 zero_noise=True sampler=dynesty label=IMR_non_mem_inj_non_mem_rec
 #bash IMR_non_mem_inj_mem_rec.sh             npoints=5000 walks=200 zero_noise=True sampler=dynesty label=IMR_non_mem_inj_mem_rec
 #bash IMR_pure_mem_inj_pure_mem_rec.sh       npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_pure_mem_inj_pure_mem_rec
