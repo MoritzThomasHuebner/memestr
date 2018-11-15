b4561906e4571b40a80ebbb75d481398e6c1d3d1
Moritz <email@moritz-huebner.de>
2018-11-15 17:13:02 +1100

    ID 016 Set up run with phase/psi/t_c, 15 walks, zero noise

--- scripts/IMR_mem_inj_mem_rec.sh
+++ scripts/IMR_mem_inj_mem_rec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pipenv shell
+source ~/.local/share/virtualenvs/memestr-OPSVMNuO/bin/activate
 source setup_distances_run.sh ${0}
 #source setup_points_walks_run.sh ${0}
 #source default_slurm_setup.sh ${0}
