dab3b0b9614a13784de28cba544b84aab87a4f34
Moritz <email@moritz-huebner.de>
2018-11-23 13:49:33 +1100

    ID 000, time windowing , 5000 live points with noise

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -24,9 +24,9 @@ import numpy as np
 #     plt.show()
 #     plt.clf()
 
-memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
-                                                         recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
-                                                         outdir='test', sampler='dynesty')
+# memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
+#                                                          recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
+#                                                          outdir='test', sample='auto', npoints=300, walks=45)
 # memestr.wrappers.wrappers.run_basic_injection_nrsur(injection_model=memestr.core.waveforms.time_domain_nr_sur_waveform_with_memory,
 #                                                     recovery_model=memestr.core.waveforms.time_domain_nr_sur_waveform_with_memory,
 #                                                     outdir='test', sampler='dynesty', npoints=1000)
@@ -66,45 +66,3 @@ memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr
 #                   geocent_time=1126259642.413,
 #                   ra=1.375,
 #                   dec=-1.2108)
-# settings = memestr.submit.parameters.AllSettings()
-# if settings.injection_parameters.random_injection_parameters:
-#     settings.injection_parameters.__dict__.update(memestr.wrappers.wrappers.sample_injection_parameters())
-#     pd = settings.recovery_priors.proper_dict()
-#     for key in pd:
-#         if isinstance(pd[key], (int, float, bilby.core.prior.DeltaFunction)):
-#             settings.recovery_priors.__dict__['prior_' + key] = \
-#                 bilby.core.prior.DeltaFunction(peak=settings.injection_parameters.__dict__[key])
-
-# models = [memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
-#           memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
-#           memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory]
-#
-# def plot_waveform(td_model):
-#     waveform_generator = bilby.gw.WaveformGenerator(time_domain_source_model=td_model,
-#                                                     parameters=settings.injection_parameters.__dict__,
-#                                                     waveform_arguments=settings.waveform_arguments.__dict__,
-#                                                     **settings.waveform_data.__dict__)
-#
-#     plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['plus']))
-#     plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['cross']))
-#     plt.xlim(20, 1000)
-#     plt.ylim(1e-27, 4e-23)
-#     plt.loglog()
-#     plt.show()
-#
-#     hf_signal = waveform_generator.frequency_domain_strain()
-#     ifo = bilby.gw.detector.get_interferometer_with_fake_noise_and_injection(
-#         'H1',
-#         injection_polarizations=hf_signal,
-#         injection_parameters=settings.injection_parameters.__dict__,
-#         outdir='test',
-#         zero_noise=False,
-#         **settings.waveform_data.__dict__)
-#     plt.plot(ifo.strain_data.frequency_array, np.abs(ifo.strain_data.frequency_domain_strain))
-#     plt.loglog()
-#     plt.show()
-#
-#
-#
-# for model in models:
-#     plot_waveform(model)
