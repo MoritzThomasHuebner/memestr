b82d404d389777bc4c97dda2cbe28252eeb1a536
Moritz <email@moritz-huebner.de>
2018-12-24 14:26:11 +1100

    Fixed another issue

--- scripts/setup_distances_run_mp.sh
+++ scripts/setup_distances_run_mp.sh
@@ -7,6 +7,6 @@ JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
 TIME="--time=1:00:00"
 NUMBER_OF_TASKS=4
-NTASKS="--ntasks="${NUMBER_OF_TASKS}"
+NTASKS="--ntasks="${NUMBER_OF_TASKS}
 MEM_PER_CPU="--mem-per-cpu=8G"
 EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
