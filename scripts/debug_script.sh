1c92a191a50da46987ac175ae975f24d92c6754e
Moritz <email@moritz-huebner.de>
2018-12-24 12:11:14 +1100

    Adapted to test multiprocessing

--- scripts/debug_script.sh
+++ scripts/debug_script.sh
@@ -5,4 +5,4 @@ SCRIPT=run_basic_job
 INJECTION_MODEL=time_domain_IMRPhenomD_waveform_without_memory
 RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_without_memory
 DISTANCE=800
-python ${JOB} ${OUTDIR} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL} luminosity_distance=800
+python3 ${JOB} ${OUTDIR} ${SCRIPT} ${INJECTION_MODEL} ${RECOVERY_MODEL} luminosity_distance=800
