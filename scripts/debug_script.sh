08a14170ebf8ade824b6d088400b73d150234245
Moritz <email@moritz-huebner.de>
2018-12-24 14:14:45 +1100

    Adapted to test multiprocessing

--- scripts/debug_script.sh
+++ scripts/debug_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 JOB=multiprocessed_run.py
 OUTDIR="debug_mp"
-SCRIPT=run_basic_job
+SCRIPT=run_basic_injection
 INJECTION_MODEL=time_domain_IMRPhenomD_waveform_without_memory
 RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_without_memory
 DISTANCE=800
