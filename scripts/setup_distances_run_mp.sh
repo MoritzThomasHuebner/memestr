e6fb9cd0b7abb0fc5ad70605f7ee802b28cf0291
Moritz <email@moritz-huebner.de>
2018-12-24 21:06:48 +1100

    Setup for debug run

--- scripts/setup_distances_run_mp.sh
+++ scripts/setup_distances_run_mp.sh
@@ -5,7 +5,8 @@ PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_
 OUTDIR=`python -c "${PYTHON_COMMAND}"`
 JOB_NAME="--job-name=$LABEL"
 TIME="--time=120:00:00"
-NUMBER_OF_TASKS=4
+OUTPUT="/dev/null"
+NUMBER_OF_TASKS=32
 NTASKS="--ntasks=$NUMBER_OF_TASKS"
 MEM_PER_CPU="--mem-per-cpu=5G"
 EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
