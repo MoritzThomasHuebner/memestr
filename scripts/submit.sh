7765313c3b30c87089a15a9116f7605252cbf527
Moritz <email@moritz-huebner.de>
2019-01-04 13:59:22 +1100

    ID 030: Only masses, dlogz=0.1, distance marginalization, sample=unif

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-bash IMR_mem_inj_mem_rec.sh                 npoints=5000 alpha=0.1 dlogz=0.1 zero_noise=False sampler=dynesty luminosity_distance=$1 distance_marginalization=True label=IMR_mem_inj_mem_rec
-bash IMR_mem_inj_non_mem_rec.sh             npoints=5000 alpha=0.1 dlogz=0.1 zero_noise=False sampler=dynesty luminosity_distance=$1 distance_marginalization=True label=IMR_mem_inj_non_mem_rec
+bash IMR_mem_inj_mem_rec.sh                 npoints=5000 alpha=0.1 dlogz=0.1 zero_noise=False sampler=dynesty sample=unif luminosity_distance=$1 distance_marginalization=True label=IMR_mem_inj_mem_rec
+bash IMR_mem_inj_non_mem_rec.sh             npoints=5000 alpha=0.1 dlogz=0.1 zero_noise=False sampler=dynesty sample=unif luminosity_distance=$1 distance_marginalization=True label=IMR_mem_inj_non_mem_rec
 #bash IMR_non_mem_inj_non_mem_rec.sh         npoints=5000 walks=200 zero_noise=True sampler=dynesty label=IMR_non_mem_inj_non_mem_rec
 #bash IMR_non_mem_inj_mem_rec.sh             npoints=5000 walks=200 zero_noise=True sampler=dynesty label=IMR_non_mem_inj_mem_rec
 #bash IMR_pure_mem_inj_pure_mem_rec.sh       npoints=4000 zero_noise=False sampler=dynesty time_marginalization=True label=IMR_pure_mem_inj_pure_mem_rec
