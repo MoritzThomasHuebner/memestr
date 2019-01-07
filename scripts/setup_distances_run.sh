451e31491b773122f310abf260a52435e823e557
Moritz <email@moritz-huebner.de>
2019-01-07 18:26:28 +1100

    Added creation of outdir into setup script

--- scripts/setup_distances_run.sh
+++ scripts/setup_distances_run.sh
@@ -3,6 +3,7 @@
 LABEL="${1::-3}"
 PYTHON_COMMAND="import memestr; print(memestr.submit.submitter.find_unallocated_name(name=\"$LABEL\"))"
 OUTDIR=`python -c "${PYTHON_COMMAND}"`
+mkdir ${OUTDIR}
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
 TIME="--time=120:00:00"
