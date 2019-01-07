bfd693d2a1619afed60e22e98e4704763bc07648
Moritz <email@moritz-huebner.de>
2019-01-07 12:45:39 +1100

    Deleted obsolete files

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -26,7 +26,7 @@ import numpy as np
 
 memestr.wrappers.wrappers.run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
                                                          recovery_model=memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
-                                                         outdir='test', npoints=5000, alpha=0.1, dlogz=0.05, zero_noise=True, sampler='dynesty',
+                                                         outdir='test', npoints=500, alpha=0.1, sample='unif', zero_noise=False, sampler='dynesty',
                                                          luminosity_distance=350, distance_marginalization=True)
 
 # memestr.wrappers.wrappers.run_basic_injection_nrsur(injection_model=memestr.core.waveforms.time_domain_nr_sur_waveform_with_memory,
