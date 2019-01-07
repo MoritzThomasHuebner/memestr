6c18940755f6e7c3bfc2ceecef0d3486b1d59644
Moritz <email@moritz-huebner.de>
2019-01-07 17:25:27 +1100

    Added super submit script

---
+++ scripts/super_submit.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+bash submit.sh 200
+sleep 30
+bash submit.sh 250
+sleep 30
+bash submit.sh 300
+sleep 30
+bash submit.sh 350
+sleep 30
+bash submit.sh 400
+sleep 30
+bash submit.sh 450
+sleep 30
+bash submit.sh 500
+sleep 30
+bash submit.sh 550
+sleep 30
+bash submit.sh 600
+sleep 30
+bash submit.sh 650
+sleep 30
+bash submit.sh 700
+sleep 30
+bash submit.sh 750
+sleep 30
+bash submit.sh 800
+sleep 30
+bash submit.sh 850
+sleep 30
+bash submit.sh 900
+sleep 30
+bash submit.sh 950
