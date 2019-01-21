d77590cac121f809634937de29d18960a01ad2be
Moritz <email@moritz-huebner.de>
2019-01-21 14:14:52 +1100

    First shot at getting masses sampled correctly

--- scripts/population.py
+++ scripts/population.py
@@ -1,36 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp2d
 
-from population import models
+
+from memestr.core.population import mass_distribution_no_vt
 
 
 def debug_plots():
-    m_1 = np.linspace(1, 50, 1000)
-    m_2 = np.linspace(1, 50, 1000)
+    m_1 = np.linspace(1, 50, 100)
+    qs = np.linspace(0.01, 1, 10)
+    for q in qs:
+        dataset = dict(m1_source=m_1, q=q)
 
-    dataset = dict(m1_source=m_1, m2_source=m_2)
-
-    a = models.mass_distribution_no_vt(dataset=dataset, alpha=0.4,
-                                                  mmin=5, mmax=41.6, lam=0,
-                                                  mpp=0, sigpp=0, beta=0, delta_m=0)
-    plt.plot(m_1, a)
-    plt.semilogy()
+        prob = mass_distribution_no_vt(dataset=dataset, alpha=9,
+                                       mmin=6.5, mmax=50, lam=0.4,
+                                       mpp=30, sigpp=5, beta=5.8, delta_m=4)
+        plt.plot(m_1, prob)
+        plt.semilogy()
+        plt.ylim(1e-6, 10)
     plt.show()
     plt.clf()
 
 
-def sample_masses_model_a():
-    return sample_masses(alpha=0.4, m_max=41.6, m_min=5)
+m_1 = np.linspace(1, 50, 1000)
+qs = np.linspace(0.01, 1, 1000)
+probs = []
+for q in qs:
+    dataset = dict(m1_source=m_1, q=q)
+    prob = mass_distribution_no_vt(dataset=dataset, alpha=9,
+                                   mmin=6.5, mmax=50, lam=0.4,
+                                   mpp=30, sigpp=5, beta=5.8, delta_m=4)
+    probs.append(prob)
 
 
-def sample_masses_model_b():
-    return sample_masses(alpha=1.6, m_max=42.0, m_min=7.9)
+prob_density = interp2d(x=m_1, y=qs, z=np.array(probs))
+
+print(prob_density)
 
 
+# def sample_masses_model_a():
+#     return sample_masses(alpha=0.4, m_max=41.6, m_min=5)
+#
+#
+# def sample_masses_model_b():
+#     return sample_masses(alpha=1.6, m_max=42.0, m_min=7.9)
+#
+#
 # def sample_masses(alpha, m_max, m_min):
 #     m_1 = np.linspace(1, 50, 1000)
 #     m_2 = np.linspace(1, 50, 1000)
-#     p_m_1 = prob.marginal_pdf(m1=m_1, alpha=alpha, m_min=m_min, m_max=m_max, M_max=100)
+#     p_m_1 = models.prob.marginal_pdf(m1=m_1, alpha=alpha, m_min=m_min, m_max=m_max, M_max=100)
 #     prior_m_1 = bilby.core.prior.Interped(xx=m_1, yy=p_m_1[0], minimum=1, maximum=50)
 #     m_1_sample = prior_m_1.sample()
 #     p_m_2_given_m_1 = prob.joint_pdf(m_1=m_1_sample, m_2=m_2, alpha=alpha, m_min=m_min, m_max=m_max, M_max=100)
