5f499a3f892aa69335cbde339d6b12fa5cb4b2f8
Moritz <email@moritz-huebner.de>
2019-01-26 17:33:10 +1100

    Made the script to figure out expected bayes factors into a module

--- memestr/core/__init__.py
+++ memestr/core/__init__.py
@@ -2,3 +2,4 @@ import memestr.core.parameters
 import memestr.core.submit
 import memestr.core.waveforms
 import memestr.core.population
+import memestr.core.expected_bayes_factor
---
+++ memestr/core/expected_bayes_factor.py
@@ -0,0 +1,64 @@
+import numpy as np
+import bilby as bb
+import memestr as me
+import logging
+import matplotlib.pyplot as plt
+
+
+def expected_bayes_factor(**params):
+    settings = me.core.parameters.AllSettings()
+    parameters = settings.injection_parameters.__dict__
+    parameters.update(params)
+    priors = settings.recovery_priors.proper_dict()
+
+    sampling_frequency = 4096
+    duration = 8
+    start_time = 0
+
+    logger = logging.getLogger('bilby')
+    logger.disabled = True
+
+    luminosity_distances = np.arange(200, 950, 50)
+    log_bfs = []
+
+    for luminosity_distance in luminosity_distances:
+
+        parameters['luminosity_distance'] = luminosity_distance
+        waveform_generator_with_mem = bb.gw.WaveformGenerator(
+            time_domain_source_model=me.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory,
+            sampling_frequency=sampling_frequency,
+            duration=duration,
+            start_time=start_time)
+        waveform_generator_no_mem = bb.gw.WaveformGenerator(
+            time_domain_source_model=me.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
+            sampling_frequency=sampling_frequency,
+            duration=duration,
+            start_time=start_time)
+
+        waveform_generator_with_mem.parameters = parameters
+
+        ifos = [bb.gw.detector.
+                    get_interferometer_with_fake_noise_and_injection(name=name,
+                                                                     injection_parameters=parameters,
+                                                                     waveform_generator=waveform_generator_with_mem,
+                                                                     sampling_frequency=sampling_frequency,
+                                                                     duration=duration, start_time=start_time,
+                                                                     zero_noise=True,
+                                                                     plot=False) for name in ['H1', 'L1', 'V1']]
+
+
+        likelihood_with_mem = \
+            bb.gw.likelihood.GravitationalWaveTransient(interferometers=ifos,
+                                                        waveform_generator=waveform_generator_with_mem,
+                                                        priors=priors, distance_marginalization=False)
+        likelihood_no_mem = bb.gw.likelihood.GravitationalWaveTransient(interferometers=ifos,
+                                                                        waveform_generator=waveform_generator_no_mem,
+                                                                        priors=priors, distance_marginalization=False)
+        likelihood_with_mem.parameters = parameters
+        likelihood_no_mem.parameters = parameters
+
+        log_bf = likelihood_with_mem.log_likelihood() - likelihood_no_mem.log_likelihood()
+        print(str(luminosity_distance) + '\t:' + str(log_bf))
+        log_bfs.append(log_bf)
+
+    return luminosity_distances, log_bfs
