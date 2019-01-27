d490a90500444eb9b5484d2903115d7a0bc88529
Moritz <email@moritz-huebner.de>
2019-01-28 00:01:42 +1100

    Set up for population run

--- scripts/IMR_mem_inj_non_mem_rec.sh
+++ scripts/IMR_mem_inj_non_mem_rec.sh
@@ -3,14 +3,14 @@
 #source setup_default.sh ${0}
 source setup_random_injections.sh ${0}
 
-sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL} ${ARRAY}<<'EOF'
+sbatch ${JOB_NAME} ${OUTPUT} ${TIME} ${NTASKS} ${MEM_PER_CPU} ${CPUS_PER_TASK} ${EMAIL} ${ARRAY}<<EOF
 #!/usr/bin/env bash
 JOB=run_basic_job.py
 SCRIPT=run_basic_injection_imr_phenom
 INJECTION_MODEL=time_domain_IMRPhenomD_waveform_with_memory
 RECOVERY_MODEL=time_domain_IMRPhenomD_waveform_without_memory
 FILENAME="./parameter_sets/\${SLURM_ARRAY_TASK_ID}"
-PARAMS=`cat \$FILENAME`
+PARAMS=\$(cat \$FILENAME)
 srun python \${JOB} ${OUTDIR}/\${SLURM_ARRAY_TASK_ID} \${SCRIPT} \${INJECTION_MODEL} \${RECOVERY_MODEL} \${PARAMS} random_seed=\${SLURM_ARRAY_TASK_ID} $@
 
 #Distance vs evidence
