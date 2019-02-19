f735c423e6a1ef88b3e525b9edc61a2215e327ab
Moritz <email@moritz-huebner.de>
2019-02-19 16:52:20 +1100

    New production run 065 with multiprocessing

--- scripts/setup_random_injections.sh
+++ scripts/setup_random_injections.sh
@@ -6,7 +6,7 @@ OUTDIR=`python -c "${PYTHON_COMMAND}"`
 mkdir ${OUTDIR}
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
-TIME="--time=240:00:00"
+TIME="--time=168:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=4G"
 CPUS_PER_TASK="--cpus-per-task=4"
