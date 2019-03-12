592d50da311f65dc982e2f716da630e149603bda
Moritz <email@moritz-huebner.de>
2019-03-12 18:27:11 +1100

    Set up for log BF vs distance run ID 000 - 015

--- scripts/super_submit.sh
+++ scripts/super_submit.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-bash submit.sh 200
-bash submit.sh 230
-bash submit.sh 262
-bash submit.sh 299
-bash submit.sh 342
-bash submit.sh 391
-bash submit.sh 448
-bash submit.sh 512
-bash submit.sh 586
-bash submit.sh 670
-bash submit.sh 766
-bash submit.sh 876
-bash submit.sh 1002
-bash submit.sh 1147
-bash submit.sh 1311
-bash submit.sh 1500
+bash submit_snrs.sh 200
+bash submit_snrs.sh 230
+bash submit_snrs.sh 262
+bash submit_snrs.sh 299
+bash submit_snrs.sh 342
+bash submit_snrs.sh 391
+bash submit_snrs.sh 448
+bash submit_snrs.sh 512
+bash submit_snrs.sh 586
+bash submit_snrs.sh 670
+bash submit_snrs.sh 766
+bash submit_snrs.sh 876
+bash submit_snrs.sh 1002
+bash submit_snrs.sh 1147
+bash submit_snrs.sh 1311
+bash submit_snrs.sh 1500
