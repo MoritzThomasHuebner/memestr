f254d7a970272ff8491be1c1874abd2324dfb986
Moritz <email@moritz-huebner.de>
2019-03-19 17:01:24 +1100

    Setup do redo 000 to 015

--- scripts/setup_random_injections.sh
+++ scripts/setup_random_injections.sh
@@ -6,9 +6,9 @@ OUTDIR=`python -c "${PYTHON_COMMAND}"`
 mkdir ${OUTDIR}
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
-TIME="--time=168:00:00"
+TIME="--time=100:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=1G"
-CPUS_PER_TASK="--cpus-per-task=4"
+CPUS_PER_TASK="--cpus-per-task=8"
 ARRAY="--array=0-7"
 EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
