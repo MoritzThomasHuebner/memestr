14efa352d8cb5d511bfa691c6245938b5c7b9ee4
Moritz <email@moritz-huebner.de>
2019-03-29 17:16:26 +1100

    Fixed outdir

--- scripts/setup_random_injections.sh
+++ scripts/setup_random_injections.sh
@@ -2,7 +2,7 @@
 
 LABEL="${1::-3}"
 PYTHON_COMMAND="import memestr; print(memestr.core.submit.find_unallocated_name(name=\"$LABEL\"))"
-OUTDIR="${2}"
+OUTDIR=(${2//outdir_base=/})
 #OUTDIR=`python -c "${PYTHON_COMMAND}"`
 mkdir - ${OUTDIR}
 JOB_NAME="--job-name=$LABEL"
