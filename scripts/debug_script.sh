5978a64aafae038de566950c292e682bdb0099c1
Moritz <email@moritz-huebner.de>
2019-04-05 16:44:52 +1100

    Setup benchmarking run

--- scripts/debug_script.sh
+++ scripts/debug_script.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH job-name="test"
-#SBATCH output="test.txt"
-#SBATCH time=1:00:00
-#SBATCH ntasks=1
-#SBATCH mem-per-cpu=1G
-#SBATCH cpus-per-task=4
+#SBATCH --job-name="test"
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=4
 srun python debug_script.py
