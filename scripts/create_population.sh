12d6b4116dc192988f0dd4ee4fdbeac5c1746dec
Moritz <email@moritz-huebner.de>
2019-04-15 16:04:57 +1000

    Added batch

---
+++ scripts/create_population.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=pop
+#SBATCH --ntasks=1
+#SBATCH --time=6:00:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
+
+srun python create_population.py
