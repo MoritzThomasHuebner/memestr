4a1394c949a8e7f7a9055ddf181bdb2f084a0bf1
Moritz <email@moritz-huebner.de>
2019-04-16 10:45:21 +1000

    set up for another run

--- scripts/create_population.py
+++ scripts/create_population.py
@@ -102,7 +102,7 @@ import matplotlib.pyplot as plt
 
 
 def read_snr(filename):
-    ifos = bilby.gw.detector.InterferometerList.from_hdf5('parameter_sets/' + str(filename))
+    ifos = bilby.gw.detector.InterferometerList.from_hdf5('parameter_sets/' + str(filename) + '_H1L1V1')
     return ifos.meta_data['matched_filter_SNR']
 
 
