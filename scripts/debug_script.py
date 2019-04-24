49f0930c8ebf210b3ec3da9596e87bfc898a2884
Moritz <email@moritz-huebner.de>
2019-04-24 18:00:06 +1000

    Removed an echo statement

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -21,7 +21,7 @@ memestr.wrappers.injection_recovery.\
     run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
                                    recovery_model=memestr.core.waveforms.frequency_domain_IMRPhenomD_waveform_without_memory,
                                    outdir='test_2', alpha=0.1, zero_noise=True, distance_marginalization=True,
-                                   time_marginalization=False, luminosity_distance=1200.0, nthreads=1, sampler='dynesty',
+                                   time_marginalization=False, luminosity_distance=1200.0, nthreads=1, sampler='cpnest',
                                    nlive=400, maxmcmc=1000, duration=16, random_seed=42, dlogz=1,
                                    sampling_frequency=2048, filename_base='0')
 import cpnest.cpnest
