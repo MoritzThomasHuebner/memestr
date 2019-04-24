419e414d21af14fbe9dce463d162c0fc802c0219
Moritz <email@moritz-huebner.de>
2019-04-24 14:26:02 +1000

    Set up for injecting wrapped NRHybSur waveforms

--- scripts/create_population.sh
+++ scripts/create_population.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=pop
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
