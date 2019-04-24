8f5ba0c1a33a7476859f0ded75ed7215c7a79f79
Moritz <email@moritz-huebner.de>
2019-04-24 14:16:45 +1000

    Set up for injecting wrapped NRHybSur waveforms

--- scripts/plot_waveforms.py
+++ scripts/plot_waveforms.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 settings = memestr.core.parameters.AllSettings()
-models = [memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
-          memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
-          memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory]
+# models = [memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform,
+#           memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory,
+#           memestr.core.waveforms.time_domain_IMRPhenomD_waveform_with_memory]
+models = [memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
+          memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory]
 print(settings.injection_parameters)
 
 
@@ -22,14 +24,14 @@ def plot_waveform(td_model, mass_ratio):
                                                     waveform_arguments=settings.waveform_arguments.__dict__,
                                                     **settings.waveform_data.__dict__)
     psd = bilby.gw.detector.PowerSpectralDensity.from_aligo()
-    waveform_generator.time_domain_source_model = memestr.core.waveforms.time_domain_IMRPhenomD_waveform_without_memory
+    waveform_generator.time_domain_source_model = memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory_wrapped
     plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['plus']),
-             label='Oscillatory')
-    waveform_generator.time_domain_source_model = memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform
+             label='Oscillatory + Memory')
+    # waveform_generator.time_domain_source_model = memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform
 
-    plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['plus']), label='Memory')
+    # plt.plot(waveform_generator.frequency_array, np.abs(waveform_generator.frequency_domain_strain()['plus']), label='Memory')
     plt.plot(psd.frequency_array, psd.asd_array, label='aLIGO ASD')
-    # plt.title('alpha = ' + str(settings.waveform_arguments.alpha))
+    plt.title('alpha = ' + str(settings.waveform_arguments.alpha))
     plt.xlabel('frequency [Hz]')
     plt.ylabel('h')
     plt.xlim(10, 1000)
@@ -53,7 +55,7 @@ def plot_waveform(td_model, mass_ratio):
     waveform_generator.time_domain_source_model = memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform
     plt.plot(waveform_generator.time_array, waveform_generator.time_domain_strain()['plus'], color='blue',
              label='memory strain')
-    plt.xlim(1.52, 1.63)
+    # plt.xlim(1.52, 1.63)
     plt.xlabel('$t [\mathrm{s}]$')
     plt.ylabel('h')
     plt.legend()
@@ -75,7 +77,8 @@ def plot_waveform(td_model, mass_ratio):
 
 network_snrs = []
 for mass_ratio in [1]:  # , 0.9, 0.8, 0.7, 0.6, 0.5, 0.4]:
-    network_snrs.append(plot_waveform(memestr.core.waveforms.time_domain_IMRPhenomD_memory_waveform, mass_ratio))
+    network_snrs.append(plot_waveform(memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
+                                      mass_ratio))
 plt.show()
 
 print(network_snrs)
