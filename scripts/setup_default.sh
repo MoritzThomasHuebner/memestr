ce952d68149bb4502f006be880c9e0e10c68d938
Moritz <email@moritz-huebner.de>
2019-04-24 17:18:51 +1000

    Set up to run full PE

--- scripts/setup_default.sh
+++ scripts/setup_default.sh
@@ -7,7 +7,7 @@ OUTDIR=${2//outdir_base=/}_${LABEL}
 mkdir -p ${OUTDIR}
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
-TIME="--time=120:00:00"
+TIME="--time=168:00:00"
 NTASKS="--ntasks=1"
 MEM_PER_CPU="--mem-per-cpu=1G"
 CPUS_PER_TASK="--cpus-per-task=2"
