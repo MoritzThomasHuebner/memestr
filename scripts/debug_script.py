eed5d57a5b5d396b9955139b3593182d3d738a62
Moritz <email@moritz-huebner.de>
2019-04-29 15:32:00 +1000

    Added more RAM

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -20,10 +20,11 @@ import memestr
 memestr.wrappers.injection_recovery.\
     run_basic_injection_imr_phenom(injection_model=memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
                                    recovery_model=memestr.core.waveforms.frequency_domain_IMRPhenomD_waveform_without_memory,
-                                   outdir='test_2', alpha=0.1, zero_noise=True, distance_marginalization=True,
-                                   time_marginalization=False, luminosity_distance=1200.0, nthreads=1, sampler='cpnest',
-                                   nlive=400, maxmcmc=1000, duration=16, random_seed=42, dlogz=1,
-                                   sampling_frequency=2048, filename_base='0')
+                                   outdir='100_IMR_mem_inj_non_mem_rec_cp', alpha=0.1, zero_noise=True,
+                                   distance_marginalization=True,
+                                   time_marginalization=False, luminosity_distance=1200.0, sampler='dynesty',
+                                   nlive=400, duration=16, random_seed=42, dlogz=0.1,
+                                   sampling_frequency=2048, resume=True, clean=False)
 import cpnest.cpnest
 
 # memestr.wrappers.wrappers.run_basic_injection_nrsur(injection_model=memestr.core.waveforms.time_domain_nr_sur_waveform_with_memory,
