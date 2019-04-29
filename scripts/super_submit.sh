c338ea1d142cf6a0cf47c6e4bd2ae31019684447
Moritz <email@moritz-huebner.de>
2019-04-29 17:14:55 +1000

    Set up for redoing initial pilot study

--- scripts/super_submit.sh
+++ scripts/super_submit.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-bash submit_snrs.sh 200 032
-bash submit_snrs.sh 230 033
-bash submit_snrs.sh 262 034
-bash submit_snrs.sh 299 035
-bash submit_snrs.sh 342 036
-bash submit_snrs.sh 391 037
-bash submit_snrs.sh 448 038
-bash submit_snrs.sh 512 039
-bash submit_snrs.sh 586 040
-bash submit_snrs.sh 670 041
-bash submit_snrs.sh 766 042
-bash submit_snrs.sh 876 043
-bash submit_snrs.sh 1002 044
-bash submit_snrs.sh 1147 045
-bash submit_snrs.sh 1311 046
-bash submit_snrs.sh 1500 047
+for i in {100..132}
+do
+    sbatch submit.sh ${i}
+done
