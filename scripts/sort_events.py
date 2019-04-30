f87a882c6b13f614e76132eae5bb9941db6e1b9b
Moritz <email@moritz-huebner.de>
2019-04-30 15:54:33 +1000

    Added sort events

--- scripts/sort_events.py
+++ scripts/sort_events.py
@@ -2,12 +2,17 @@ import bilby as bb
 import numpy as np
 import pandas as pd
 
-snrs = pd.DataFrame(['id', 'snr'])
+data = pd.DataFrame(['id', 'snr'])
+ids = []
+snrs = []
 
 for i in range(5):
     ifos = bb.gw.detector.InterferometerList.from_hdf5('parameter_sets/' + str(i) + '_H1L1V1.h5')
     best_snrs = [ifo.meta_data['matched_filter_SNR'].real for ifo in ifos]
     network_snr = np.sqrt(np.sum([snr ** 2 for snr in best_snrs]))
-    snrs.append(dict(id=i, snr=network_snr))
+    ids.append(i)
+    snrs.append(network_snr)
 
-print(snrs)
+data.id = ids
+data.snr = snrs
+print(data)
