1386f02ead5b24433a55738384d5803dd761631a
Moritz <email@moritz-huebner.de>
2019-04-30 15:59:29 +1000

    Added sort events

--- scripts/sort_events.py
+++ scripts/sort_events.py
@@ -5,7 +5,7 @@ import pandas as pd
 ids = []
 snrs = []
 
-for i in range(100):
+for i in range(1000):
     ifos = bb.gw.detector.InterferometerList.from_hdf5('parameter_sets/' + str(i) + '_H1L1V1.h5')
     best_snrs = [ifo.meta_data['matched_filter_SNR'].real for ifo in ifos]
     network_snr = np.sqrt(np.sum([snr ** 2 for snr in best_snrs]))
