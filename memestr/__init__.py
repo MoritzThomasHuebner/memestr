7f7498f6f3ad3961ecb08a931f8eab8caac68421
Moritz <email@moritz-huebner.de>
2019-04-30 16:50:17 +1000

    Added first batch of production runs with cpnest

--- memestr/__init__.py
+++ memestr/__init__.py
@@ -14,6 +14,5 @@ models = dict(
 scripts = dict(
     run_production_injection_imr_phenom=wrappers.injection_recovery.run_production_injection_imr_phenom,
     run_basic_injection_imr_phenom=wrappers.injection_recovery.run_basic_injection_imr_phenom,
-    run_basic_injection_nrsur=wrappers.injection_recovery.run_basic_injection_nrsur,
     run_basic_injection=wrappers.injection_recovery.run_basic_injection
 )
--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -85,38 +85,6 @@ def update_kwargs(default_kwargs, kwargs):
     return new_kwargs
 
 
-def run_basic_injection_nrsur(injection_model, recovery_model, outdir, **kwargs):
-    start_time = -0.5
-    end_time = 0.0  # 0.029
-    duration = end_time - start_time
-
-    priors = dict()
-    injection_parameters = InjectionParameters.init_with_updated_kwargs(**kwargs)
-    for key in injection_parameters.__dict__:
-        priors['prior_' + key] = injection_parameters.__dict__[key]
-    # priors['prior_total_mass'] = bilby.core.prior.Uniform(minimum=50, maximum=70, latex_label="$M_{tot}$")
-    # priors['prior_mass_ratio'] = bilby.core.prior.Uniform(minimum=1, maximum=2, latex_label="$q$")
-    # priors['prior_luminosity_distance'] = bilby.gw.prior.UniformComovingVolume(name='luminosity_distance', minimum=1e1,
-    #                                                                            maximum=5e3, latex_label="$L_D$")
-    # priors['prior_inc'] = bilby.core.prior.Uniform(minimum=0, maximum=np.pi, latex_label="$\iota$")
-    priors['prior_phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=np.pi, latex_label="$\phi$")
-    # priors['prior_ra'] = bilby.core.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, latex_label="$RA$")
-    # priors['prior_dec'] = bilby.core.prior.Cosine(name='dec', latex_label="$DEC$")
-    priors['prior_psi'] = bilby.core.prior.Uniform(name='psi', minimum=0, maximum=np.pi, latex_label="$\psi$")
-    # priors['prior_geocent_time'] = bilby.core.prior.Uniform(1126259642.322, 1126259642.522, name='geocent_time')
-    nr_sur_kwargs = dict(
-        start_time=start_time,
-        duration=duration,
-        label='NRSur',
-        new_seed=True,
-        zero_noise=False
-    )
-    nr_sur_kwargs.update(priors)
-    nr_sur_kwargs.update(kwargs)
-    return run_basic_injection(injection_model=injection_model, recovery_model=recovery_model, outdir=outdir,
-                               **nr_sur_kwargs)
-
-
 def run_basic_injection_imr_phenom(injection_model, recovery_model, outdir, **kwargs):
     priors = dict()
     injection_parameters = InjectionParameters.init_with_updated_kwargs(**kwargs)
@@ -206,8 +174,11 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
 
     bilby.core.utils.setup_logger(outdir=outdir, label=settings.sampler_settings.label)
 
+    filename_base = str(settings.detector_settings.filename_base).replace('dynesty', '')
+    filename_base = str(filename_base).replace('cpnest', '')
+    filename_base = str(filename_base).replace('pypolychord', '')
     ifos = bilby.gw.detector.InterferometerList.from_hdf5('parameter_sets/' +
-                                                          str(settings.detector_settings.filename_base) +
+                                                          str(filename_base) +
                                                           '_H1L1V1.h5')
     waveform_generator = bilby.gw.WaveformGenerator(frequency_domain_source_model=recovery_model,
                                                     parameters=settings.injection_parameters.__dict__,
