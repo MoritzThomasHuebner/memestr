b5a40bafafe42d442acb6f7c7b050d3318c2cf62
Moritz <email@moritz-huebner.de>
2019-05-01 14:49:18 +1000

    Added another debug script

---
+++ scripts/debug_script2.py
@@ -0,0 +1,14 @@
+import memestr
+
+memestr.wrappers.injection_recovery. \
+    run_production_injection_imr_phenom(
+        injection_model=memestr.core.waveforms.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
+        recovery_model=memestr.core.waveforms.frequency_domain_IMRPhenomD_waveform_without_memory,
+        filename_base=0,
+        outdir='test_production', label='IMR_mem_inj_non_mem_rec',
+        alpha=0.1, distance_marginalization=False,
+        time_marginalization=False, phase_marginalization=False,
+        sampler='dynesty',
+        nlive=400, duration=16, random_seed=42, dlogz=0.1,
+        sampling_frequency=2048, resume=False, clean=True)
+
