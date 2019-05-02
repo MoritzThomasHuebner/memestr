51c8ecea192817b9d2f42077e2e5e382134cf2fa
Moritz <email@moritz-huebner.de>
2019-05-02 20:29:11 +1000

    Set up to run more dynesty stuff

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-for i in {0..15}
+#for i in {0..15}
+#do
+#    sbatch production_submit.sh ${i}_dynesty
+#done
+for i in {16..180}
 do
     sbatch production_submit.sh ${i}_dynesty
 done
 
-for i in {970..999}
-do
-    sbatch production_submit.sh ${i}_dynesty
-done
+#for i in {970..999}
+#do
+#    sbatch production_submit.sh ${i}_dynesty
+#done
