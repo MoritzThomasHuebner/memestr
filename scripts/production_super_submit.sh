446c7baebb4877b0f2204879097bc6ef75851a02
Moritz <email@moritz-huebner.de>
2019-05-02 12:22:08 +1000

    Set up for dynesty runs

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -2,10 +2,10 @@
 
 for i in {0..15}
 do
-    sbatch production_submit.sh ${i}_cpnest
+    sbatch production_submit.sh ${i}_dynesty
 done
 
-#for i in {970..999}
-#do
-#    sbatch production_submit.sh ${i}_pypolychord
-#done
+for i in {970..999}
+do
+    sbatch production_submit.sh ${i}_dynesty
+done
