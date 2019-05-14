122ec86e5b58a78a0ec7a4c71fafec6c82eac9a1
Moritz <email@moritz-huebner.de>
2019-05-14 22:07:31 +1000

    Finalizing reweighting

--- memestr/core/postprocessing.py
+++ memestr/core/postprocessing.py
@@ -211,9 +211,8 @@ def reweigh_by_two_likelihoods(posterior, likelihood_memory, likelihood_no_memor
         sample = dict()
         for parameter in ['total_mass', 'mass_ratio', 'inc', 'luminosity_distance',
                           'phase', 'ra', 'dec', 'psi', 'geocent_time', 's13', 's23']:
-            sample[parameter] = posterior.iloc[i][parameter]
-        likelihood_memory.parameters = sample
-        likelihood_no_memory.parameters = sample
+            likelihood_memory[parameter] = posterior.iloc[i][parameter]
+            likelihood_no_memory[parameter] = posterior.iloc[i][parameter]
         weight = likelihood_memory.log_likelihood() - likelihood_no_memory.log_likelihood()
         weights.append(weight)
     return logsumexp(weights)
