471bce45a4a32996f624dcd9050a85b8ae8dce65
Moritz <email@moritz-huebner.de>
2019-05-15 15:41:22 +1000

    Finalizing reweighting

--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -271,7 +271,7 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
     # time_and_phase_shifted_result.plot_corner(parameters=params)
 
     original_result = bilby.result.read_in_result(filename=str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_result.json')
-    sample_file = str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_result.json'
+    sample_file = str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_equal_weights.txt'
     samples = np.loadtxt(sample_file)
     log_likelihoods = samples[:, 1]  # extract second column
     original_result.samples['log_likelihood'] = log_likelihoods
