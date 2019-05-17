ed29281aa39bbd869db4df86bfa493d0c4b8cd49
Moritz <email@moritz-huebner.de>
2019-05-17 12:42:49 +1000

    Fixed some things

--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -233,28 +233,30 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
     np.random.seed(int(time.time()))
     logger.info('Injection Parameters')
     logger.info(str(settings.injection_parameters))
-    result = bilby.core.sampler.run_sampler(likelihood=likelihood_imr_phenom,
-                                            priors=priors,
-                                            injection_parameters=deepcopy(settings.injection_parameters.__dict__),
-                                            outdir=outdir,
-                                            save=True,
-                                            verbose=True,
-                                            random_seed=np.random.randint(0, 100000),
-                                            sampler=settings.sampler_settings.sampler,
-                                            npoints=settings.sampler_settings.npoints,
-                                            label=settings.sampler_settings.label,
-                                            clean=settings.sampler_settings.clean,
-                                            nthreads=settings.sampler_settings.nthreads,
-                                            maxmcmc=settings.sampler_settings.maxmcmc,
-                                            resume=settings.sampler_settings.resume,
-                                            save_bounds=False,
-                                            check_point_plot=True,
-                                            n_check_point=1000)
-    result.save_to_file()
-    logger.info(str(result))
-    result.posterior = bilby.gw.conversion. \
-        generate_posterior_samples_from_marginalized_likelihood(result.posterior, likelihood_imr_phenom)
-    result.save_to_file()
+    # result = bilby.core.sampler.run_sampler(likelihood=likelihood_imr_phenom,
+    #                                         priors=priors,
+    #                                         injection_parameters=deepcopy(settings.injection_parameters.__dict__),
+    #                                         outdir=outdir,
+    #                                         save=True,
+    #                                         verbose=True,
+    #                                         random_seed=np.random.randint(0, 100000),
+    #                                         sampler=settings.sampler_settings.sampler,
+    #                                         npoints=settings.sampler_settings.npoints,
+    #                                         label=settings.sampler_settings.label,
+    #                                         clean=settings.sampler_settings.clean,
+    #                                         nthreads=settings.sampler_settings.nthreads,
+    #                                         maxmcmc=settings.sampler_settings.maxmcmc,
+    #                                         resume=settings.sampler_settings.resume,
+    #                                         save_bounds=False,
+    #                                         check_point_plot=True,
+    #                                         n_check_point=1000)
+    # result.save_to_file()
+    # logger.info(str(result))
+    # result.posterior = bilby.gw.conversion. \
+    #     generate_posterior_samples_from_marginalized_likelihood(result.posterior, likelihood_imr_phenom)
+    # result.save_to_file()
+    result = bilby.result.read_in_result(filename=str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_result.json')
+
     params = deepcopy(settings.injection_parameters.__dict__)
     del params['s11']
     del params['s12']
@@ -265,7 +267,8 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
     # result.plot_corner(lionize=settings.other_settings.lionize, parameters=params)
     try:
         time_and_phase_shifted_result = bilby.result.read_in_result(filename=str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/time_and_phase_shifted_result.json')
-    except OSError:
+    except OSError as e:
+        logger.warning(e)
         time_and_phase_shifted_result = adjust_phase_and_geocent_time_complete_posterior_proper(result=result,
                                                                                                 ifo=ifos[0],
                                                                                                 verbose=True)
