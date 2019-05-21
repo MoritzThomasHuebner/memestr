0f6518798725636a7bae5587ccec15d8c1e8a3be
Moritz <email@moritz-huebner.de>
2019-05-21 16:48:42 +1000

    New try

--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -322,6 +322,7 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
                               'phase', 'ra', 'dec', 'psi', 'geocent_time', 's13', 's23']:
                 likelihood_imr_phenom.parameters[parameter] = result.posterior.iloc[i][parameter]
             log_l_ratios.append(likelihood_imr_phenom_unmarginalized.log_likelihood_ratio())
+            print(log_l_ratios[i])
 
         result.posterior.log_likelihood = log_l_ratios
         time_and_phase_shifted_result.posterior.log_likelihood = log_l_ratios
