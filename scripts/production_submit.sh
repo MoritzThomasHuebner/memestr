b4d90350f91001bbfac7553648086ddc8d56de15
Moritz <email@moritz-huebner.de>
2019-05-22 13:12:00 +1000

    Improved initial guesses

--- scripts/production_submit.sh
+++ scripts/production_submit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash production_IMR_non_mem_rec.sh              outdir_base=${1} filename_base=${1} npoints=2000 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=True resume=True sampler=pypolychord duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
+bash production_IMR_non_mem_rec.sh              outdir_base=${1} filename_base=${1} npoints=600 alpha=0.1 distance_marginalization=False time_marginalization=False phase_marginalization=False resume=True sampler=pypolychord duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
