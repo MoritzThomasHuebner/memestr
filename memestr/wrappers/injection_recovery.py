895dd9f4c2eb639d45d1bc4158ed2948b0a62dd3
Moritz <email@moritz-huebner.de>
2019-05-27 16:18:50 +1000

    Try fixing phase shift

--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -262,6 +262,11 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
     # result.save_to_file()
     # logger.info(str(result))
     result = bilby.result.read_in_result(filename=str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_result.json')
+    ref_distances = np.ones(len(result.posterior)) * 10.
+    ref_phases = np.zeros(len(result.posterior))
+    result.posterior.luminosity_distance = ref_distances
+    result.posterior.phase = ref_phases
+
     result.posterior = bilby.gw.conversion. \
         generate_posterior_samples_from_marginalized_likelihood(result.posterior, likelihood_imr_phenom)
     result.save_to_file()
@@ -287,7 +292,7 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
         time_and_phase_shifted_result, shifts, maximum_overlaps = adjust_phase_and_geocent_time_complete_posterior_proper(
             result=result,
             ifo=ifos[0],
-            verbose=True)
+            verbose=False)
         np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/shifts.txt', shifts)
         np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/maximum_overlaps.txt',
                    maximum_overlaps)
