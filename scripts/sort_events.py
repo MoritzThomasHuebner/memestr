a8871513c2a1a1349928887d10f46f20471b2a2d
Moritz <email@moritz-huebner.de>
2019-05-28 14:08:05 +1000

    Reverted

--- scripts/sort_events.py
+++ scripts/sort_events.py
@@ -14,26 +14,25 @@ for i in range(1000):
     network_snr = np.sqrt(np.sum([snr ** 2 for snr in best_snrs]))
     ids.append(i)
     snrs.append(network_snr)
-
+#
 data = pd.DataFrame({'ids': ids, 'snr': snrs})
 data = data.sort_values(by='snr')
 np.savetxt('snrs.txt', data.values)
-
-plt.hist(snrs, bins=32)
-plt.savefig('snrs')
-plt.clf()
-
-# for new_id, old_id in enumerate(data.ids):
-#     src = 'parameter_sets/' + str(old_id) + '_H1L1V1.h5'
-#     dst = 'parameter_sets/' + str(new_id) + '_H1L1V1_new.h5'
-#     copyfile(src, dst)
-#     src = 'parameter_sets/' + str(old_id)
-#     dst = 'parameter_sets/' + str(new_id) + '_new'
-#     copyfile(src, dst)
 #
-# for old_id in range(len(data.ids)):
-#     os.remove('parameter_sets/' + str(old_id))
-#     os.remove('parameter_sets/' + str(old_id) + '_H1L1V1.h5')
-#     os.rename('parameter_sets/' + str(old_id) + '_new', 'parameter_sets/' + str(old_id))
-#     os.rename('parameter_sets/' + str(old_id) + '_H1L1V1_new.h5', 'parameter_sets/' + str(old_id) + '_H1L1V1.h5')
+# plt.hist(snrs, bins=32)
+# plt.savefig('snrs')
+# plt.clf()
 #
+for new_id, old_id in enumerate(data.ids):
+    src = 'parameter_sets/' + str(old_id) + '_H1L1V1.h5'
+    dst = 'parameter_sets/' + str(new_id) + '_H1L1V1_new.h5'
+    copyfile(src, dst)
+    src = 'parameter_sets/' + str(old_id)
+    dst = 'parameter_sets/' + str(new_id) + '_new'
+    copyfile(src, dst)
+
+for old_id in range(len(data.ids)):
+    os.remove('parameter_sets/' + str(old_id))
+    os.remove('parameter_sets/' + str(old_id) + '_H1L1V1.h5')
+    os.rename('parameter_sets/' + str(old_id) + '_new', 'parameter_sets/' + str(old_id))
+    os.rename('parameter_sets/' + str(old_id) + '_H1L1V1_new.h5', 'parameter_sets/' + str(old_id) + '_H1L1V1.h5')
