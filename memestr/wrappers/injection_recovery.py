7a6323d137eaebc6c0b7a09a5b228d4a06cf22d9
Moritz <email@moritz-huebner.de>
2019-05-31 19:14:08 +1000

    Some things got renamed

--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -15,9 +15,9 @@ from memestr.core.postprocessing import adjust_phase_and_geocent_time_complete_p
     reweigh_by_two_likelihoods, reweigh_by_likelihood
 from memestr.core.submit import get_injection_parameter_set
 from memestr.core.utils import get_ifo
-from memestr.core.waveforms import frequency_domain_nr_hyb_sur_waveform_with_memory_wrapped
+from memestr.core.waveforms import frequency_domain_nr_hyb_sur_waveform_with_memory_wrapped, \
+    time_domain_nr_hyb_sur_waveform_with_memory_wrapped
 from memestr.core.waveforms import frequency_domain_nr_hyb_sur_waveform_without_memory_wrapped_no_shift_return
-from memestr.core.waveforms import time_domain_nr_hyb_sur_waveform_with_memory_wrapped
 from memestr.core.waveforms import time_domain_nr_hyb_sur_waveform_without_memory_wrapped_no_shift_return
 
 
@@ -171,30 +171,30 @@ def run_production_injection_imr_phenom(injection_model, recovery_model, outdir,
         minimum=np.maximum(injection_parameters['total_mass'] - 20, 15),
         maximum=injection_parameters['total_mass'] + 30,
         latex_label="$M_{tot}$")
-    priors['prior_mass_ratio'] = bilby.core.prior.Uniform(
-        minimum=np.maximum(injection_parameters['mass_ratio'] - 0.5, 0.4),
-        maximum=1,
-        latex_label="$q$")
+    # priors['prior_mass_ratio'] = bilby.core.prior.Uniform(
+    #     minimum=np.maximum(injection_parameters['mass_ratio'] - 0.5, 0.4),
+    #     maximum=1,
+    #     latex_label="$q$")
     priors['prior_luminosity_distance'] = bilby.gw.prior.UniformComovingVolume(minimum=10,
                                                                                maximum=5000,
                                                                                latex_label="$L_D$",
                                                                                name='luminosity_distance')
-    priors['prior_inc'] = bilby.core.prior.Sine(latex_label="$\\theta_{jn}$")
-    priors['prior_ra'] = bilby.core.prior.Uniform(minimum=0, maximum=2 * np.pi, latex_label="$RA$")
-    priors['prior_dec'] = bilby.core.prior.Cosine(latex_label="$DEC$")
+    # priors['prior_inc'] = bilby.core.prior.Sine(latex_label="$\\theta_{jn}$")
+    # priors['prior_ra'] = bilby.core.prior.Uniform(minimum=0, maximum=2 * np.pi, latex_label="$RA$")
+    # priors['prior_dec'] = bilby.core.prior.Cosine(latex_label="$DEC$")
     priors['prior_phase'] = bilby.core.prior.Uniform(minimum=0,
                                                      maximum=2 * np.pi,
                                                      latex_label="$\phi$")
-    priors['prior_psi'] = bilby.core.prior.Uniform(minimum=0,
-                                                   maximum=np.pi,
-                                                   latex_label="$\psi$")
+    # priors['prior_psi'] = bilby.core.prior.Uniform(minimum=0,
+    #                                                maximum=np.pi,
+    #                                                latex_label="$\psi$")
     priors['prior_geocent_time'] = bilby.core.prior.Uniform(minimum=injection_parameters['geocent_time'] - 0.1,
                                                             maximum=injection_parameters['geocent_time'] + 0.1,
                                                             latex_label='$t_c$')
-    priors['prior_s13'] = bilby.gw.prior.AlignedSpin(name='s13', a_prior=bilby.core.prior.Uniform(0.0, 0.5),
-                                                     latex_label='s13')
-    priors['prior_s23'] = bilby.gw.prior.AlignedSpin(name='s23', a_prior=bilby.core.prior.Uniform(0.0, 0.5),
-                                                     latex_label='s23')
+    # priors['prior_s13'] = bilby.gw.prior.AlignedSpin(name='s13', a_prior=bilby.core.prior.Uniform(0.0, 0.5),
+    #                                                  latex_label='s13')
+    # priors['prior_s23'] = bilby.gw.prior.AlignedSpin(name='s23', a_prior=bilby.core.prior.Uniform(0.0, 0.5),
+    #                                                  latex_label='s23')
 
     imr_phenom_kwargs = dict(
         label='IMRPhenomD'
@@ -243,26 +243,29 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
     np.random.seed(int(time.time()))
     logger.info('Injection Parameters')
     logger.info(str(settings.injection_parameters))
-    # result = bilby.core.sampler.run_sampler(likelihood=likelihood_imr_phenom,
-    #                                         priors=priors,
-    #                                         injection_parameters=deepcopy(settings.injection_parameters.__dict__),
-    #                                         outdir=outdir,
-    #                                         save=True,
-    #                                         verbose=True,
-    #                                         random_seed=np.random.randint(0, 100000),
-    #                                         sampler=settings.sampler_settings.sampler,
-    #                                         npoints=settings.sampler_settings.npoints,
-    #                                         label=settings.sampler_settings.label,
-    #                                         clean=settings.sampler_settings.clean,
-    #                                         nthreads=settings.sampler_settings.nthreads,
-    #                                         maxmcmc=settings.sampler_settings.maxmcmc,
-    #                                         resume=settings.sampler_settings.resume,
-    #                                         save_bounds=False,
-    #                                         check_point_plot=True,
-    #                                         n_check_point=1000)
-    # result.save_to_file()
-    # logger.info(str(result))
-    result = bilby.result.read_in_result(filename=str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_result.json')
+    result = bilby.core.sampler.run_sampler(likelihood=likelihood_imr_phenom,
+                                            priors=priors,
+                                            injection_parameters=deepcopy(settings.injection_parameters.__dict__),
+                                            outdir=outdir,
+                                            save=True,
+                                            verbose=True,
+                                            random_seed=np.random.randint(0, 100000),
+                                            sampler=settings.sampler_settings.sampler,
+                                            npoints=settings.sampler_settings.npoints,
+                                            label=settings.sampler_settings.label,
+                                            clean=settings.sampler_settings.clean,
+                                            nthreads=settings.sampler_settings.nthreads,
+                                            maxmcmc=settings.sampler_settings.maxmcmc,
+                                            resume=settings.sampler_settings.resume,
+                                            save_bounds=False,
+                                            check_point_plot=True,
+                                            n_check_point=1000)
+    result.save_to_file()
+    logger.info(str(result))
+    import sys
+    sys.exit(0)
+    # result = bilby.result.read_in_result(filename=str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_result.json')
+    result = bilby.result.read_in_result('0_dynesty/IMR_mem_inj_non_mem_rec_result.json')
     logger.info(result.posterior.columns.values)
     result.posterior.columns.drop(labels=['luminosity_distance', 'phase'])
     result.posterior['luminosity_distance'] = pd.Series(np.ones(len(result.posterior)) * 10.)
@@ -270,13 +273,21 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
 
     result.posterior = bilby.gw.conversion. \
         generate_posterior_samples_from_marginalized_likelihood(result.posterior, likelihood_imr_phenom)
-    # result.save_to_file()
+    result.label = 'reconstructed_result'
+    result.save_to_file()
 
     params = deepcopy(settings.injection_parameters.__dict__)
     del params['s11']
     del params['s12']
     del params['s21']
     del params['s22']
+    del params['mass_ratio']
+    del params['inc']
+    del params['ra']
+    del params['dec']
+    del params['psi']
+    del params['s13']
+    del params['s23']
     del params['random_injection_parameters']
     # params = dict(phase=0.6674848916080516, geocent_time=13.567036458124411)
     result.plot_corner(lionize=settings.other_settings.lionize, parameters=params)
@@ -294,27 +305,27 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
             result=result,
             ifo=ifos[0],
             verbose=False)
-        np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/shifts.txt', shifts)
-        np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/maximum_overlaps.txt',
-                   maximum_overlaps)
+        # np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/shifts.txt', shifts)
+        # np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/maximum_overlaps.txt', maximum_overlaps)
+        np.savetxt('0_dynesty/shifts.txt', shifts)
+        np.savetxt('0_dynesty/maximum_overlaps.txt', maximum_overlaps)
         time_and_phase_shifted_result.label = 'time_and_phase_shifted'
         time_and_phase_shifted_result.save_to_file()
         time_and_phase_shifted_result.plot_corner(parameters=deepcopy(params))
 
     time_and_phase_shifted_result_copy = deepcopy(time_and_phase_shifted_result)
 
-    sample_file = str(
-        filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_equal_weights.txt'
-    samples = np.loadtxt(sample_file)
-    log_likelihoods = - 0.5 * samples[:, 1]  # extract second column
+    # sample_file = str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/IMR_mem_inj_non_mem_rec_equal_weights.txt'
+    # samples = np.loadtxt(sample_file)
+    # log_likelihoods = - 0.5 * samples[:, 1]  # extract second column
 
-    np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/log_likelihoods.txt', log_likelihoods)
-    logger.info('Filename base: ' + str(filename_base))
-    logger.info('Length log likelihoods: ' + str(len(log_likelihoods)))
-    logger.info('Length posterior original: ' + str(len(result.posterior)))
-    logger.info('Length posterior shifted: ' + str(len(time_and_phase_shifted_result.posterior)))
-    logger.info('Length samples original: ' + str(len(result.samples)))
-    logger.info('Length samples shifted: ' + str(len(time_and_phase_shifted_result.samples)))
+    # np.savetxt(str(filename_base) + '_pypolychord_production_IMR_non_mem_rec/log_likelihoods.txt', log_likelihoods)
+    # logger.info('Filename base: ' + str(filename_base))
+    # logger.info('Length log likelihoods: ' + str(len(log_likelihoods)))
+    # logger.info('Length posterior original: ' + str(len(result.posterior)))
+    # logger.info('Length posterior shifted: ' + str(len(time_and_phase_shifted_result.posterior)))
+    # logger.info('Length samples original: ' + str(len(result.samples)))
+    # logger.info('Length samples shifted: ' + str(len(time_and_phase_shifted_result.samples)))
 
     # norm_weights = np.exp(test_log_weights)
     # result.plot_corner(
@@ -342,13 +353,13 @@ def run_production_recovery(recovery_model, outdir, **kwargs):
         time_and_phase_shifted_result.save_to_file()
 
     waveform_generator_memory = bilby.gw.WaveformGenerator(
-        frequency_domain_source_model=frequency_domain_nr_hyb_sur_waveform_with_memory_wrapped,
+        time_domain_source_model=time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
         parameters=deepcopy(settings.injection_parameters.__dict__),
         waveform_arguments=deepcopy(settings.waveform_arguments.__dict__),
         **settings.waveform_data.__dict__)
 
     waveform_generator_no_memory = bilby.gw.WaveformGenerator(
-        frequency_domain_source_model=frequency_domain_nr_hyb_sur_waveform_without_memory_wrapped_no_shift_return,
+        time_domain_source_model=time_domain_nr_hyb_sur_waveform_without_memory_wrapped_no_shift_return,
         parameters=deepcopy(settings.injection_parameters.__dict__),
         waveform_arguments=deepcopy(settings.waveform_arguments.__dict__),
         **settings.waveform_data.__dict__)
