646c2722250a42de431271ebb8645c461f413d08
Moritz <email@moritz-huebner.de>
2019-06-02 09:47:38 +1000

    Fixed a thing

--- scripts/debug_script3.py
+++ scripts/debug_script3.py
@@ -8,8 +8,8 @@ memestr.wrappers.injection_recovery. \
     run_production_injection_imr_phenom(
         injection_model=memestr.core.waveforms.surrogate.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
         recovery_model=memestr.core.waveforms.frequency_domain_IMRPhenomD_waveform_without_memory,
-        filename_base=1007,
-        outdir='1007_pypolychord_production_IMR_non_mem_rec', label='IMR_mem_inj_non_mem_rec',
+        filename_base=1900,
+        outdir='1900_pypolychord_production_IMR_non_mem_rec', label='IMR_mem_inj_non_mem_rec',
         alpha=0.1, distance_marginalization=True,
         time_marginalization=False, phase_marginalization=True,
         sampler='dynesty', nthreads=1,
