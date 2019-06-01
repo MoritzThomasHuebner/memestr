91b01ec525adeed64da2d4e3f38e8a3514e87ce9
Moritz <email@moritz-huebner.de>
2019-06-01 16:59:56 +1000

    test

--- scripts/debug_script3.py
+++ scripts/debug_script3.py
@@ -5,10 +5,10 @@ memestr.wrappers.injection_recovery. \
     run_production_injection_imr_phenom(
         injection_model=memestr.core.waveforms.surrogate.time_domain_nr_hyb_sur_waveform_with_memory_wrapped,
         recovery_model=memestr.core.waveforms.frequency_domain_IMRPhenomD_waveform_without_memory,
-        filename_base=3,
-        outdir='3_dynesty', label='IMR_mem_inj_non_mem_rec',
+        filename_base=900,
+        outdir='900_pypolychord_production_IMR_non_mem_rec', label='IMR_mem_inj_non_mem_rec',
         alpha=0.1, distance_marginalization=True,
         time_marginalization=False, phase_marginalization=True,
         sampler='dynesty', nthreads=1,
-        npoints=200, duration=16, random_seed=42, dlogz=0.1,
+        npoints=2000, duration=16, random_seed=42, dlogz=0.1,
         sampling_frequency=2048, resume=False, clean=False, n_check_point=100)
