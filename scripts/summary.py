a49076fa26a7296026aabb6c57e20141f3bba16a
Moritz <email@moritz-huebner.de>
2019-06-04 12:45:21 +1000

    Set up for reweighting run

--- scripts/summary.py
+++ scripts/summary.py
@@ -26,6 +26,8 @@ for i in range(1000, 2000):
         hom_log_bf = np.loadtxt(str(i) + '_pypolychord_production_IMR_non_mem_rec/hom_log_bf.txt')
         if memory_log_bf > 1:
             print(i)
+        if memory_log_bf > 2:
+            continue
         # if memory_log_bf < 1:
         #     memory_log_bfs.append(memory_log_bf)
         # if hom_log_bf < 20:
