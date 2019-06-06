be65359558b4ed0ef7b92638a51d37576368818d
Moritz <email@moritz-huebner.de>
2019-06-06 18:00:15 +1000

    Set up for production real event reweighting

--- scripts/production_real_event_submit.sh
+++ scripts/production_real_event_submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=real_event
 #SBATCH --ntasks=1
 #SBATCH --time=168:00:00
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
 
