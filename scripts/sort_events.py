0330d9ceaaeefe4bf8869e5da323e58856fac9ed
Moritz <email@moritz-huebner.de>
2019-06-06 13:37:42 +1000

    Fixed sorting

--- scripts/sort_events.py
+++ scripts/sort_events.py
@@ -7,14 +7,16 @@ import matplotlib.pyplot as plt
 
 ids = []
 snrs = []
+new_ids = []
 
-for i in range(2000, 3000):
+for i in range(1000, 2000):
     ifos = bb.gw.detector.InterferometerList.from_hdf5('parameter_sets/' + str(i) + '_H1L1V1.h5')
     best_snrs = [ifo.meta_data['matched_filter_SNR'].real for ifo in ifos]
     network_snr = np.sqrt(np.sum([snr ** 2 for snr in best_snrs]))
     ids.append(i)
     snrs.append(network_snr)
-#
+    new_ids.append(i)
+
 data = pd.DataFrame({'ids': ids, 'snr': snrs})
 data = data.sort_values(by='snr')
 np.savetxt('snrs.txt', data.values)
@@ -23,7 +25,8 @@ plt.hist(snrs, bins=32)
 plt.savefig('snrs')
 plt.clf()
 
-for new_id, old_id in enumerate(data.ids):
+
+for new_id, old_id in zip(new_ids, data.ids):
     src = 'parameter_sets/' + str(old_id) + '_H1L1V1.h5'
     dst = 'parameter_sets/' + str(new_id) + '_H1L1V1_new.h5'
     copyfile(src, dst)
