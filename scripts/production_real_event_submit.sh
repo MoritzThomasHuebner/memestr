ec8db93544b20e83d749235db0df54e3e585b3a5
Moritz <email@moritz-huebner.de>
2019-06-09 00:32:04 +1000

    Set up real event run with my own time and phase shifting

--- scripts/production_real_event_submit.sh
+++ scripts/production_real_event_submit.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=real_event
 #SBATCH --ntasks=1
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
