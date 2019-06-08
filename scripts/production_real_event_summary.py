8066f27c55230a7a44a9cd3547208ed8d7246601
Moritz <email@moritz-huebner.de>
2019-06-08 19:15:49 +1000

    Set up for next run

--- scripts/production_real_event_summary.py
+++ scripts/production_real_event_summary.py
@@ -2,31 +2,50 @@ import numpy as np
 import sys
 import bilby
 from scipy.misc import logsumexp
+import matplotlib.pyplot as plt
 
-event_id = sys.argv[1]
-number_of_parallel_runs = int(sys.argv[2])
+logger = bilby.core.utils.logger
+
+
+# event_id = sys.argv[1]
+# number_of_parallel_runs = int(sys.argv[2])
 
 # event_id = 'GW170823'
-# number_of_parallel_runs = 64
+number_of_parallel_runs = 64
+event_ids = ['GW150914', 'GW151012', 'GW151226', 'GW170104', 'GW170608',
+             'GW170729', 'GW170809', 'GW170814', 'GW170818', 'GW170823']
+ethan_hom_log_bfs = [-0.21, 0.31, -0.05, -0.10, -0.33, 1.15, -0.09, 0.11, 0.37, -0.25]
 
-base_result = bilby.result.read_in_result(filename=event_id + '/22_pe_result.json')
-hom_like = np.array([])
-memory_like = np.array([])
-hom_weights = []
+memory_log_bfs = []
 
+for ethan_hom_log_bf, event_id in zip(ethan_hom_log_bfs, event_ids):
+    base_result = bilby.result.read_in_result(filename=event_id + '/22_pe_result.json')
+    hom_like = np.array([])
+    memory_like = np.array([])
+    hom_weights = []
 
-for run_id in range(number_of_parallel_runs):
-    hom_like = np.append(hom_like, np.loadtxt(event_id + '/moritz_hom_log_likelihoods_' + str(run_id) + '.txt'))
-    memory_like = np.append(memory_like, np.loadtxt(event_id + '/moritz_memory_log_likelihoods_' + str(run_id) + '.txt'))
+    for run_id in range(number_of_parallel_runs):
+        hom_like = np.append(hom_like, np.loadtxt(event_id + '/moritz_hom_log_likelihoods_' + str(run_id) + '.txt'))
+        memory_like = np.append(memory_like, np.loadtxt(event_id + '/moritz_memory_log_likelihoods_' + str(run_id) + '.txt'))
 
+    for i in range(len(base_result.posterior)):
+        hom_weights.append(hom_like[i] - base_result.posterior.log_likelihood.iloc[i])
 
-for i in range(len(base_result.posterior)):
-    hom_weights.append(hom_like[i] - base_result.posterior.log_likelihood.iloc[i])
+    memory_weights = memory_like - hom_like
 
-memory_weights = memory_like - hom_like
+    hom_log_bf = logsumexp(hom_weights) - np.log(len(hom_weights))
+    memory_log_bf = logsumexp(memory_weights) - np.log(len(memory_weights))
 
-hom_log_bf = logsumexp(hom_weights) - np.log(len(hom_weights))
-memory_log_bf = logsumexp(memory_weights) - np.log(len(memory_weights))
+    logger.info("Ethan HOM LOG BF: " + str(ethan_hom_log_bf))
+    logger.info("Moritz HOM LOG BF: " + str(hom_log_bf))
+    logger.info("Memory LOG BF: " + str(memory_log_bf))
+    memory_log_bfs.append(memory_log_bf)
 
-print("HOM LOG BF: " + str(hom_log_bf))
-print("Memory LOG BF: " + str(memory_log_bf))
+memory_log_bfs = np.array(memory_log_bfs)
+cummulative_memory_log_bfs = np.cumsum(memory_log_bfs)
+
+plt.plot(memory_log_bfs, label='Memory log BFs')
+plt.plot(cummulative_memory_log_bfs, label='Cumulative memory log BFs')
+plt.xticks(np.arange(10), tuple(event_ids))
+plt.savefig('money_plot')
+plt.clf()
