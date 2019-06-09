167e2ce19ef122ab4ab72fdd0f72d788579a7ce1
Moritz <email@moritz-huebner.de>
2019-06-09 11:53:09 +1000

    Set up real event run with my own time and phase shifting

--- scripts/sort_events.py
+++ scripts/sort_events.py
@@ -9,7 +9,7 @@ ids = []
 snrs = []
 new_ids = []
 
-for i in range(1000, 2000):
+for i in range(0, 2000):
     ifos = bb.gw.detector.InterferometerList.from_hdf5('parameter_sets/' + str(i) + '_H1L1V1.h5')
     best_snrs = [ifo.meta_data['matched_filter_SNR'].real for ifo in ifos]
     network_snr = np.sqrt(np.sum([snr ** 2 for snr in best_snrs]))
