94bf839aa0b915d5d1b62a5b92c948aff4135ed9
Moritz <email@moritz-huebner.de>
2019-06-10 13:58:27 +1000

    Made it a bit speedier

--- scripts/production_real_event_submit.sh
+++ scripts/production_real_event_submit.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=real_event
 #SBATCH --ntasks=1
-#SBATCH --time=72:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
 #
-#SBATCH --array=0-63
+#SBATCH --array=0-127
 
-srun python production_real_event.py $1 64 ${SLURM_ARRAY_TASK_ID}
+srun python production_real_event.py $1 128 ${SLURM_ARRAY_TASK_ID}
