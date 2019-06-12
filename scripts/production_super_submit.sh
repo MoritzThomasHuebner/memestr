9f38203733645d8bdbb03d6643e0cae5d16b6d68
Moritz <email@moritz-huebner.de>
2019-06-12 10:47:21 +1000

    Testing submit

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+input="n_effs_additional_runs"
+i=0
+while IFS= read -r line
+do
+  if [$line -gt "0"]
+  then
+    echo "$line"
+    for j in {0..$line}
+    do
+#      bash production_submit.sh ${i}_dynesty j
+       echo $j
+    done
+  fi
+  echo $i
+  ((i++))
+
+done <<< $(command)
 
 for i in {1999..0}
 do
-    bash production_submit.sh ${i}_dynesty
+
 done
 
