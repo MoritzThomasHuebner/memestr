fb769dcf7d3b03fe332e8a682e99f2bb93a44f24
Moritz <email@moritz-huebner.de>
2019-06-14 14:08:25 +1000

    more tests

--- scripts/production_IMR_time_phase_submit.sh
+++ scripts/production_IMR_time_phase_submit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash production_IMR_time_and_phase.sh              outdir_base=${1} filename_base=${1} sub_run_id=${2} npoints=250 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=True resume=True sampler=dynesty duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
+bash production_IMR_time_phase.sh              outdir_base=${1} filename_base=${1} sub_run_id=${2} npoints=250 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=True resume=True sampler=dynesty duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
