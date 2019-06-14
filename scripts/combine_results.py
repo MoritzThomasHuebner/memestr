8696666f537d01a0acb07f5d722a85c04c52c4cb
Moritz <email@moritz-huebner.de>
2019-06-14 11:29:54 +1000

    Test run

--- scripts/combine_results.py
+++ scripts/combine_results.py
@@ -7,12 +7,12 @@ logger = bilby.core.utils.logger
 # print(i)
 i = 1999
 outdir = "{}_dynesty_production_IMR_non_mem_rec/".format(i)
-filename = "reconstructed_result_result.json"
+filename = "reconstructed_result{}_result.json"
 base_result = read_in_result(outdir + filename)
 res_list = ResultList([base_result])
 for j in range(41):
     try:
-        res = read_in_result(outdir + str(i) + filename)
+        res = read_in_result(outdir + filename.format(i))
         res_list.append(res)
     except OSError as e:
         logger.info(e)
