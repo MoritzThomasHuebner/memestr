77908fa15d2fa9d0b9827843c4d65c7629edea40
Moritz <email@moritz-huebner.de>
2019-06-14 13:07:15 +1000

    Added more runs

--- scripts/summary_summary.py
+++ scripts/summary_summary.py
@@ -129,10 +129,10 @@ for i in range(2000):
 # plt.clf()
 
 np.savetxt("n_effs", n_effs)
-n_effs_additional_runs = [int(20/x) for x in n_effs]
+n_effs_additional_runs = [int(60/x) for x in n_effs]
 with open("n_effs_additional_runs", 'w') as f:
     for i in range(len(n_effs_additional_runs)):
         print(i)
         f.write(str(n_effs_additional_runs[i]) + '\n')
 print(sum(n_effs_additional_runs))
-# np.savetxt("n_effs_additional_runs", n_effs_additional_runs)
+np.savetxt("n_effs_additional_runs", n_effs_additional_runs)
