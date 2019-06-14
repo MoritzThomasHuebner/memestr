760acfc4f6f2ee3ea2e40bc06e76c248fd2c0044
Moritz <email@moritz-huebner.de>
2019-06-14 15:13:17 +1000

    Setup for more runs

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-bash production_IMR_time_phase_submit.sh 0_dynesty
+#bash production_IMR_time_phase_submit.sh 0_dynesty
 
-#input="n_effs_additional_runs"
-#i=0
-#while IFS= read -r line
-#do
-#  if [[ ${line} -gt "0" ]]
-#  then
-#    for ((j=0; j<=${line}; j++)); do
-#      bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
-#    done
-#  fi
-#  ((i++))
-#done < "$input"
-#
+input="n_effs_additional_runs"
+i=0
+while IFS= read -r line
+do
+  if [[ ${line} -gt "0" ]]
+  then
+    for ((j=0; j<=${line}; j++)); do
+      bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
+    done
+  fi
+  ((i++))
+done < "$input"
