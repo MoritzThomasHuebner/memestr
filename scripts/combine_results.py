9c06eb56c0bf3dde382d15be0f08405340566e22
Moritz <email@moritz-huebner.de>
2019-06-16 13:59:35 +1000

    Set up for initial time and phase shifting

--- scripts/combine_results.py
+++ scripts/combine_results.py
@@ -3,17 +3,17 @@ from bilby.core.result import ResultList, read_in_result
 
 logger = bilby.core.utils.logger
 
-for i in range(2000):
+for i in range(1950, 1951K):
     outdir = "{}_dynesty_production_IMR_non_mem_rec/".format(i)
     filename = "reconstructed_result_result.json"
     base_result = read_in_result(outdir + filename)
     res_list = ResultList([base_result])
-    for j in range(41):
+    for j in range(81):
         try:
             res = read_in_result(outdir + "reconstructed_result{}_result.json".format(j))
             res_list.append(res)
         except OSError as e:
-            # logger.info(e)
+            logger.info(e)
             break
     new_res = res_list.combine()
     new_res.label = "reconstructed_combined"
