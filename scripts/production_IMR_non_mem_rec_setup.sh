5334c5cc75f73f10fb46c51c273077fe74708782
Moritz <email@moritz-huebner.de>
2019-06-16 15:18:32 +1000

    Restarting jobs

--- scripts/production_IMR_non_mem_rec_setup.sh
+++ scripts/production_IMR_non_mem_rec_setup.sh
@@ -5,9 +5,9 @@ OUTDIR=${2//outdir_base=/}_${LABEL}
 mkdir -p ${OUTDIR}
 JOB_NAME="--job-name=$LABEL"
 OUTPUT=""
-TIME="--time=24:00:00"
+TIME="--time=00:30:00"
 NTASKS="--ntasks=1"
-MEM_PER_CPU="--mem-per-cpu=1G"
+MEM_PER_CPU="--mem-per-cpu=2G"
 CPUS_PER_TASK="--cpus-per-task=1"
 EMAIL="--mail-type=END --mail-user=email@moritz-huebner.de"
 ARRAY=""
--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-for i in {0..1949}
-do
-    bash production_IMR_time_phase_submit.sh ${i}_dynesty
-done
-#input="n_effs_additional_runs"
-#i=0
-#while IFS= read -r line
+#for i in {0..1999}
 #do
-#  if [[ ${line} -gt "0" ]]
-#  then
-#    for ((j=0; j<=${line}; j++)); do
-#      bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
-#    done
-#  fi
-#  ((i++))
-#done < "$input"
+#    bash production_IMR_time_phase_submit.sh ${i}_dynesty
+#done
+input="n_effs_additional_runs"
+i=0
+while IFS= read -r line
+do
+  if [[ ${line} -gt "0" ]]
+  then
+    for ((j=0; j<=${line}; j++)); do
+      bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
+    done
+  fi
+  ((i++))
+done < "$input"
