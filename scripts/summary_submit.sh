65e6e00af3dee3f05a7e2a682f58816c18071972
Moritz <email@moritz-huebner.de>
2019-06-25 15:47:14 +1000

    Added MWM

--- scripts/summary_submit.sh
+++ scripts/summary_submit.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=summary
 #SBATCH --ntasks=1
-#SBATCH --time=3:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=1G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
