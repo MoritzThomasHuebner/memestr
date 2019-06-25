ec2657a93674dbaac394eb49d82b94f71aad221a
Moritz <email@moritz-huebner.de>
2019-06-25 21:27:05 +1000

    Set up for new pop study

--- scripts/create_population.py
+++ scripts/create_population.py
@@ -24,7 +24,7 @@ def create_parameter_set(filename):
     network_snr = 0
     settings = AllSettings()
     trials = 0
-    while network_snr < 30:
+    while network_snr < 1:
         idx = np.random.randint(0, len(all_params.total_masses))
         total_mass = all_params.total_masses[idx]
         mass_ratio = all_params.mass_ratios[idx]
