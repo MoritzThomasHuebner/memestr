e85a3c81f1d04b2cff92d09a668741439ae18425
Moritz <email@moritz-huebner.de>
2019-07-04 14:51:29 +1000

    Test

--- memestr/wrappers/injection_recovery.py
+++ memestr/wrappers/injection_recovery.py
@@ -229,7 +229,7 @@ def run_reweighting(recovery_model, outdir, **kwargs):
     #IMR
     # result = bilby.result.read_in_result(filename=str(filename_base) + '_dynesty_production_IMR_non_mem_rec/reconstructed_combined_result.json')
     # time_and_phase_shifted_result = bilby.result.read_in_result(filename=str(filename_base) + '_dynesty_production_IMR_non_mem_rec/time_and_phase_shifted_combined_result.json')
-    result = bilby.result.read_in_result(filename=str(filename_base) + '_IMR_non_mem_rec/combined_result.json')
+    result = bilby.result.read_in_result(filename=str(filename_base) + '_IMR_inj_production_IMR_non_mem_rec/combined_result.json')
 
     waveform_generator_memory = bilby.gw.WaveformGenerator(
         frequency_domain_source_model=frequency_domain_nr_hyb_sur_waveform_with_memory_wrapped,
