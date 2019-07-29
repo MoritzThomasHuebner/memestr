41dd0f606b9b3477d940b9d179f66e2ed35bbafc
Moritz <email@moritz-huebner.de>
2019-07-29 22:55:48 +1000

    Set up for new population study

--- scripts/injection_log_bfs_analysis.py
+++ scripts/injection_log_bfs_analysis.py
@@ -2,14 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 log_bfs = np.array([])
-snrs = np.array([])
 trials = np.array([])
+snrs = np.array([])
+memory_snrs = np.array([])
 
 for i in range(32):
-    data = np.loadtxt('Injection_log_bfs/Injection_log_bfs_snr_60_{}.txt'.format(i))
+    data = np.loadtxt('Injection_log_bfs/Injection_log_bfs_snr_0_{}.txt'.format(i))
     log_bfs = np.append(log_bfs, data[:, 0])
     trials = np.append(trials, data[:, 1])
     snrs = np.append(snrs, data[:, 2])
+    memory_snrs = np.append(memory_snrs, data[:, 3])
 print(len(trials))
 
 # for i in range(32, 97):
@@ -17,27 +19,40 @@ print(len(trials))
 #     log_bfs = np.append(log_bfs, data[:, 0])
 #     trials = np.append(trials, data[:, 1])
 
-plt.scatter(log_bfs, snrs)
+plt.scatter(log_bfs, memory_snrs)
 plt.xlabel('log BFs')
 plt.ylabel('SNR')
-plt.show()
+plt.savefig('Injection_log_bfs/Injection_log_bfs_snr_0_log_bfs_vs_memory_snrs')
+# plt.show()
 plt.clf()
 
 plt.hist(log_bfs, bins=int(np.sqrt(len(log_bfs))))
 plt.semilogy()
 plt.xlabel('log BFs')
-plt.show()
+plt.savefig('Injection_log_bfs/Injection_log_bfs_snr_0_log_bfs_hist')
+# plt.show()
 plt.clf()
 
 plt.hist(snrs, bins=int(np.sqrt(len(snrs))))
 plt.xlabel('SNRs')
-plt.show()
+plt.savefig('Injection_log_bfs/Injection_log_bfs_snr_0_snrs_hist')
+# plt.show()
 plt.clf()
 
 plt.hist(trials, bins=int(np.sqrt(len(trials))))
 plt.xlabel('trials')
-plt.show()
+plt.savefig('Injection_log_bfs/Injection_log_bfs_snr_0_trials_hist')
+# plt.show()
 plt.clf()
+
+plt.plot(np.sort(snrs), np.linspace(1, 0, len(snrs)))
+plt.semilogy()
+plt.xlabel('SNR')
+plt.ylabel('fraction above SNR')
+plt.savefig('Injection_log_bfs/Injection_log_bfs_snr_0_bayes_freq_comparison')
+# plt.show()
+plt.clf()
+
 import sys
 sys.exit(0)
 
