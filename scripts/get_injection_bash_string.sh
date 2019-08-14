074bcd5cc4f94362364de454518e626570e144a3
Moritz <email@moritz-huebner.de>
2019-08-14 13:41:27 +1000

    Simplified some stuff

--- scripts/get_injection_bash_string.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-FILENAME="./parameter_sets/$1"
-while IFS= read -r var
-do
-  echo "$var"
-done < "$FILENAME"
--- scripts/production_real_event_summary.py
+++ scripts/production_real_event_summary.py
@@ -10,59 +10,10 @@ matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 matplotlib.rcParams.update({'font.size': 15})
 logger = bilby.core.utils.logger
 
-
-# event_id = sys.argv[1]
-# number_of_parallel_runs = int(sys.argv[2])
-
-# event_id = 'GW170823'
 number_of_parallel_runs = 64
 event_ids = ['GW150914', 'GW151012', 'GW151226', 'GW170104', 'GW170608',
              'GW170729', 'GW170809', 'GW170814', 'GW170818', 'GW170823']
-# ethan_hom_log_bfs = [-0.21, 0.31, -0.05, -0.10, -0.33, 1.15, -0.09, 0.11, 0.37, -0.25]
 
-# memory_log_bfs = []
-
-# for event_id in event_ids:
-#     ethan_result = np.loadtxt(event_id + '/new_likelihoods.dat')
-#     ethan_22_log_likelihood = ethan_result[:, 0]
-#     ethan_posterior_hom_log_likelihood = ethan_result[:, 1]
-#     ethan_weight_log_likelihood = ethan_result[:, 2]
-#
-#     base_result = bilby.result.read_in_result(filename=event_id + '/22_pe_result.json')
-#     hom_like_moritz = np.array([])
-#     hom_like_ethan = np.array([])
-#     memory_like = np.array([])
-#     hom_weights_moritz = []
-#     hom_weights_ethan = []
-#     hom_memory_weights = []
-#     logger.info(event_id)
-#     try:
-#         for run_id in range(number_of_parallel_runs):
-#             hom_like_moritz = np.append(hom_like_moritz, np.loadtxt(event_id + '/moritz_hom_log_likelihoods_' + str(run_id) + '.txt'))
-#             hom_like_ethan = np.append(hom_like_ethan, np.loadtxt(event_id + '/ethan_hom_log_likelihoods_' + str(run_id) + '.txt'))
-#             memory_like = np.append(memory_like, np.loadtxt(event_id + '/moritz_memory_log_likelihoods_' + str(run_id) + '.txt'))
-#     except ValueError as e:
-#         logger.warning(e)
-#         logger.warning("Run ID: " + str(run_id))
-#         continue
-#
-#     for i in range(len(base_result.posterior)):
-#         hom_weights_moritz.append(hom_like_moritz[i] - base_result.posterior.log_likelihood.iloc[i])
-#         hom_weights_ethan.append(hom_like_ethan[i] - base_result.posterior.log_likelihood.iloc[i])
-#         hom_memory_weights.append(memory_like[i] - base_result.posterior.log_likelihood.iloc[i])
-#
-#     hom_log_bf_moritz = logsumexp(hom_weights_moritz) - np.log(len(hom_weights_moritz))
-#     hom_log_bf_ethan = logsumexp(hom_weights_ethan) - np.log(len(hom_weights_ethan))
-#     hom_log_bf_ethan_posterior = np.log(np.sum(ethan_weight_log_likelihood)) - np.log(len(ethan_weight_log_likelihood))
-#     hom_memory_log_bf = logsumexp(hom_memory_weights) - np.log(len(hom_memory_weights))
-#     memory_log_bf = hom_memory_log_bf - hom_log_bf_moritz
-#     logger.info("Ethan Posterior HOM LOG BF: " + str(hom_log_bf_ethan_posterior))
-#     logger.info("Ethan Restored HOM LOG BF: " + str(hom_log_bf_ethan))
-#     logger.info("Moritz HOM LOG BF: " + str(hom_log_bf_moritz))
-#     logger.info("Memory LOG BF: " + str(memory_log_bf))
-#     memory_log_bfs.append(memory_log_bf)
-#
-# np.savetxt('real_event_log_bfs.txt', memory_log_bfs)
 memory_log_bfs = np.loadtxt('real_event_log_bfs.txt')
 memory_log_bfs = np.array(memory_log_bfs)
 cumulative_memory_log_bfs = np.cumsum(memory_log_bfs)
@@ -72,9 +23,8 @@ plt.grid(False)
 plt.axhline(0, color='grey', linestyle='--')
 plt.xticks(np.arange(10), tuple(event_ids), rotation=45)
 plt.ylabel('log BF')
-# plt.legend()
 plt.tight_layout()
-plt.savefig('gwtc-1.pdf')
+plt.savefig('summary/gwtc-1.pdf')
 plt.clf()
 
 
@@ -85,6 +35,6 @@ plt.ylabel('Cumulative log BF')
 plt.grid(False)
 plt.legend()
 plt.tight_layout()
-plt.savefig('gwtc-1-cumulative.png')
+plt.savefig('summary/gwtc-1-cumulative.png')
 plt.clf()
 
