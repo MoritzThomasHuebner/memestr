49d9e220f3ac228d01f23043ceb82ae535a64c3b
Moritz <email@moritz-huebner.de>
2019-08-15 18:15:02 +1000

    Set up for new runs

--- memestr/core/__init__.py
+++ memestr/core/__init__.py
@@ -1,7 +1,5 @@
 import memestr.core.parameters
 import memestr.core.submit
-import memestr.core.likelihood
 import memestr.core.waveforms
 import memestr.core.population
-import memestr.core.expected_bayes_factor
 import memestr.core.postprocessing
--- scripts/production_real_event_summary.py
+++ scripts/production_real_event_summary.py
@@ -1,20 +1,17 @@
 import numpy as np
-import sys
 import bilby
-from scipy.misc import logsumexp
 import matplotlib
-# matplotlib.rcParams['text.usetex'] = True
-# matplotlib.rcParams['text.latex.unicode'] = True
 import matplotlib.pyplot as plt
 matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 matplotlib.rcParams.update({'font.size': 15})
+matplotlib.rcParams.update({'font.family': 'sans'})
 logger = bilby.core.utils.logger
 
 number_of_parallel_runs = 64
 event_ids = ['GW150914', 'GW151012', 'GW151226', 'GW170104', 'GW170608',
              'GW170729', 'GW170809', 'GW170814', 'GW170818', 'GW170823']
 
-memory_log_bfs = np.loadtxt('real_event_log_bfs.txt')
+memory_log_bfs = np.loadtxt('gwtc-1/real_event_log_bfs.txt')
 memory_log_bfs = np.array(memory_log_bfs)
 cumulative_memory_log_bfs = np.cumsum(memory_log_bfs)
 logger.info("Cumulative log BF: " + str(cumulative_memory_log_bfs[-1]))
@@ -22,9 +19,9 @@ plt.plot(memory_log_bfs, label='Memory log BF', marker='H', linestyle='None', co
 plt.grid(False)
 plt.axhline(0, color='grey', linestyle='--')
 plt.xticks(np.arange(10), tuple(event_ids), rotation=45)
-plt.ylabel('log BF')
+plt.ylabel('Memory log BF')
 plt.tight_layout()
-plt.savefig('gwtc-1/gwtc-1.pdf')
+plt.savefig('gwtc-1/gwtc-1-poster.pdf')
 plt.clf()
 
 
