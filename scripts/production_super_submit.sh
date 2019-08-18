a905bf7565d3354b44872401ac14d82a0e8e0cd7
Moritz <email@moritz-huebner.de>
2019-08-18 13:52:33 +1000

    Try to restart runs

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -17,9 +17,9 @@
 #  ((i++))
 #done < "$input"
 
-for i in {20000..20029}
+for i in {2001..2001}
 do
-    for j in {0..7}
+    for j in {200..207}
     do
         bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
     done
