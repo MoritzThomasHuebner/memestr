87902fda627b9fa093c7eb09201b1b34e521d539
Moritz <email@moritz-huebner.de>
2019-08-19 12:05:38 +1000

    Set up for reweighting stuff

--- scripts/production_IMR_reweight_submit.sh
+++ scripts/production_IMR_reweight_submit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash production_IMR_non_mem_rec.sh               outdir_base=${1} filename_base=${1} sub_run_id=${2} script=run_reweighting reweight_model=time_domain_IMRPhenomD_waveform_without_memory recovery_model=time_domain_IMRPhenomD_waveform_with_memory npoints=250 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=False resume=True sampler=dynesty duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
+bash production_IMR_non_mem_rec.sh               outdir_base=${1} filename_base=${1} sub_run_id=${2} script=run_reweighting reweight_model=time_domain_IMRPhenomD_waveform_with_memory recovery_model=time_domain_IMRPhenomD_waveform_without_memory npoints=250 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=False resume=True sampler=dynesty duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -19,7 +19,7 @@
 
 for i in {20000..20029}
 do
-    for j in {10..17}
+    for j in {0..7}
     do
         bash production_IMR_reweight_submit.sh ${i}_dynesty ${j}
     done
