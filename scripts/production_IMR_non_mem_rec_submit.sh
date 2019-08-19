54478305141d003633270a58ee80dcf3f6f96dbb
Moritz <email@moritz-huebner.de>
2019-08-19 14:22:54 +1000

    Re-run stuff

--- scripts/production_IMR_non_mem_rec_submit.sh
+++ scripts/production_IMR_non_mem_rec_submit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bash production_IMR_non_mem_rec.sh              outdir_base=${1} filename_base=${1} sub_run_id=${2} script=run_production_injection recovery_model=frequency_domain_nr_hyb_sur_waveform_without_memory_wrapped npoints=200 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=False resume=True sampler=dynesty duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
+bash production_IMR_non_mem_rec.sh              outdir_base=${1} filename_base=${1} sub_run_id=${2} script=run_production_injection recovery_model=time_domain_IMRPhenomD_waveform_without_memory npoints=200 alpha=0.1 distance_marginalization=True time_marginalization=False phase_marginalization=False resume=True sampler=dynesty duration=16 random_seed=42 sampling_frequency=2048 label=IMR_mem_inj_non_mem_rec
 #recovery_model=time_domain_IMRPhenomD_waveform_without_memory
