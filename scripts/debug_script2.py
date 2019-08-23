80d68e120e0f7b96474a94d812cd898502fc0ae1
Moritz <email@moritz-huebner.de>
2019-08-23 16:14:10 +1000

    Changed stuff

--- scripts/debug_script2.py
+++ scripts/debug_script2.py
@@ -2,7 +2,7 @@ import bilby
 import numpy as np
 
 network_snrs = []
-for run_id in range(48):
+for run_id in range(32, 48):
     run_id = str(run_id).zfill(3)
     res = bilby.result.read_in_result('SNR_VS_LOGBF_DATA/{}_IMR_mem_inj_non_mem_rec/0/IMR_mem_inj_non_mem_rec_result.json'.format(run_id))
     network_snr = np.sqrt(np.sum([res.meta_data['likelihood']['interferometers'][ifo]['optimal_SNR']**2 for ifo in ['H1', 'V1', 'L1']]))
