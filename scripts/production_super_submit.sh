ac22fa6543e6ef0d29d83135801a566b953be864
Moritz <email@moritz-huebner.de>
2019-09-25 20:19:07 +1000

    Set up for new data generation

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -4,6 +4,6 @@ for i in {20000..20029}
 do
     for j in {0..7}
     do
-        bash production_IMR_reweight_submit.sh ${i}_dynesty ${j}
+        bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
     done
 done
