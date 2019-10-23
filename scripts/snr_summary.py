994179e6cc582bc905f776b7445307114d8bba62
Moritz <email@moritz-huebner.de>
2019-10-23 16:16:18 +1100

    Changed back to new data

--- scripts/snr_summary.py
+++ scripts/snr_summary.py
@@ -44,15 +44,20 @@ waveform_generator = bilby.gw.WaveformGenerator(time_domain_source_model=no_mem_
                                                 parameters=settings.injection_parameters.__dict__,
                                                 waveform_arguments=settings.waveform_arguments.__dict__,
                                                 **settings.waveform_data.__dict__)
-ifos = bilby.gw.detector.InterferometerList.from_hdf5('parameter_sets/20000_H1L1V1.h5')
-priors = deepcopy(settings.recovery_priors.proper_dict())
-likelihood = bilby.gw.likelihood \
-    .GravitationalWaveTransient(interferometers=deepcopy(ifos),
-                                waveform_generator=waveform_generator,
-                                priors=priors,
-                                distance_marginalization=True)
+# ifos = bilby.gw.detector.InterferometerList.from_hdf5('parameter_sets/20000_H1L1V1.h5')
+# priors = deepcopy(settings.recovery_priors.proper_dict())
+# likelihood = bilby.gw.likelihood \
+#     .GravitationalWaveTransient(interferometers=deepcopy(ifos),
+#                                 waveform_generator=waveform_generator,
+#                                 priors=priors,
+#                                 distance_marginalization=True)
 
 for run_id in range(20000, 20030):
+    res_mem_rec = bilby.core.result.read_in_result(
+        '{}_dynesty_production_IMR_non_mem_rec/{}IMR_mem_inj_mem_rec_result.json'.format(run_id, 0))
+    snrs.append(np.sqrt(np.sum([res_mem_rec.meta_data['likelihood']['interferometers'][ifo]['optimal_SNR']**2 for ifo in ['H1', 'L1', 'V1']])))
+    continue
+
     # sampling_log_bfs = []
     # reweight_log_bfs = []
     # for i in range(0, 8):
@@ -79,20 +84,19 @@ for run_id in range(20000, 20030):
     print("no memory evidence: " + str(no_mem_evidence))
     mem_log_bfs_injected.append(mem_evidence - no_mem_evidence)
 
-    # snrs.append(np.sqrt(np.sum([res_mem_rec.meta_data['likelihood']['interferometers'][ifo]['optimal_SNR']**2 for ifo in ['H1', 'L1', 'V1']])))
     print(run_id)
     print(mem_log_bfs_injected[-1])
     # print(mem_log_bfs_reweight[-1])
     # print(mem_log_bfs_sampled[-1])
     # print(mem_log_bfs_sampled_err[-1])
 
-# np.savetxt('SNR_VS_LOGBF_DATA/new_data.txt', np.array([mem_log_bfs_reweight, mem_log_bfs_reweight_err,
-#                                                        mem_log_bfs_sampled, mem_log_bfs_sampled_err]))
 res = np.loadtxt('SNR_VS_LOGBF_DATA/new_data.txt')
 mem_log_bfs_reweight = res[0]
 mem_log_bfs_reweight_err = res[1]
 mem_log_bfs_sampled = res[2]
 mem_log_bfs_sampled_err = res[3]
+np.savetxt('SNR_VS_LOGBF_DATA/new_data.txt', np.array([snrs, mem_log_bfs_reweight, mem_log_bfs_reweight_err,
+                                                       mem_log_bfs_sampled, mem_log_bfs_sampled_err]))
 
 fig = plt.figure(figsize=(7, 7))
 gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
