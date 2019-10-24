26086b71a7b213e65d2ab9965bca498715f4be84
Moritz <email@moritz-huebner.de>
2019-10-24 11:14:04 +1100

    Redoing the distance v bf plot

--- scripts/production_super_submit.sh
+++ scripts/production_super_submit.sh
@@ -2,8 +2,8 @@
 
 for i in {20000..20029}
 do
-    for j in {10..17}
+    for j in {0..7}
     do
-        bash production_IMR_non_mem_rec_submit.sh ${i}_dynesty ${j}
+        bash production_IMR_reweight_submit.sh ${i}_dynesty ${j}
     done
 done
