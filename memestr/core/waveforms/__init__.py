f790912aa63d3ebcc317ef5603b75d1bfbe333c4
Moritz <email@moritz-huebner.de>
2019-10-24 13:47:46 +1100

    Renamed waveforms

--- memestr/core/waveforms/__init__.py
+++ memestr/core/waveforms/__init__.py
@@ -5,12 +5,12 @@ from .ethan import *
 from .mwm import *
 
 models = dict(
-    frequency_domain_nr_hyb_sur_waveform_with_memory=fd_nr_sur_with_memory,
-    frequency_domain_nr_hyb_sur_waveform_without_memory=fd_nr_sur,
-    frequency_domain_nr_hyb_sur_memory_waveform=fd_nr_sur_memory_only,
-    frequency_domain_IMRPhenomD_waveform_without_memory=fd_imrd,
-    frequency_domain_IMRPhenomD_waveform_with_memory=fd_imrd_with_memory,
-    time_domain_IMRPhenomD_waveform_with_memory=td_imrd_with_memory,
-    time_domain_IMRPhenomD_waveform_without_memory=td_imrd,
-    time_domain_IMRPhenomD_memory_waveform=td_imrd_memory_only
+    fd_nr_sur_with_memory=fd_nr_sur_with_memory,
+    fd_nr_sur=fd_nr_sur,
+    fd_nr_sur_memory_only=fd_nr_sur_memory_only,
+    fd_imrd=fd_imrd,
+    fd_imrd_with_memory=fd_imrd_with_memory,
+    td_imrd_with_memory=td_imrd_with_memory,
+    td_imrd=td_imrd,
+    td_imrd_memory_only=td_imrd_memory_only
 )
