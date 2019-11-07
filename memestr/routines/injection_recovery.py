a82f2bdf9152fab15eec9679f82271a65979da5f
Moritz <email@moritz-huebner.de>
2019-11-07 16:09:22 +1100

    Fixed a thing

--- memestr/routines/injection_recovery.py
+++ memestr/routines/injection_recovery.py
@@ -34,6 +34,6 @@ def run_production_injection(**kwargs):
         del params['s21']
         del params['s22']
         del params['random_injection_parameters']
-        result.plot_corner(lionize=settings.other_settings.lionize, parameters=params, outdir=outdir)
+        result.plot_corner(lionize=settings.other_settings.lionize, parameters=params)
 
 
