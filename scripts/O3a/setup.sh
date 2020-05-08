f1aab57bcdaf93a7355c2343dca21171729dcb2e
Moritz Huebner <email@moritz-huebner.de>
2020-05-08 13:07:19 +1000

    Created automated submission script

---
+++ scripts/O3a/setup.sh
@@ -0,0 +1,9 @@
+for EVENT in GW150914 GW151012 GW151226 GW170104 GW170608 GW170729 GW170809 GW170814 GW170818 GW170823
+do
+  bilby_pipe ${EVENT}.ini
+  bash ${EVENT}/submit/run_data0_*_generation.sh
+  FILENAME=$(find "./${EVENT}/submit/" -iname "*generation.sh")
+  echo '#!/bin/bash' >  "${FILENAME}"
+  echo 'echo 0' >> "${FILENAME}"
+  bash ${EVENT}/submit/run_master_slurm.sh
+done
