533530492fc542b3d983caf73418e5f942ebad72
Moritz Huebner <email@moritz-huebner.de>
2020-08-03 20:41:54 +1000

    Fixed a path

--- scripts/O3a/setup.sh
+++ scripts/O3a/setup.sh
@@ -2,7 +2,7 @@ for EVENT in GW150914 GW151012 GW151226 GW170104 GW170608 GW170729 GW170809 GW17
 do
   bilby_pipe ${EVENT}.ini
   bash ${EVENT}/submit/run_data0_*_generation.sh
-  FILENAME=$(find "./${EVENT}/submit/" -iname "*generation.sh")
+  FILENAME=$(find "./${EVENT}_precessing/submit/" -iname "*generation.sh")
   echo '#!/bin/bash' >  "${FILENAME}"
   echo 'echo 0' >> "${FILENAME}"
   echo 'periodic-restart-time = 1209600' >> "${EVENT}/run_config_complete.ini"
