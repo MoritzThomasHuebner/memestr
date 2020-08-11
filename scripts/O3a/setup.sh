8f3617fee08419b69cc6a52ecfde0d9381758e49
Moritz Huebner <email@moritz-huebner.de>
2020-08-11 16:51:25 +1000

    Set up for runs on pcdev

--- scripts/O3a/setup.sh
+++ scripts/O3a/setup.sh
@@ -1,10 +1,10 @@
 for EVENT in GW150914 GW151012 GW151226 GW170104 GW170608 GW170729 GW170809 GW170814 GW170818 GW170823 GW190412
 do
-  bilby_pipe ${EVENT}_22.ini
-  bash ${EVENT}_precessing_22/submit/run_data0_*_generation.sh
-  FILENAME=$(find "./${EVENT}_precessing_22/submit/" -iname "*generation.sh")
-  echo '#!/bin/bash' >  "${FILENAME}"
-  echo 'echo 0' >> "${FILENAME}"
-  echo 'periodic-restart-time = 1209600' >> "${EVENT}_precessing_22/run_config_complete.ini"
-  sbatch ${EVENT}_precessing_22/submit/run_master_slurm.sh
+  bilby_pipe ${EVENT}_22.ini --submit
+#  bash ${EVENT}_precessing_22/submit/run_data0_*_generation.sh
+#  FILENAME=$(find "./${EVENT}_precessing_22/submit/" -iname "*generation.sh")
+#  echo '#!/bin/bash' >  "${FILENAME}"
+#  echo 'echo 0' >> "${FILENAME}"
+#  echo 'periodic-restart-time = 1209600' >> "${EVENT}_precessing_22/run_config_complete.ini"
+#  sbatch ${EVENT}_precessing_22/submit/run_master_slurm.sh
 done
