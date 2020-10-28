23475c158fbbc01c0bf1821fbc5a07d98a24212d
Moritz Huebner <email@moritz-huebner.de>
2020-10-28 19:08:37 +1100

    Changed to 22 runs

--- scripts/O3a/setup.sh
+++ scripts/O3a/setup.sh
@@ -3,10 +3,10 @@ EVENT_LIST="GW150914 GW151012 GW151226 GW170104 GW170608 GW170729 GW170809 GW170
 for EVENT in ${EVENT_LIST}
 do
   bilby_pipe ${EVENT}.ini
-  bash ${EVENT}/submit/run_data0_*_generation.sh
-  FILENAME=$(find "./${EVENT}/submit/" -iname "*generation.sh")
-  echo '#!/bin/bash' >  "${FILENAME}"
-  echo 'echo 0' >> "${FILENAME}"
-  echo 'periodic-restart-time = 1209600' >> "${EVENT}/run_config_complete.ini"
-  sbatch ${EVENT}/submit/run_master_slurm.sh
+#  bash ${EVENT}/submit/run_data0_*_generation.sh
+#  FILENAME=$(find "./${EVENT}/submit/" -iname "*generation.sh")
+#  echo '#!/bin/bash' >  "${FILENAME}"
+#  echo 'echo 0' >> "${FILENAME}"
+#  echo 'periodic-restart-time = 1209600' >> "${EVENT}/run_config_complete.ini"
+#  sbatch ${EVENT}/submit/run_master_slurm.sh
 done
