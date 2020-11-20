a129efa952465ccea93c70ffaf75afab5e626e28
Moritz Huebner <email@moritz-huebner.de>
2020-11-20 18:28:13 +1100

    Removed suffix argument

--- scripts/O3a/corner_plotting.py
+++ scripts/O3a/corner_plotting.py
@@ -9,7 +9,7 @@ for e in events:
         event = e.name
         detectors = e.detectors
         result = bilby.core.result.read_in_result(f'{event}/result/run_data0_{time_tag}_analysis_{detectors}_dynesty_merge_result.json')
-        result.outdir = f'{event}/result/'
+        result.outdir = f'.'
         result.plot_corner()
     except Exception as ex:
         print(ex)
