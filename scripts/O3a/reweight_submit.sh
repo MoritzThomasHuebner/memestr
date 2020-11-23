e98e6e267c59e8249ecb78b6caa3cf7248c13b5c
Moritz Huebner <email@moritz-huebner.de>
2020-11-23 21:05:39 +1100

    Set up reweighting for individual part runs

--- scripts/O3a/reweight_submit.sh
+++ scripts/O3a/reweight_submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=test
 #
 #SBATCH --ntasks=1
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=1G
 
 
-srun python reweight.py ${1}
+srun python reweight.py ${1} ${2}
--- scripts/O3a/reweight_super_submit.sh
+++ scripts/O3a/reweight_super_submit.sh
@@ -1,4 +1,7 @@
 for i in {0..50}
 do
-  sbatch reweight_submit.sh ${i}
+  for j in {0..4}
+  do
+    sbatch reweight_submit.sh ${i} ${j}
+  done
 done
