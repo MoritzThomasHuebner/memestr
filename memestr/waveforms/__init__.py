c405f6ddc9c7421adc985618aed4bca7bf38f324
Moritz Huebner <email@moritz-huebner.de>
2021-01-22 16:44:02 +1100

    Reparametrised spin parameters

--- memestr/waveforms/__init__.py
+++ memestr/waveforms/__init__.py
@@ -3,4 +3,3 @@ from .nrsur7dq4 import *
 from .nrhybsur3dq8 import *
 from .utils import *
 from .mwm import *
-from .phenom import *
--- memestr/waveforms/nrsur7dq4.py
+++ memestr/waveforms/nrsur7dq4.py
@@ -4,28 +4,59 @@ import gwmemory
 from .utils import apply_window, gamma_lmlm, convert_to_frequency_domain
 
 
-def fd_nr_sur_7dq4(frequencies, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+# def fd_nr_sur_7dq4(frequencies, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+#                    luminosity_distance, inc, phase, **kwargs):
+#     series = bilby.core.series.CoupledTimeAndFrequencySeries(start_time=0)
+#     series.frequency_array = frequencies
+#     waveform, memory_generator = _evaluate_surrogate(times=series.time_array, total_mass=total_mass,
+#                                                      mass_ratio=mass_ratio, inc=inc,
+#                                                      luminosity_distance=luminosity_distance, phase=phase,
+#                                                      s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23,
+#                                                      kwargs=kwargs, fold_in_memory=False)
+#     return convert_to_frequency_domain(memory_generator, series, waveform, inc, phase, **kwargs)
+
+
+def fd_nr_sur_7dq4(frequencies, mass_ratio, total_mass, a_1, a_2, tilt_1, tilt_2, phi_12, phi_jl,
                    luminosity_distance, inc, phase, **kwargs):
     series = bilby.core.series.CoupledTimeAndFrequencySeries(start_time=0)
     series.frequency_array = frequencies
+
+    mass_1, mass_2 = bilby.gw.conversion.total_mass_and_mass_ratio_to_component_masses(
+        mass_ratio=mass_ratio, total_mass=total_mass)
+    params = dict(a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+                  phi_jl=phi_jl, theta_jn=inc, mass_1=mass_1, mass_2=mass_2,
+                  reference_frequency=kwargs['reference_frequency'], phase=phase)
+    params = bilby.gw.conversion.generate_spin_parameters(params)
+
     waveform, memory_generator = _evaluate_surrogate(times=series.time_array, total_mass=total_mass,
                                                      mass_ratio=mass_ratio, inc=inc,
                                                      luminosity_distance=luminosity_distance, phase=phase,
-                                                     s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23,
+                                                     s11=params['spin_1x'], s12=params['spin_1y'],
+                                                     s13=params['spin_1z'], s21=params['spin_2x'],
+                                                     s22=params['spin_2y'], s23=params['spin_2z'],
                                                      kwargs=kwargs, fold_in_memory=False)
     return convert_to_frequency_domain(memory_generator, series, waveform, inc, phase, **kwargs)
 
 
-def fd_nr_sur_7dq4_with_memory(frequencies, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+def fd_nr_sur_7dq4_with_memory(frequencies, mass_ratio, total_mass, a_1, a_2, tilt_1, tilt_2, phi_12, phi_jl,
                                luminosity_distance, inc, phase, **kwargs):
     series = bilby.core.series.CoupledTimeAndFrequencySeries(start_time=0)
     series.frequency_array = frequencies
+
+    mass_1, mass_2 = bilby.gw.conversion.total_mass_and_mass_ratio_to_component_masses(
+        mass_ratio=mass_ratio, total_mass=total_mass)
+    params = dict(a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+                  phi_jl=phi_jl, theta_jn=inc, mass_1=mass_1, mass_2=mass_2,
+                  reference_frequency=kwargs['reference_frequency'], phase=phase)
+    params = bilby.gw.conversion.generate_spin_parameters(params)
 
     waveform, memory, memory_generator = _evaluate_surrogate(times=series.time_array, total_mass=total_mass,
                                                              mass_ratio=mass_ratio, inc=inc,
                                                              luminosity_distance=luminosity_distance,
                                                              phase=phase,
-                                                             s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23,
+                                                             s11=params['spin_1x'], s12=params['spin_1y'],
+                                                             s13=params['spin_1z'], s21=params['spin_2x'],
+                                                             s22=params['spin_2y'], s23=params['spin_2z'],
                                                              kwargs=kwargs)
     for mode in memory:
         waveform[mode] += memory[mode]
@@ -33,43 +64,82 @@ def fd_nr_sur_7dq4_with_memory(frequencies, mass_ratio, total_mass, s11, s12, s1
     return convert_to_frequency_domain(memory_generator, series, waveform, inc, phase, **kwargs)
 
 
-def fd_nr_sur_7dq4_memory_only(frequencies, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+def fd_nr_sur_7dq4_memory_only(frequencies, mass_ratio, total_mass, a_1, a_2, tilt_1, tilt_2, phi_12, phi_jl,
                                luminosity_distance, inc, phase, **kwargs):
     series = bilby.core.series.CoupledTimeAndFrequencySeries(start_time=0)
     series.frequency_array = frequencies
 
+    mass_1, mass_2 = bilby.gw.conversion.total_mass_and_mass_ratio_to_component_masses(
+        mass_ratio=mass_ratio, total_mass=total_mass)
+    params = dict(a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+                  phi_jl=phi_jl, theta_jn=inc, mass_1=mass_1, mass_2=mass_2,
+                  reference_frequency=kwargs['reference_frequency'], phase=phase)
+    params = bilby.gw.conversion.generate_spin_parameters(params)
+
     _, memory, memory_generator = _evaluate_surrogate(times=series.time_array, total_mass=total_mass,
                                                       mass_ratio=mass_ratio, inc=inc,
                                                       luminosity_distance=luminosity_distance,
-                                                      phase=phase, s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23,
+                                                      s11=params['spin_1x'], s12=params['spin_1y'],
+                                                      s13=params['spin_1z'], s21=params['spin_2x'],
+                                                      s22=params['spin_2y'], s23=params['spin_2z'],
                                                       kwargs=kwargs)
 
     return convert_to_frequency_domain(memory_generator, series, memory, inc, phase, **kwargs)
 
 
-def td_nr_sur_7dq4_memory_only(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+def td_nr_sur_7dq4_memory_only(times, mass_ratio, total_mass, a_1, a_2, tilt_1, tilt_2, phi_12, phi_jl,
                                luminosity_distance, inc, phase, **kwargs):
+
+    mass_1, mass_2 = bilby.gw.conversion.total_mass_and_mass_ratio_to_component_masses(
+        mass_ratio=mass_ratio, total_mass=total_mass)
+    params = dict(a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+                  phi_jl=phi_jl, theta_jn=inc, mass_1=mass_1, mass_2=mass_2,
+                  reference_frequency=kwargs['reference_frequency'], phase=phase)
+    params = bilby.gw.conversion.generate_spin_parameters(params)
+
     _, memory, _ = _evaluate_surrogate(times=times, total_mass=total_mass, mass_ratio=mass_ratio, inc=inc,
                                        luminosity_distance=luminosity_distance, phase=phase,
-                                       s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23, kwargs=kwargs,
+                                       s11=params['spin_1x'], s12=params['spin_1y'],
+                                       s13=params['spin_1z'], s21=params['spin_2x'],
+                                       s22=params['spin_2y'], s23=params['spin_2z'], kwargs=kwargs,
                                        fold_in_memory=True)
     return apply_window(waveform=memory, times=times, kwargs=kwargs)
 
 
-def td_nr_sur_7dq4(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+def td_nr_sur_7dq4(times, mass_ratio, total_mass, a_1, a_2, tilt_1, tilt_2, phi_12, phi_jl,
                    luminosity_distance, inc, phase, **kwargs):
+
+    mass_1, mass_2 = bilby.gw.conversion.total_mass_and_mass_ratio_to_component_masses(
+        mass_ratio=mass_ratio, total_mass=total_mass)
+    params = dict(a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+                  phi_jl=phi_jl, theta_jn=inc, mass_1=mass_1, mass_2=mass_2,
+                  reference_frequency=kwargs['reference_frequency'], phase=phase)
+    params = bilby.gw.conversion.generate_spin_parameters(params)
+
     waveform, memory = _evaluate_surrogate(times=times, total_mass=total_mass, mass_ratio=mass_ratio,
                                            inc=inc, luminosity_distance=luminosity_distance, phase=phase,
-                                           s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23,
+                                           s11=params['spin_1x'], s12=params['spin_1y'],
+                                           s13=params['spin_1z'], s21=params['spin_2x'],
+                                           s22=params['spin_2y'], s23=params['spin_2z'],
                                            kwargs=kwargs, fold_in_memory=False)
     return waveform
 
 
-def td_nr_sur_7dq4_with_memory(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+def td_nr_sur_7dq4_with_memory(times, mass_ratio, total_mass, a_1, a_2, tilt_1, tilt_2, phi_12, phi_jl,
                                luminosity_distance, inc, phase, **kwargs):
+
+    mass_1, mass_2 = bilby.gw.conversion.total_mass_and_mass_ratio_to_component_masses(
+        mass_ratio=mass_ratio, total_mass=total_mass)
+    params = dict(a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
+                  phi_jl=phi_jl, theta_jn=inc, mass_1=mass_1, mass_2=mass_2,
+                  reference_frequency=kwargs['reference_frequency'], phase=phase)
+    params = bilby.gw.conversion.generate_spin_parameters(params)
+
     waveform, memory, _ = _evaluate_surrogate(times=times, total_mass=total_mass, mass_ratio=mass_ratio,
                                               inc=inc, luminosity_distance=luminosity_distance, phase=phase,
-                                              s11=s11, s12=s12, s13=s13, s21=s21, s22=s22, s23=s23,
+                                              s11=params['spin_1x'], s12=params['spin_1y'],
+                                              s13=params['spin_1z'], s21=params['spin_2x'],
+                                              s22=params['spin_2y'], s23=params['spin_2z'],
                                               kwargs=kwargs, fold_in_memory=True)
     for mode in waveform:
         waveform[mode] += memory[mode]
