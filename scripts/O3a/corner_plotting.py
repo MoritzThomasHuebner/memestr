a076a7c798716102ab9c68e813565536feac9d77
Moritz Huebner <email@moritz-huebner.de>
2021-02-11 22:53:47 +1100

    Updated labels for corner plots

--- scripts/O3a/corner_plotting.py
+++ scripts/O3a/corner_plotting.py
@@ -3,17 +3,17 @@ import bilby
 from memestr.events import events, precessing_events
 
 
-for e in events:
-    try:
-        time_tag = e.time_tag
-        event = e.name
-        detectors = e.detectors
-        result = bilby.core.result.read_in_result(f'{event}/result/run_data0_{time_tag}_analysis_{detectors}_dynesty_merge_result.json')
-        result.outdir = f'.'
-        result.label = f"{e.name}_phenomx"
-        result.plot_corner()
-    except Exception as ex:
-        print(ex)
+# for e in events:
+#     try:
+#         time_tag = e.time_tag
+#         event = e.name
+#         detectors = e.detectors
+#         result = bilby.core.result.read_in_result(f'{event}/result/run_data0_{time_tag}_analysis_{detectors}_dynesty_merge_result.json')
+#         result.outdir = f'.'
+#         result.label = f"{e.name}_phenomx"
+#         result.plot_corner(filename=f'corner_plots/{e.name}_phenomx.png')
+#     except Exception as ex:
+#         print(ex)
 
 for e in precessing_events:
     try:
@@ -21,8 +21,6 @@ for e in precessing_events:
         event = e.name
         detectors = e.detectors
         result = bilby.core.result.read_in_result(f'{event}/result/run_data0_{time_tag}_analysis_{detectors}_dynesty_merge_result.json')
-        result.outdir = f'corner_plots'
-        result.label = f"{e.name}_nrsur"
-        result.plot_corner()
+        result.plot_corner(filename=f'corner_plots/{e.name}_nrsur.png')
     except Exception as e:
         print(e)
