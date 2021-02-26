64496f70aec91556ed2f642b14fe39c9fcbbfdf5
Moritz Huebner <email@moritz-huebner.de>
2021-02-26 13:19:24 +1100

    Added mode selection feature

--- memestr/waveforms/nrsur7dq4.py
+++ memestr/waveforms/nrsur7dq4.py
@@ -151,9 +151,9 @@ def _evaluate_surrogate(times, total_mass, mass_ratio, inc, luminosity_distance,
                                                     units='mks'
                                                     )
 
-    oscillatory, _ = memory_generator.time_domain_oscillatory(times=times, inc=inc, phase=phase)
+    oscillatory, _ = memory_generator.time_domain_oscillatory(times=times, inc=inc, phase=phase, modes=kwargs.get('modes', None))
     if not fold_in_memory:
         return oscillatory, memory_generator
     else:
-        memory, _ = memory_generator.time_domain_memory(inc=inc, phase=phase, gamma_lmlm=gamma_lmlm)
+        memory, _ = memory_generator.time_domain_memory(inc=inc, phase=phase, gamma_lmlm=gamma_lmlm, modes=kwargs.get('modes', None))
         return oscillatory, memory, memory_generator
