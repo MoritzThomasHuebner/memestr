87e7bd4ecb46da915d2de01b0f9e9cdda497976c
MoritzThomasHuebner <email@moritz-huebner.de>
2021-04-30 13:52:20 +1000

    Added post-processing script

--- scripts/analyse_injection_study.py
+++ scripts/analyse_injection_study.py
@@ -7,12 +7,11 @@ ln_bfs = []
 for i in range(2000):
     with open(f'injections/{i}_memory/IMR_mem_inj_non_mem_rec_result.json') as f:
         res = json.load(f)
-    with open(f'injections/{i}_memory/IMR_mem_inj_non_mem_rec_result.json') as f:
+    with open(f'injections/{i}_memory/pp_result.json') as f:
         pp_res = json.load(f)
 
     snrs_squared = [res['meta_data']['likelihood']['interferometers'][ifo]['optimal_SNR']**2 for ifo in ['H1', 'L1', 'V1']]
     network_snrs.append(np.sqrt(np.sum(snrs_squared)))
-    print(pp_res.keys())
     ln_bfs.append(pp_res['memory_log_bf'])
 
 ln_bfs = np.array(ln_bfs)
