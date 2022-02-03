a52e07cfdb6a4daaa43ab51ec41e43b8c20e3253
Moritz Huebner <email@moritz-huebner.de>
2022-02-03 16:20:43 +1100

    Reweight precessing runs

--- scripts/O3a/reweight.py
+++ scripts/O3a/reweight.py
@@ -72,7 +72,7 @@ if minimum_frequency != 0:
 try:
     log_memory_weights = np.loadtxt(outfile_name)
 except Exception:
-    use_stored_likelihood = True# minimum_frequency == 0
+    use_stored_likelihood = minimum_frequency == 0
     reweighted_time_shift_memory_log_bf, log_memory_weights = memestr.postprocessing.reweight_by_likelihood_parallel(
         new_likelihood=likelihood_mem, result=result,
         reference_likelihood=likelihood_osc, use_stored_likelihood=use_stored_likelihood, n_parallel=n_parallel)
