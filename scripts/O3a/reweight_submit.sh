880dc062df88a2a971c47380303f8058476f0ed0
Moritz Huebner <email@moritz-huebner.de>
2022-02-17 13:57:27 +1100

    Final parallelisation issue resolved

--- scripts/O3a/reweight_submit.sh
+++ scripts/O3a/reweight_submit.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=test
 #
 #SBATCH --ntasks=1
-#SBATCH --time=6:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=1G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=16
 
 
 #srun python reweight.py ${1} ${2} 16 ${3}
