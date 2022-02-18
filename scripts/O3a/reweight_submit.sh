f1527fa301ae7ea891c290104a331014592778e6
Moritz Huebner <email@moritz-huebner.de>
2022-02-18 15:46:26 +1100

    Redo runs

--- scripts/O3a/reweight_submit.sh
+++ scripts/O3a/reweight_submit.sh
@@ -4,7 +4,7 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=250M
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 
--- scripts/O3a/reweight_super_submit.sh
+++ scripts/O3a/reweight_super_submit.sh
@@ -1,5 +1,5 @@
 for i in {0..78}
 do
   sbatch reweight_submit.sh ${i} 0 0
-  sbatch reweight_submit.sh ${i} 1 0
+#  sbatch reweight_submit.sh ${i} 1 0
 done
