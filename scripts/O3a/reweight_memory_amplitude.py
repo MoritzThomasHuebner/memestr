19f7228902815a9cbfff9da1c409fce8cc8e4235
Moritz Huebner <email@moritz-huebner.de>
2022-02-18 17:17:10 +1100

    fixed list comprehension

--- scripts/O3a/reweight_memory_amplitude.py
+++ scripts/O3a/reweight_memory_amplitude.py
@@ -69,10 +69,12 @@ outfile_name = f"{event}_memory_amplitude_samples"
 reweighting_terms_list = memestr.postprocessing.reconstruct_memory_amplitude_parallel(
     result=result, likelihood_memory=likelihood_mem, likelihood_oscillatory=likelihood_osc, n_parallel=n_parallel)
 
-amplitude_samples = np.flatten([[term.memory_amplitude_sample for term in reweighting_terms] for reweighting_terms in reweighting_terms_list])
-d_inner_h_mem = np.flatten([[term.d_inner_h_mem for term in reweighting_terms] for reweighting_terms in reweighting_terms_list])
-optimal_snr_squared_h_mem = np.flatten([[term.optimal_snr_squared_h_mem for term in reweighting_terms] for reweighting_terms in reweighting_terms_list])
-h_osc_inner_h_mem = np.flatten([[term.h_osc_inner_h_mem for term in reweighting_terms] for reweighting_terms in reweighting_terms_list])
+import itertools
+
+amplitude_samples = list(itertools.chain(*[[term.memory_amplitude_sample for term in reweighting_terms] for reweighting_terms in reweighting_terms_list]))
+d_inner_h_mem = list(itertools.chain(*[[term.d_inner_h_mem for term in reweighting_terms] for reweighting_terms in reweighting_terms_list]))
+optimal_snr_squared_h_mem = list(itertools.chain(*[[term.optimal_snr_squared_h_mem for term in reweighting_terms] for reweighting_terms in reweighting_terms_list]))
+h_osc_inner_h_mem = list(itertools.chain(*[[term.h_osc_inner_h_mem for term in reweighting_terms] for reweighting_terms in reweighting_terms_list]))
 
 np.savetxt(f'memory_amplitude_results/{event}_memory_amplitude_posterior.txt', amplitude_samples)
 np.savetxt(f'memory_amplitude_results/{event}_d_inner_h_mem.txt', d_inner_h_mem)
