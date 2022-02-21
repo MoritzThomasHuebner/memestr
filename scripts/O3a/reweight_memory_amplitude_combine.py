def13f625592d5e8fa26b86c76afe5ed618ac1c7
Moritz Huebner <email@moritz-huebner.de>
2022-02-21 17:40:09 +1100

    combining memory amplitude analyses

--- scripts/O3a/reweight_memory_amplitude_combine.py
+++ scripts/O3a/reweight_memory_amplitude_combine.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from scipy.stats.kde import gaussian_kde
 
 from memestr.events import events, precessing_events
@@ -12,8 +13,9 @@ else:
     event_list = events
 
 amps = np.linspace(-500, 500, 1000)
-ln_probs = np.ones(len(amps))
+ln_probs = np.zeros(len(amps))
 dx = amps[1] - amps[0]
+unlogged_probs = np.ones(len(amps))
 
 for event in event_list:
     event_name = event.name
@@ -23,17 +25,17 @@ for event in event_list:
 
     filename = f"memory_amplitude_results/{event_name}_memory_amplitude_posterior.txt"
     try:
-        samples = np.loadtxt(f"memory_amplitude_results/{event_name}_memory_amplitude_posterior.txt")
+        samples = np.array(pd.read_csv(f'memory_amplitude_results/{event_name}_memory_amplitude_posterior.csv')['amplitude_samples'])
     except OSError as e:
         print(e)
-        break
+        continue
     probs, edges = np.histogram(samples, bins=1000, density=True)
     ln_probs += np.log(probs)
-
-    plt.step(amps, probs)
-    plt.title(event)
-    plt.savefig(f"memory_amplitude_results/{event_name}_rebinned_posterior.png")
-    plt.clf()
+    unlogged_probs *= probs
+    # plt.step(amps, probs)
+    # plt.title(event)
+    # plt.savefig(f"memory_amplitude_results/{event_name}_rebinned_posterior.png")
+    # plt.clf()
 
 probs = np.exp(ln_probs)
 integrand = probs * dx
@@ -41,23 +43,34 @@ integrated_probs = np.sum(probs * dx)
 probs /= integrated_probs
 cdf = np.cumsum(probs)
 
+print(amps[np.argmax(probs)])
+print(amps[np.where(cdf < 0.05)[0][-1]])
+print(amps[np.where(cdf > 0.95)[0][0]])
+
 plt.step(amps, probs, where='mid')
 plt.xlabel('memory amplitude')
 plt.ylabel('probability')
-plt.savefig(f'memory_amplitude_results/combined_result.png')
+plt.savefig(f'memory_amplitude_results/combined_result_alt.png')
+plt.clf()
+
+plt.step(amps, unlogged_probs, where='mid')
+plt.xlabel('memory amplitude')
+plt.ylabel('probability')
+plt.xlim(-50, 50)
+plt.savefig(f'memory_amplitude_results/combined_result_zoomed_alt_unlogged.png')
 plt.clf()
 
 plt.step(amps, probs, where='mid')
 plt.xlabel('memory amplitude')
 plt.ylabel('probability')
 plt.xlim(-50, 50)
-plt.savefig(f'memory_amplitude_results/combined_result_zoomed.png')
+plt.savefig(f'memory_amplitude_results/combined_result_zoomed_alt.png')
 plt.clf()
 
 plt.step(amps, cdf, where='mid')
 plt.xlabel('memory amplitude')
 plt.ylabel('CDF')
-plt.savefig(f'memory_amplitude_results/combined_result_cdf.png')
+plt.savefig(f'memory_amplitude_results/combined_result_cdf_alt.png')
 plt.clf()
 
 
--- scripts/O3a/reweight_memory_amplitude_combine_pop.py
+++ scripts/O3a/reweight_memory_amplitude_combine_pop.py
@@ -1,8 +1,7 @@
-import bilby.core.result
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.special import logsumexp
+import matplotlib.pyplot as plt
 
 from memestr.events import events, precessing_events
 from memestr.postprocessing import reweight_by_memory_amplitude
@@ -14,10 +13,6 @@ if precessing:
 else:
     event_list = events
 
-amps = np.linspace(-50, 50, 10000)
-ln_probs = np.ones(len(amps))
-dx = amps[1] - amps[0]
-
 posteriors = []
 
 for event in event_list:
@@ -25,25 +20,77 @@ for event in event_list:
     if precessing:
         event_name += "_2000"
     try:
-        posteriors.append(pd.read_csv(f'memory_amplitude_results/{event_name}_memory_amplitude_posterior.csv'))
+        posterior = pd.read_csv(f'memory_amplitude_results/{event_name}_memory_amplitude_posterior.csv')
+        posterior['d_inner_h_mem'] = np.array([complex(a) for a in np.array(posterior['d_inner_h_mem'])])
+        posteriors.append(posterior)
     except Exception as e:
         print(e)
-        break
+        continue
+
+
+amps = np.linspace(-50, 50, 1000)
+dx = amps[1] - amps[0]
+
 
 combined_log_l = []
 
 
 def calculate_inner_sum(memory_amplitude, posterior):
-    return logsumexp(reweight_by_memory_amplitude(
+    res = logsumexp(reweight_by_memory_amplitude(
         memory_amplitude=memory_amplitude, d_inner_h_mem=posterior['d_inner_h_mem'],
         optimal_snr_squared_h_mem=posterior['optimal_snr_squared_h_mem'],
         h_osc_inner_h_mem=posterior['h_osc_inner_h_mem']))
+    return res
+
+
+def calculate_outer_sum(memory_amplitude, posteriors):
+    outer_sum = 0
+    for posterior in posteriors:
+        outer_sum += calculate_inner_sum(memory_amplitude, posterior) - np.log(len(posterior))
+    return outer_sum
 
 
 for a in amps:
-    print(a)
-    log_l = np.sum([calculate_inner_sum(a, posterior) - np.log(len(posterior))] for posterior in posteriors)
+    log_l = calculate_outer_sum(memory_amplitude=a, posteriors=posteriors)
     combined_log_l.append(log_l)
+    print(f"{a}:\t{log_l}")
 
 res = dict(memory_amplitudes=amps, log_l=combined_log_l)
-pd.DataFrame.from_dict(res).to_csv('memory_amplitude_results/combined_memory_amplitude_posterior_samples.csv')
+pd.DataFrame.from_dict(res).to_csv('memory_amplitude_results/combined_memory_amplitude_posterior_pdf.csv', index=False)
+df = pd.read_csv('memory_amplitude_results/combined_memory_amplitude_posterior_pdf.csv')
+amps = np.array(df['memory_amplitudes'])
+combined_log_l = np.array(df['log_l'])
+
+combined_log_l -= np.max(combined_log_l)
+probs = np.exp(combined_log_l)
+probs = np.nan_to_num(probs)
+integrand = probs * dx
+integrated_probs = np.sum(probs * dx)
+probs /= integrated_probs
+
+cdf = np.cumsum(probs)
+cdf /= cdf[-1]
+print(amps[np.argmax(probs)])
+print(amps[np.where(cdf < 0.05)[0][-1]])
+print(amps[np.where(cdf > 0.95)[0][0]])
+
+plt.step(amps, probs, where='mid')
+plt.xlabel('memory amplitude')
+plt.ylabel('probability')
+plt.savefig(f'memory_amplitude_results/combined_result.png')
+plt.clf()
+
+plt.step(amps, probs, where='mid')
+plt.xlabel('memory amplitude')
+plt.ylabel('probability')
+plt.xlim(-50, 50)
+plt.savefig(f'memory_amplitude_results/combined_result_zoomed.png')
+plt.clf()
+
+plt.step(amps, cdf, where='mid')
+plt.xlabel('memory amplitude')
+plt.ylabel('CDF')
+plt.savefig(f'memory_amplitude_results/combined_result_cdf.png')
+plt.clf()
+
+
