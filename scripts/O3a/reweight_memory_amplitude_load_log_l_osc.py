31f99c3e523bc3385867b31cb93349ad87e6cda6
Moritz Huebner <email@moritz-huebner.de>
2022-03-02 00:43:04 +1100

    Added script to load log_l_osc into the csv files

--- scripts/O3a/reweight_memory_amplitude_load_log_l_osc.py
+++ scripts/O3a/reweight_memory_amplitude_load_log_l_osc.py
@@ -3,8 +3,10 @@ import pandas as pd
 import memestr
 
 for event in memestr.events.events:
-    posterior = pd.read_csv(f'memory_amplitude_results/{event.name}_memory_amplitude_posterior_50.csv')
+    csv_file = f'memory_amplitude_results/{event.name}_memory_amplitude_posterior_50.csv'
+    posterior = pd.read_csv(csv_file)
     result = bilby.core.result.read_in_result(
         f'{event.name}/result/run_data0_{event.time_tag}_analysis_{event.detectors}_dynesty_merge_result.json')
     posterior['log_l_osc'] = result.posterior['log_likelihood']
-    posterior.to_csv('test.csv', index=False)
+    posterior.to_csv(csv_file, index=False)
+    print(event.name)
